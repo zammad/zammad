838a8ea807a1624d277e19cdf1c62f660ea59595
Martin Edenhofer <me@edenhofer.de>
2012-10-23 21:19:07 +0200

    Removed debugging.

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,21 +1,21 @@
 module Cache
   def self.delete( key )
-    puts 'Cache.delete' + key.to_s
+#    puts 'Cache.delete' + key.to_s
     Rails.cache.delete( key.to_s )
   end
   def self.write( key, data, params = {} )
     if !params[:expires_in]
       params[:expires_in] = 24.hours
     end
-    puts 'Cache.write: ' + key.to_s
+#    puts 'Cache.write: ' + key.to_s
     Rails.cache.write( key.to_s, data, params)
   end
   def self.get( key )
-    puts 'Cache.get: ' + key.to_s
+#    puts 'Cache.get: ' + key.to_s
     Rails.cache.read( key.to_s )
   end
   def self.clear
-    puts 'Cache.clear...'
+#    puts 'Cache.clear...'
     Rails.cache.clear
   end
 end
