2e04329e14afd0cb8eed60c5ec3514dccb747f9b
roy <roy@kaldung.com>
2012-11-12 20:43:15 +0100

    fixed UI test

--- test/ui_test.rb
+++ test/ui_test.rb
@@ -23,7 +23,6 @@ class ExampleTest < Test::Unit::TestCase
 
     def test_login_failed
        browser.get "http://portal.znuny.com/"
-       puts "Page title is #{browser.title}"
        element_username = browser.find_element :name => "username"
        element_username.send_keys "roy@kaldung.de"
        element_password = browser.find_element :name => "password"
@@ -34,12 +33,12 @@ class ExampleTest < Test::Unit::TestCase
 
     def test_login_passed
        browser.get "http://portal.znuny.com/"
-       puts "Page title is #{browser.title}"
        element_username = browser.find_element :name => "username"
        element_username.send_keys "roy@kaldung.com"
        element_password = browser.find_element :name => "password"
        element_password.send_keys "090504"
        element_password.submit
+       browser.wait_for_page_to_load
        assert_equal browser.current_url, "https://portal.znuny.com/#ticket_view/my_tickets"
     end
 end
