098db74038af21e6c1ababc2ef68167372790d81
Martin Edenhofer <me@edenhofer.de>
2012-11-13 13:41:06 +0100

    Migration not longer needed.

--- db/migrate/20121113085712_organization_shared.rb
+++
@@ -1,8 +0,0 @@
-class OrganizationShared < ActiveRecord::Migration
-  def up
-    add_column :overviews, :organization_shared, :boolean, :null => false, :default => false
-  end
-
-  def down
-  end
-end
