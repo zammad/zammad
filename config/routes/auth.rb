5ef7947521cc6e73362d901aefe9c5ae21c5f25e
Martin Edenhofer <me@edenhofer.de>
2012-11-22 15:40:36 +0100

    Removed white spaces.

--- config/routes/auth.rb
+++ config/routes/auth.rb
@@ -3,12 +3,12 @@ module ExtraRoutes
 
     # omniauth
     map.match '/auth/:provider/callback', :to => 'sessions#create_omniauth'
-    
+
     # sessions
     map.match '/signin',                  :to => 'sessions#create'
     map.match '/signshow',                :to => 'sessions#show'
     map.match '/signout',                 :to => 'sessions#destroy'
-    
+
   end
   module_function :add
 end
