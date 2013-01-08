f31e3350bf09bf569c2ec8f82be4877abceac2ee
Roy Kaldung <roy@kaldung.com>
2013-01-08 14:47:12 +0100

    test broken build

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'simplecov'
+rsisequire 'simplecov'
 require 'simplecov-rcov'
 
 class ActiveSupport::TestCase
