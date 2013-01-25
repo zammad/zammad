9f528316e7e637c17bab3a3a6af1c3f40ac93194
Martin Edenhofer <me@edenhofer.de>
2013-01-25 10:15:41 +0100

    Extend Data with getWeek().

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,9 @@ if (!window.console) {
 if (!console.log) {
   console.log = function(){}
 }
+
+
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(),0,1);
+  return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+}
