bf56a3a17eae16b12fe62d90438db06a6268f6db
Martin Edenhofer <me@edenhofer.de>
2013-01-25 11:57:44 +0100

    Improved compat. check.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -53,7 +53,7 @@ class Setting < ApplicationModel
       self.state_initial = self.state
     end
     def state_check
-      if self.state
+      if self.state || self.state == false
         if !self.state.respond_to?('has_key?') || !self.state.has_key?(:value)
           self.state = { :value => self.state }
         end
