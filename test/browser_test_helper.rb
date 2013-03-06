5fdde73d460e13ee7ee37d222672f4be7fd45d94
Martin Edenhofer <me@edenhofer.de>
2013-03-06 23:14:44 +0100

    Removed not needed gem.

--- test/browser_test_helper.rb
+++ test/browser_test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-#require 'watir-webdriver'
 require 'selenium-webdriver'
 
 class TestCase < Test::Unit::TestCase
