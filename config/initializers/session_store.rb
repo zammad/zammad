f85adab5f134b2ce91a51ac91e227f8997dc4d31
Martin Edenhofer <me@edenhofer.de>
2013-03-06 15:24:46 +0100

    Generate uniq session keys (needed if you run multible installations).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,6 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Zammad::Application.config.session_store :active_record_store
+Zammad::Application.config.session_store :active_record_store, {
+  :key => '_zammad_session_' + Digest::MD5.hexdigest(Rails.root.to_s).to_s[5..15]
+}
