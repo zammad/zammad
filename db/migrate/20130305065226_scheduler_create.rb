b5c07f089cdde2b8d86937dc1f5ef8c0ad0a4b79
Martin Edenhofer <me@edenhofer.de>
2013-03-07 08:25:10 +0100

    Try to get correct exit code for unit tests.

--- db/migrate/20130305065226_scheduler_create.rb
+++ db/migrate/20130305065226_scheduler_create.rb
@@ -13,30 +13,6 @@ class SchedulerCreate < ActiveRecord::Migration
       t.column :created_by_id,  :integer,                 :null => false
       t.timestamps
     end
-    Scheduler.create(
-      :name           => 'Check Channels',
-      :method         => 'Channel.fetch',
-      :period         => 30,
-      :active         => true,
-      :updated_by_id  => 1,
-      :created_by_id  => 1,
-    )
-    Scheduler.create(
-      :name           => 'Import OTRS diff load',
-      :method         => 'Import::OTRS.diff_loop',
-      :period         => 60 * 10,
-      :active         => true,
-      :updated_by_id  => 1,
-      :created_by_id  => 1,
-    )
-    Scheduler.create(
-      :name           => 'Generate Session data',
-      :method         => 'Session.jobs',
-      :period         => 60,
-      :active         => true,
-      :updated_by_id  => 1,
-      :created_by_id  => 1,
-    )
     add_index :schedulers, [:name], :unique => true
   end
 
