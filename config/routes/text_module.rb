a0ab8b30c35c3ece0eb90dfdf20ef7265b59ee2e
Martin Edenhofer <me@edenhofer.de>
2013-05-29 17:07:27 +0200

    Fixed controller name.

--- config/routes/text_module.rb
+++ config/routes/text_module.rb
@@ -2,11 +2,11 @@ module ExtraRoutes
   def add(map)
 
     # roles
-    map.match '/api/text_modules',                  :to => 'text_modules#index',  :via => :get
-    map.match '/api/text_modules/:id',              :to => 'text_modules#show',   :via => :get
-    map.match '/api/text_modules',                  :to => 'text_modules#create', :via => :post
-    map.match '/api/text_modules/:id',              :to => 'text_modules#update', :via => :put
-    map.match '/api/text_modules/:id',              :to => 'templates#destroy',   :via => :delete
+    map.match '/api/text_modules',                  :to => 'text_modules#index',    :via => :get
+    map.match '/api/text_modules/:id',              :to => 'text_modules#show',     :via => :get
+    map.match '/api/text_modules',                  :to => 'text_modules#create',   :via => :post
+    map.match '/api/text_modules/:id',              :to => 'text_modules#update',   :via => :put
+    map.match '/api/text_modules/:id',              :to => 'text_modules#destroy',  :via => :delete
 
   end
   module_function :add
