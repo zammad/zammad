c58485281f80b86c18ef2dbf29588b678fccffa0
Martin Edenhofer <me@edenhofer.de>
2013-05-29 17:11:34 +0200

    Prepard for rails 4.

--- config/routes/tag.rb
+++ config/routes/tag.rb
@@ -2,9 +2,9 @@ module ExtraRoutes
   def add(map)
 
     # links
-    map.match '/api/tags',                       :to => 'tags#list'
-    map.match '/api/tags/add',                   :to => 'tags#add'
-    map.match '/api/tags/remove',                :to => 'tags#remove'
+    map.match '/api/tags',                       :to => 'tags#list', :via => :get
+    map.match '/api/tags/add',                   :to => 'tags#add', :via => :get
+    map.match '/api/tags/remove',                :to => 'tags#remove', :via => :get
 
   end
   module_function :add
