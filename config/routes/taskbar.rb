e47b20ec0304a84f3f793f7ef1d8c9b8fc6809a5
Martin Edenhofer <me@edenhofer.de>
2013-07-26 22:50:28 +0200

    Small code layout change.

--- config/routes/taskbar.rb
+++ config/routes/taskbar.rb
@@ -4,7 +4,7 @@ module ExtraRoutes
     map.match '/api/taskbar/:id',              :to => 'taskbar#show',   :via => :get
     map.match '/api/taskbar',                  :to => 'taskbar#create', :via => :post
     map.match '/api/taskbar/:id',              :to => 'taskbar#update', :via => :put
-    map.match '/api/taskbar/:id',              :to => 'taskbar#destroy',   :via => :delete
+    map.match '/api/taskbar/:id',              :to => 'taskbar#destroy',:via => :delete
   end
   module_function :add
 end
