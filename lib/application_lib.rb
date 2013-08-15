3feab3354d757eb0e8052b5734241d14d7473af5
Martin Edenhofer <me@edenhofer.de>
2013-08-15 23:15:05 +0200

    Added doc.

--- lib/application_lib.rb
+++ lib/application_lib.rb
@@ -1,4 +1,17 @@
 class ApplicationLib
+
+=begin
+
+load adapter based on setting option
+
+  adapter = self.load_adapter_by_setting( 'some_setting_with_class_name' )
+
+returns
+
+  result = adapter_class
+
+=end
+
   def self.load_adapter_by_setting(setting)
     adapter = Setting.get( setting )
     return if !adapter
@@ -6,12 +19,22 @@ class ApplicationLib
     # load backend
     self.load_adapter(adapter)
   end
+
+=begin
+
+load adapter
+
+  adapter = self.load_adapter( 'some_class_name' )
+
+returns
+
+  result = adapter_class
+
+=end
+
   def self.load_adapter(adapter)
 
     # load adapter
-    backend = Object.const_get(adapter)
-
-    # return backend
-    return backend
+    Object.const_get(adapter)
   end
 end
