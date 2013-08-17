300453e2c13352ea0498b7c472a2d6193df51645
Martin Edenhofer <me@edenhofer.de>
2013-08-17 23:48:48 +0200

    Added file header.

--- lib/geo_ip/freegeoip.rb
+++ lib/geo_ip/freegeoip.rb
@@ -1,3 +1,5 @@
+# Copyright (C) 2012-2013 Zammad Foundation, http://zammad-foundation.org/
+
 require 'faraday'
 require 'cache'
 
--- lib/geo_location/gmaps.rb
+++ lib/geo_location/gmaps.rb
@@ -1,3 +1,5 @@
+# Copyright (C) 2012-2013 Zammad Foundation, http://zammad-foundation.org/
+
 class GeoLocation::Gmaps
 
   def self.geocode(address)
