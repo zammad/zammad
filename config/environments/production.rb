067576f6f480626b993e184ad3bd0705d381edba
Martin Edenhofer <me@edenhofer.de>
2013-09-15 23:40:25 +0200

    Moved to rails4 settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,6 @@ Zammad::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   # autoload on
   config.dependency_loading = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,9 +27,6 @@ Zammad::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
