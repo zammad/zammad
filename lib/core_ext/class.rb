d6d21e6cfe0d6747d8d6c524ee8cb491b4cffa7e
Martin Edenhofer <me@edenhofer.de>
2013-09-20 13:40:25 +0200

    Improved to_app_model.

--- lib/core_ext/class.rb
+++ lib/core_ext/class.rb
@@ -1,6 +1,10 @@
 class Class
   def to_app_model
-    name = self.to_s.downcase
-    name.gsub( /::/, '_' )
+    camel_cased_word = self.to_s
+    camel_cased_word.gsub(/::/, '_').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
   end
 end
