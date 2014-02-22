be58283432da9967d9922fde717b28f48723f30d
Roy Kaldung <roy@kaldung.com>
2014-02-22 13:39:03 +0100

    fixed typo, removed commented out lines ;-)

--- db/migrate/20140128000001_add_search_index.rb
+++ db/migrate/20140128000001_add_search_index.rb
@@ -1,42 +1,37 @@
 class AddSearchIndex < ActiveRecord::Migration
   def up
     Setting.create_or_update(
-      :title       => 'Elastic Search Endpoint URL',
+      :title       => 'Elasticsearch Endpoint URL',
       :name        => 'es_url',
-      :area        => 'SearchIndex::ElasticSearch',
+      :area        => 'SearchIndex::Elasticsearch',
       :description => 'Define endpoint of Elastic Search.',
       :state       => '',
       :frontend => false
     )
     Setting.create_or_update(
-      :title       => 'Elastic Search Endpoint User',
+      :title       => 'Elasticsearch Endpoint User',
       :name        => 'es_user',
-      :area        => 'SearchIndex::ElasticSearch',
-      :description => 'Define http basic auth user of Elastic Search.',
+      :area        => 'SearchIndex::Elasticsearch',
+      :description => 'Define http basic auth user of Elasticsearch.',
       :state       => '',
       :frontend => false
     )
     Setting.create_or_update(
       :title       => 'Elastic Search Endpoint Password',
       :name        => 'es_password',
-      :area        => 'SearchIndex::ElasticSearch',
-      :description => 'Define http basic auth password of Elastic Search.',
+      :area        => 'SearchIndex::Elasticsearch',
+      :description => 'Define http basic auth password of Elasticsearch.',
       :state       => '',
       :frontend => false
     )
     Setting.create_or_update(
       :title       => 'Elastic Search Endpoint Index',
       :name        => 'es_index',
-      :area        => 'SearchIndex::ElasticSearch',
-      :description => 'Define Elastic Search index name.',
+      :area        => 'SearchIndex::Elasticsearch',
+      :description => 'Define Elasticsearch index name.',
       :state       => 'zammad',
       :frontend => false
     )
-
-#    Setting.set('es_url', 'http://217.111.80.181')
-#    Setting.set('es_user', 'elasticsearch')
-#    Setting.set('es_password', 'zammad')
-#    Setting.set('es_index', Socket.gethostname + '_zammad')
 
     Ticket.search_index_reload
     User.search_index_reload
