5e7e1f9423f6a1a0f8ee51ca3c6f120081726fe3
rkaldung <roy@kaldung.com>
2014-08-11 14:33:29 +0200

    added distro detection

--- script/install.sh
+++ script/install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+get_distro(){
+  arch=$(uname -m)
+  kernel=$(uname -r)
+  if [ -f /etc/lsb-release ]; then
+    os=$(lsb_release -s -d)
+  elif [ -f /etc/debian_version ]; then
+    os="Debian $(cat /etc/debian_version)"
+  elif [ -f /etc/redhat-release ]; then
+    os=`cat /etc/redhat-release`
+  else
+    os="$(uname -s) $(uname -r)"
+  fi
+}
 
