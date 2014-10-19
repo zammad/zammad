e6ccb4522529ded65ec76d457753b8b6328c93c0
Thorsten Eckel <te@znuny.com>
2014-10-19 18:56:53 +0200

    Fixed follow up issue of reload/load commit 7311868.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,8 @@
 # Copyright (C) 2012-2014 Zammad Foundation, http://zammad-foundation.org/
 
 class Store < ApplicationModel
-  require 'store/object'
-  require 'store/file'
+  load 'store/object.rb'
+  load 'store/file.rb'
 
   store       :preferences
   belongs_to  :store_object,          :class_name => 'Store::Object'
