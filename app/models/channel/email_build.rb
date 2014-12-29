201116bbe069cbcb6813fa26888ccc8402829f01
Martin Edenhofer <me@edenhofer.de>
2014-12-30 00:31:57 +0100

    Removed not needed sign.

--- app/models/channel/email_build.rb
+++ app/models/channel/email_build.rb
@@ -91,7 +91,7 @@ module Channel::EmailBuild
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>'
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <head>
   <body style="#{css}">#{html}</body>
 </html>
