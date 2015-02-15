eaf537017ef04a0538e5e2af6ced5e8750a0c86c
Martin Edenhofer <me@edenhofer.de>
2015-02-15 13:54:28 +0100

    If system_init_done is not finished, allow inserts with id for any objects.

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -44,7 +44,7 @@ class ApplicationModel < ActiveRecord::Base
   @@import_class_list = ['Ticket', 'Ticket::Article', 'History', 'Ticket::State', 'Ticket::StateType', 'Ticket::Priority', 'Group', 'User', 'Role' ]
 
   def check_attributes_protected
-    if ( !Setting.get('system_init_done') || Setting.get('import_mode')) && @@import_class_list.include?( self.class.to_s )
+    if !Setting.get('system_init_done') || ( Setting.get('import_mode') && @@import_class_list.include?( self.class.to_s ) )
       # do noting, use id as it is
     else
       self[:id] = nil
