c1d2eca4a5677b1b7051f8e488ed140b450dfe53
Martin Edenhofer <me@edenhofer.de>
2015-02-26 01:12:12 +0100

    Extended test.

--- test/integration/elasticsearch_test.rb
+++ test/integration/elasticsearch_test.rb
@@ -1,16 +1,18 @@
 # encoding: utf-8
 require 'integration_test_helper'
 
-class ElasticSearchTest < ActiveSupport::TestCase
+class ElasticsearchTest < ActiveSupport::TestCase
 
-  Setting.set('es_url', 'http://172.0.0.1:9200')
+  #Setting.set('es_url', 'http://172.0.0.1:9200')
+  Setting.set('es_url', 'http://10.240.2.11:9200')
   Setting.set('es_index', 'estest.local_zammad')
   #Setting.set('es_user', 'elasticsearch')
   #Setting.set('es_password', 'zammad')
 
   # drop/create indexes
-  Rake::Task["searchindex:drop"].execute
-  Rake::Task["searchindex:create"].execute
+  #Rake::Task["searchindex:drop"].execute
+  #Rake::Task["searchindex:create"].execute
+  system('rake searchindex:rebuild')
 
   groups = Group.where( :name => 'Users' )
   roles  = Role.where( :name => 'Agent' )
@@ -74,7 +76,7 @@ class ElasticSearchTest < ActiveSupport::TestCase
       :ticket_id     => ticket2.id,
       :from          => 'some_sender@example.org',
       :to            => 'some_recipient@example.org',
-      :subject       => 'some subject2',
+      :subject       => 'some subject2 / autobahn what else?',
       :message_id    => 'some@id',
       :body          => 'some other message',
       :internal      => false,
@@ -87,7 +89,7 @@ class ElasticSearchTest < ActiveSupport::TestCase
 
     ticket3 = Ticket.create(
       :title         => "something else",
-      :group         => Group.lookup( :name => 'Users'),
+      :group         => Group.lookup( :name => 'WithoutAccess'),
       :customer_id   => 2,
       :state         => Ticket::State.lookup( :name => 'open' ),
       :priority      => Ticket::Priority.lookup( :name => '2 normal' ),
@@ -100,7 +102,7 @@ class ElasticSearchTest < ActiveSupport::TestCase
       :to            => 'some_recipient@example.org',
       :subject       => 'some subject3',
       :message_id    => 'some@id',
-      :body          => 'some other message 3',
+      :body          => 'some other message 3 / kindergarden what else?',
       :internal      => false,
       :sender        => Ticket::Article::Sender.where(:name => 'Customer').first,
       :type          => Ticket::Article::Type.where(:name => 'email').first,
@@ -112,11 +114,24 @@ class ElasticSearchTest < ActiveSupport::TestCase
 
     result = Ticket.search(
       :current_user => agent,
-      :query        => 'subject2',
+      :query        => 'autobahn',
       :limit        => 15,
     )
-    puts "RRR #{result.inspect}"
-    assert_equal( 24, Group.count, 'groups' )
+
+    assert(!result.empty?, 'result exists')
+    assert(result[0], 'record 1')
+    assert(!result[1], 'record 2')
+    assert_equal(result[0].id, ticket2.id)
+
+
+    result = Ticket.search(
+      :current_user => agent,
+      :query        => 'kindergarden',
+      :limit        => 15,
+    )
+    assert(result.empty?, 'result should be empty')
+    assert(!result[0], 'record 1')
+
 
   end
 end
