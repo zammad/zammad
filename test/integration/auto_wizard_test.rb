d92992c670a5a332794daee7a1c23d3a2f269e52
Martin Edenhofer <me@edenhofer.de>
2015-04-09 01:39:27 +0200

    Integration test for auto wizard.

---
+++ test/integration/auto_wizard_test.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require 'browser_test_helper'
+
+class AutoWizardTest < TestCase
+  def test_auto_wizard
+    @browser = browser_instance
+    location( :url => browser_url )
+
+    watch_for(
+      :css     => 'body',
+      :value   => 'Invite',
+      :timeout => 10,
+    )
+
+    click( :css => '.content .btn--primary' )
+
+    watch_for(
+      :css       => '.user-menu .user a',
+      :attribute => 'title',
+      :value     => 'hans.atila@zammad.org',
+      :timeout   => 20,
+    )
+
+  end
+
+end
