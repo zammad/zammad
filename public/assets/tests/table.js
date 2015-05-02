4b7026ab4dbb104d7f133aa7ed1d0c844896b2b0
Martin Edenhofer <me@edenhofer.de>
2015-05-02 11:11:40 +0200

    Added new translation.

--- public/assets/tests/table.js
+++ public/assets/tests/table.js
@@ -496,7 +496,7 @@ test( "table test 3", function() {
     },
   })
   equal( el.find('table > thead > tr').length, 1, 'row count')
-  equal( el.find('table > thead > tr > th:nth-child(1)').text().trim(), 'Realname', 'check header')
+  equal( el.find('table > thead > tr > th:nth-child(1)').text().trim(), 'richtiger Name', 'check header')
   equal( el.find('table > thead > tr > th:nth-child(3)').text().trim(), 'Some Name', 'check header')
   equal( el.find('tbody > tr:nth-child(1) > td').length, 3, 'check row 1')
   equal( el.find('tbody > tr:nth-child(1) > td:nth-child(1)').text().trim(), 'realname 55', 'check row 1')
