c5ae5e767c7f8c4e1be85ba70e4d5afb89c504d8
Martin Edenhofer <me@edenhofer.de>
2015-05-15 09:45:32 +0200

    Do not serve static assets in production per default.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Zammad::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
