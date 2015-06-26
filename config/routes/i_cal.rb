8c775463c585c409494a629d790ce661a419240a
Thorsten Eckel <te@znuny.com>
2015-06-26 10:24:18 +0200

    Removed api version prefix for iCal routes.

--- config/routes/i_cal.rb
+++ config/routes/i_cal.rb
@@ -1,7 +1,6 @@
 Zammad::Application.routes.draw do
-  api_path = Rails.configuration.api_path
 
-  match api_path + '/ical',                 to: 'i_cal#all',    via: :get
-  match api_path + '/ical/:object',         to: 'i_cal#object', via: :get
-  match api_path + '/ical/:object/:method', to: 'i_cal#object', via: :get
+  match '/ical',                 to: 'i_cal#all',    via: :get
+  match '/ical/:object',         to: 'i_cal#object', via: :get
+  match '/ical/:object/:method', to: 'i_cal#object', via: :get
 end
