ab53bbc3a7981a02d47352706949f5f88cc58668
Martin Edenhofer <me@edenhofer.de>
2015-07-01 02:05:13 +0200

    Merged some migrations.

--- db/migrate/20141119000001_update_setting2.rb
+++ db/migrate/20141119000001_update_setting2.rb
@@ -18,6 +18,15 @@ class UpdateSetting2 < ActiveRecord::Migration
       state: 'logo.svg',
       frontend: true
     )
+    Setting.create_if_not_exists(
+      title: 'Online Service',
+      name: 'system_online_service',
+      area: 'Core',
+      description: 'Defines if application is used as online service.',
+      options: {},
+      state: false,
+      frontend: true
+    )
   end
 
   def down
--- db/migrate/20150701000001_add_cleanup.rb
+++ db/migrate/20150701000001_add_cleanup.rb
@@ -20,5 +20,14 @@ class AddCleanup < ActiveRecord::Migration
       updated_by_id: 1,
       created_by_id: 1,
     )
+    Scheduler.create_or_update(
+      name: 'Delete old entries.',
+      method: 'RecentView.cleanup',
+      period: 1.day,
+      prio: 2,
+      active: true,
+      updated_by_id: 1,
+      created_by_id: 1,
+    )
   end
 end
