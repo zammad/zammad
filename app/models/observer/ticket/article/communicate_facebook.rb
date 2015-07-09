be8021388859c08cf4a7faf79770da9608cb049b
Thorsten Eckel <te@znuny.com>
2015-07-09 16:56:01 +0200

    Initial version of facebook channel connector.

--- app/models/observer/ticket/article/communicate_facebook.rb
+++ app/models/observer/ticket/article/communicate_facebook.rb
@@ -1,5 +1,7 @@
 # Copyright (C) 2012-2014 Zammad Foundation, http://zammad-foundation.org/
 
+require 'channel/facebook'
+
 class Observer::Ticket::Article::CommunicateFacebook < ActiveRecord::Observer
   observe 'ticket::_article'
 
@@ -13,17 +15,18 @@ class Observer::Ticket::Article::CommunicateFacebook < ActiveRecord::Observer
     return 1 if sender.nil?
     return 1 if sender['name'] == 'Customer'
 
-    # only apply on emails
+    # only apply for facebook
     type = Ticket::Article::Type.lookup( id: record.type_id )
-    return if type['name'] != 'facebook'
+    return if type['name'] !~ /\Afacebook/
 
-    a = Channel::Facebook.new
-    a.send(
-      {
-        from: 'me@znuny.com',
-        to: 'medenhofer',
-        body: record.body
-      }
-    )
+    facebook = Channel::Facebook.new
+    post     = facebook.send({
+                               type:        type['name'],
+                               to:          record.to,
+                               body:        record.body,
+                               in_reply_to: record.in_reply_to
+                             })
+    record.message_id = post['id']
+    record.save
   end
 end
