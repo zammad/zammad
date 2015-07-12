4f1821daf25b8e88dd2233a89ed4df741802ef6f
Martin Edenhofer <me@edenhofer.de>
2015-07-12 04:40:11 +0200

    Split into 2 separate migrations.

--- db/migrate/20150712000002_update_setting2.rb
+++ db/migrate/20150712000002_update_setting2.rb
@@ -5,27 +5,5 @@ class UpdateSetting2 < ActiveRecord::Migration
     add_column :settings, :preferences, :string, limit: 2000, null: true
     Setting.reset_column_information
 
-    # update settings
-    %w(product_name ticket_hook chat).each {|setting_name|
-      setting = Setting.find_by(name: setting_name)
-      next if !setting
-      setting.preferences[:render] = true
-      setting.save
-    }
-    %w(product_name).each {|setting_name|
-      setting = Setting.find_by(name: setting_name)
-      next if !setting
-      setting.preferences[:session_check] = true
-      setting.save
-    }
-    setting = Setting.find_by(name: 'product_name')
-    setting.preferences[:prio] = 1
-    setting.save
-    setting = Setting.find_by(name: 'organization')
-    setting.preferences[:prio] = 2
-    setting.save
-    setting = Setting.find_by(name: 'product_logo')
-    setting.preferences[:prio] = 3
-    setting.save
   end
 end
---
+++ db/migrate/20150712000003_update_setting3.rb
@@ -0,0 +1,27 @@
+class UpdateSetting3 < ActiveRecord::Migration
+  def up
+
+    # update settings
+    %w(product_name ticket_hook chat).each {|setting_name|
+      setting = Setting.find_by(name: setting_name)
+      next if !setting
+      setting.preferences[:render] = true
+      setting.save
+    }
+    %w(product_name).each {|setting_name|
+      setting = Setting.find_by(name: setting_name)
+      next if !setting
+      setting.preferences[:session_check] = true
+      setting.save
+    }
+    setting = Setting.find_by(name: 'product_name')
+    setting.preferences[:prio] = 1
+    setting.save
+    setting = Setting.find_by(name: 'organization')
+    setting.preferences[:prio] = 2
+    setting.save
+    setting = Setting.find_by(name: 'product_logo')
+    setting.preferences[:prio] = 3
+    setting.save
+  end
+end
