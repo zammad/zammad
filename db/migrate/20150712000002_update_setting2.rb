67a8ee52d36127825964ab145756f4b6d7cdf1f7
Martin Edenhofer <me@edenhofer.de>
2015-07-12 04:16:14 +0200

    Improved migration.

--- db/migrate/20150712000002_update_setting2.rb
+++ db/migrate/20150712000002_update_setting2.rb
@@ -3,6 +3,7 @@ class UpdateSetting2 < ActiveRecord::Migration
 
     # add preferences
     add_column :settings, :preferences, :string, limit: 2000, null: true
+    Setting.reset_column_information
 
     # update settings
     %w(product_name ticket_hook chat).each {|setting_name|
