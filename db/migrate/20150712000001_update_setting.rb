9c0a535d2863602df2629ad662837afcfb446ed8
Martin Edenhofer <me@edenhofer.de>
2015-07-12 03:37:28 +0200

    Fixed migration on fresh setups.

--- db/migrate/20150712000001_update_setting.rb
+++ db/migrate/20150712000001_update_setting.rb
@@ -2,6 +2,7 @@ class UpdateSetting < ActiveRecord::Migration
   def up
     %w(product_name product_logo organization).each {|setting_name|
       setting = Setting.find_by(name: setting_name)
+      next if !setting
       setting.area = 'System::Branding'
       setting.save
     }
