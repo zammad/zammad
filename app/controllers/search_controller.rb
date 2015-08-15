4655039cd104c099ebaf05620dd1cbafd45dc657
Martin Edenhofer <me@edenhofer.de>
2015-08-16 01:27:11 +0200

    Merged /api/v1/search and /api/v1/search/object controller methods. Added controller tests for permission handling.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,13 @@
 class SearchController < ApplicationController
   before_action :authentication_check
 
+  # GET|POST /api/v1/search
   # GET|POST /api/v1/search/:objects
 
   def search_generic
 
-    # enable search only for agents and admins
-    if !current_user.role?(Z_ROLENAME_AGENT) && !current_user.role?(Z_ROLENAME_ADMIN)
+    # enable search only for users with valid session
+    if !current_user
       response_access_deny
       return true
     end
@@ -19,9 +20,15 @@ class SearchController < ApplicationController
 
     # convert objects string into array of class names
     # e.g. user-ticket-another_object = %w( User Ticket AnotherObject )
-    objects = params[:objects].split('-').map(&:camelize)
+    if !params[:objects]
+      objects_all = %w( Ticket User Organization )
+    else
+      objects_all = params[:objects].split('-').map(&:camelize)
+    end
+    objects = objects_all.clone
+puts "OBJECTS: #{objects.inspect}"
     search_tickets = objects.delete('Ticket')
-
+puts "OBJECTS_a: #{objects_all.inspect}/#{search_tickets.inspect}"
     # try search index backend
     assets = {}
     result = []
@@ -53,7 +60,7 @@ class SearchController < ApplicationController
     else
 
       # do query
-      objects.each { |object|
+      objects_all.each { |object|
 
         found_objects = object.constantize.search(
           query:        query,
@@ -72,86 +79,6 @@ class SearchController < ApplicationController
       }
     end
 
-    render json: {
-      assets: assets,
-      result: result,
-    }
-  end
-
-  # GET /api/v1/search
-  def search
-
-    # get params
-    query = params[:term]
-    limit = params[:limit] || 10
-
-    assets  = {}
-    result  = []
-    objects = %w( Ticket User Organization )
-    if SearchIndexBackend.enabled?
-
-      # to ticket search in serparate call
-      objects.delete('Ticket')
-
-      # to query search index backend (excluse tickets here, see below)
-      found_objects = {}
-      items = SearchIndexBackend.search( query, limit, objects )
-      items.each { |item|
-        require item[:type].to_filename
-        record = Kernel.const_get( item[:type] ).find( item[:id] )
-        assets = record.assets(assets)
-
-        found_objects[ item[:type] ] ||= []
-        found_objects[ item[:type] ].push item[:id]
-      }
-
-      # do ticket query by Ticket class to handle ticket permissions
-      tickets = Ticket.search(
-        query: query,
-        limit: limit,
-        current_user: current_user,
-      )
-      tickets.each do |ticket|
-        found_objects[ 'Ticket' ] ||= []
-        found_objects[ 'Ticket' ].push ticket.id
-      end
-
-      # generate whole result
-      found_objects.each { |object, object_ids|
-
-        data = {
-          name: object,
-          ids:  object_ids,
-        }
-        result.push data
-      }
-    else
-
-      objects.each { |object|
-
-        found_objects = object.constantize.search(
-          query:        query,
-          limit:        limit,
-          current_user: current_user,
-        )
-
-        object_ids = []
-        found_objects.each do |found_object|
-          object_ids.push found_object.id
-          assets = found_object.assets(assets)
-        end
-
-        next if object_ids.empty?
-
-        data = {
-          name: object,
-          ids:  object_ids,
-        }
-        result.push data
-      }
-    end
-
-    # return result
     render json: {
       assets: assets,
       result: result,
