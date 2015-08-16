ecb0ace02d2fbcd27f89f13765400dddc5d4b3dc
Martin Edenhofer <me@edenhofer.de>
2015-08-16 11:24:15 +0200

    Check if models_config exists to support migrations of existing systems.

--- config/initializers/models_searchable.rb
+++ config/initializers/models_searchable.rb
@@ -2,7 +2,7 @@
 if ActiveRecord::Base.connection.tables.include?('settings')
   models_current = Models.searchable.map(&:to_s)
   models_config = Setting.get('models_searchable')
-  if models_current != models_config
+  if models_config && models_current != models_config
     Setting.set('models_searchable', models_current)
   end
 end
