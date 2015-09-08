93559feecf5e1b5ccf3ba1b208577bcf659a7bcc
Martin Edenhofer <me@edenhofer.de>
2015-09-08 14:12:14 +0200

    Fixed calculation of average state.

--- lib/stats/ticket_in_process.rb
+++ lib/stats/ticket_in_process.rb
@@ -65,7 +65,7 @@ class Stats::TicketInProcess
       return result
     end
 
-    in_percent = ( result[:used_for_average].to_f / (result[:total].to_f/100) ).round(1)
+    in_percent = ( result[:used_for_average].to_f / (result[:average_per_agent].to_f/100) ).round(1)
     if in_percent >= 90
       result[:state] = 'supergood'
     elsif in_percent >= 65
