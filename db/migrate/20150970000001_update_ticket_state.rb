99b64347e2d8c2fbb773a0d7ea8cc887f4705321
Martin Edenhofer <me@edenhofer.de>
2015-09-10 22:06:57 +0200

    Fixed screech setup.

--- db/migrate/20150970000001_update_ticket_state.rb
+++ db/migrate/20150970000001_update_ticket_state.rb
@@ -2,6 +2,7 @@ class UpdateTicketState < ActiveRecord::Migration
   def up
     add_column :ticket_states, :ignore_escalation, :boolean, null: false, default: false
 
+    return if !Ticket::State.first
     Ticket::State.create_or_update( id: 3, name: 'pending reminder', state_type_id: Ticket::StateType.find_by(name: 'pending reminder').id, ignore_escalation: true )
     Ticket::State.create_or_update( id: 4, name: 'closed', state_type_id: Ticket::StateType.find_by(name: 'closed').id, ignore_escalation: true )
     Ticket::State.create_or_update( id: 5, name: 'merged', state_type_id: Ticket::StateType.find_by(name: 'merged').id, ignore_escalation: true )
