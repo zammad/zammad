dcf78780da73a269083e71cf5f84cb7626bd6ade
Martin Edenhofer <me@edenhofer.de>
2015-09-26 01:01:35 +0200

    Improved error handling.

--- config/initializers/models_searchable.rb
+++ config/initializers/models_searchable.rb
@@ -3,7 +3,7 @@ if ActiveRecord::Base.connection.tables.include?('settings')
   if Setting.columns_hash.key?('state_current') # TODO: remove me later
     models_current = Models.searchable.map(&:to_s)
     models_config = Setting.get('models_searchable')
-    if models_config && models_current != models_config
+    if models_current != models_config
       Setting.set('models_searchable', models_current)
     end
   end
