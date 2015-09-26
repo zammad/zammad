bef4c843c8434cfcf2576799b0c593efeae6340f
Martin Edenhofer <me@edenhofer.de>
2015-09-26 08:59:05 +0200

    Applied rubocop.

--- db/migrate/20150978000001_update_settings6.rb
+++ db/migrate/20150978000001_update_settings6.rb
@@ -1,7 +1,6 @@
 class UpdateSettings6 < ActiveRecord::Migration
   def up
-    if Setting.column_names.include?('state')
-      rename_column :settings, :state, :state_current
-    end
+    return if !Setting.column_names.include?('state')
+    rename_column :settings, :state, :state_current
   end
 end
