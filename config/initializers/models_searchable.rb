f1ad842aee5997052e99247c0af1931f2b72174d
Martin Edenhofer <me@edenhofer.de>
2015-09-26 17:59:58 +0200

    Fixed initial setup.

--- config/initializers/models_searchable.rb
+++ config/initializers/models_searchable.rb
@@ -3,7 +3,8 @@ if ActiveRecord::Base.connection.tables.include?('settings')
   if Setting.columns_hash.key?('state_current') # TODO: remove me later
     models_current = Models.searchable.map(&:to_s)
     models_config = Setting.get('models_searchable')
-    if models_current != models_config
+    setting = Setting.find_by(name: 'models_searchable')
+    if setting && models_current != models_config
       Setting.set('models_searchable', models_current)
     end
   end
