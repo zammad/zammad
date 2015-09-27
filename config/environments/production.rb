53ff4fc0db6abfc808aa6a8710e04bb4ccab4e1d
Martin Edenhofer <me@edenhofer.de>
2015-09-27 22:08:17 +0200

    Removed not longer needed option.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
