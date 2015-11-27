3c0e20bc0604aae4f9b333db0003f9805b4b26b3
Martin Edenhofer <me@edenhofer.de>
2015-11-27 08:39:43 +0100

    To process browser tests with normal password.

--- script/build/test_startup.sh
+++ script/build/test_startup.sh
@@ -3,6 +3,7 @@ RAILS_ENV=$1
 APP_PORT=$2
 WS_PORT=$3
 
+rails r "Setting.set('developer_mode', true)"
 rails r "Setting.set('websocket_port', '#{WS_PORT}')"
 
 pumactl start --pidfile tmp/pids/puma.pid -d -p $APP_PORT -e $RAILS_ENV
