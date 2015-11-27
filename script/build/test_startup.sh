12e38ea4d1647fc1d1d71251808e30e9956e0190
Martin Edenhofer <me@edenhofer.de>
2015-11-27 09:28:14 +0100

    Fixed typo.

--- script/build/test_startup.sh
+++ script/build/test_startup.sh
@@ -4,7 +4,7 @@ APP_PORT=$2
 WS_PORT=$3
 
 rails r "Setting.set('developer_mode', true)"
-rails r "Setting.set('websocket_port', '#{WS_PORT}')"
+rails r "Setting.set('websocket_port', '$WS_PORT')"
 
 pumactl start --pidfile tmp/pids/puma.pid -d -p $APP_PORT -e $RAILS_ENV
 script/websocket-server.rb start -d -p $WS_PORT
