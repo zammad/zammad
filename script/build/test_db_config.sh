e751f975b3d5247f4cb1effe11c235a84a27c6ea
Martin Edenhofer <me@edenhofer.de>
2015-12-01 19:43:54 +0100

    Improved creation of config/database.yml

--- script/build/test_db_config.sh
+++ script/build/test_db_config.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
-DBENV=$1
-DBNAME=$2
+
+if [ "$RNAME" == '' ]; then
+  echo "ERROR: Env RNAME is missing!"
+  exit 1
+fi
+
+DBNAME=$RNAME
 DBFILE=config/database.yml
+DB_PRODUCTION="$DBNAME_production"
+DB_TEST="$DBNAME_test"
+DB_DEVELOPMENT="$DBNAME_development"
 
-echo "Creating $DBFILE for tests with dbname $DBNAME"
+echo "Creating $DBFILE for tests with $DBNAME"
 
-echo "$DBENV:" >> $DBFILE
+echo "production:" > $DBFILE
 echo "  adapter: mysql2" >> $DBFILE
-echo "  database: $DBNAME" >> $DBFILE
+echo "  database: $DB_PRODUCTION" >> $DBFILE
+echo "  pool: 50" >> $DBFILE
+echo "  timeout: 5000" >> $DBFILE
+echo "  encoding: utf8" >> $DBFILE
+echo "  username: root" >> $DBFILE
+echo "  password:" >> $DBFILE
+echo "test:" >> $DBFILE
+echo "  adapter: mysql2" >> $DBFILE
+echo "  database: $DB_TEST" >> $DBFILE
+echo "  pool: 50" >> $DBFILE
+echo "  timeout: 5000" >> $DBFILE
+echo "  encoding: utf8" >> $DBFILE
+echo "  username: root" >> $DBFILE
+echo "  password:" >> $DBFILE
+echo "development:" >> $DBFILE
+echo "  adapter: mysql2" >> $DBFILE
+echo "  database: $DB_DEVELOPMENT" >> $DBFILE
 echo "  pool: 50" >> $DBFILE
 echo "  timeout: 5000" >> $DBFILE
 echo "  encoding: utf8" >> $DBFILE
