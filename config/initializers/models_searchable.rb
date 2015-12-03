d0964b7960a2912cf70e9a377036899c182e0205
Martin Edenhofer <me@edenhofer.de>
2015-12-03 23:16:04 +0100

    Revert "test failure reason"
    This reverts commit 254cb5d11d2f5fc6e694d2d25e595e0c16a6c3d5.

--- config/initializers/models_searchable.rb
+++ config/initializers/models_searchable.rb
@@ -1,11 +1,11 @@
 # update settings for searchable models
-#if ActiveRecord::Base.connection.tables.include?('settings')
-#  if Setting.columns_hash.key?('state_current') # TODO: remove me later
-#    models_current = Models.searchable.map(&:to_s)
-#    models_config = Setting.get('models_searchable')
-#    setting = Setting.find_by(name: 'models_searchable')
-#    if setting && models_current != models_config
-#      Setting.set('models_searchable', models_current)
-#    end
-#  end
-#end
+if ActiveRecord::Base.connection.tables.include?('settings')
+  if Setting.columns_hash.key?('state_current') # TODO: remove me later
+    models_current = Models.searchable.map(&:to_s)
+    models_config = Setting.get('models_searchable')
+    setting = Setting.find_by(name: 'models_searchable')
+    if setting && models_current != models_config
+      Setting.set('models_searchable', models_current)
+    end
+  end
+end
