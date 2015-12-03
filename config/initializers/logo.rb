4c87a8e3287b6b1bcaab78ad886db429bec022c7
Roy Kaldung <roy@kaldung.com>
2015-12-03 16:29:29 +0100

    test failure reason

--- config/initializers/logo.rb
+++ config/initializers/logo.rb
@@ -1,6 +1,6 @@
 # sync logo to fs / only if settings already exists
-if ActiveRecord::Base.connection.tables.include?('settings')
-  if Setting.column_names.include?('state_current')
-    StaticAssets.sync
-  end
-end
+#if ActiveRecord::Base.connection.tables.include?('settings')
+#  if Setting.column_names.include?('state_current')
+#    StaticAssets.sync
+#  end
+#end
