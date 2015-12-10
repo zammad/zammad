9c28ca285d522bbb5093ff226cc6025a07bbdb2c
Martin Edenhofer <me@edenhofer.de>
2015-12-10 11:35:00 +0100

    Added cleanups for chats.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3269,6 +3269,24 @@ Scheduler.create_or_update(
   updated_by_id: 1,
   created_by_id: 1,
 )
+Scheduler.create_or_update(
+  name: 'Closed chat sessions where participients are offline.',
+  method: 'Chat.cleanup_close',
+  period: 60 * 15,
+  prio: 2,
+  active: true,
+  updated_by_id: 1,
+  created_by_id: 1,
+)
+Scheduler.create_or_update(
+  name: 'Cleanup closed sessions.',
+  method: 'Chat.cleanup',
+  period: 5.days,
+  prio: 2,
+  active: true,
+  updated_by_id: 1,
+  created_by_id: 1,
+)
 
 # install locales and translations
 Locale.create_if_not_exists(
