be1cde6efff58ad6fd26639fc4431b694ca12363
Martin Edenhofer <me@edenhofer.de>
2015-12-31 09:51:06 +0100

    Set fqdn for each env.

--- script/build/test_startup.sh
+++ script/build/test_startup.sh
@@ -7,6 +7,7 @@ export ZAMMAD_SETTING_TTL=15
 
 rails r "Setting.set('developer_mode', true)"
 rails r "Setting.set('websocket_port', '$WS_PORT')"
+rails r "Setting.set('fqdn', '$IP:$BROWSER_PORT')"
 
 pumactl start --pidfile tmp/pids/puma.pid -d -p $APP_PORT -e $RAILS_ENV
 script/websocket-server.rb start -d -p $WS_PORT
