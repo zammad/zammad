db9076809ebd9d2258db45a1840b119667332136
Thorsten Eckel <te@znuny.com>
2016-01-13 13:07:23 +0100

    Current state of OTRS import browser test.

--- test/integration/otrs_import_browser_test.rb
+++ test/integration/otrs_import_browser_test.rb
@@ -3,8 +3,27 @@ require 'browser_test_helper'
 
 class OtrsImportBrowserTest < TestCase
   def test_import
+
+    if !ENV['IMPORT_BT_OTRS_ENDPOINT']
+      fail "ERROR: Need IMPORT_BT_OTRS_ENDPOINT - hint IMPORT_BT_OTRS_ENDPOINT='http://vz305.demo.znuny.com/otrs/public.pl?Action=ZammadMigrator'"
+    end
+    if !ENV['IMPORT_BT_OTRS_ENDPOINT_KEY']
+      fail "ERROR: Need IMPORT_BT_OTRS_ENDPOINT_KEY - hint IMPORT_BT_OTRS_ENDPOINT_KEY='01234567899876543210'"
+    end
+
+    import_url = "#{ENV['IMPORT_BT_OTRS_ENDPOINT']};Key=#{ENV['IMPORT_BT_OTRS_ENDPOINT_KEY']}"
+
     @browser = browser_instance
     location(url: browser_url)
+
+    click(css: 'a[href="#import"]')
+
+    click(css: 'a[href="#import/otrs"]')
+
+    set(
+      css:   '#otrs-link',
+      value: import_url
+    )
 
     watch_for(
       css: 'body',
