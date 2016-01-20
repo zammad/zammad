a386f23555953ec56b758cab85e6f2b53e157d8b
Martin Edenhofer <me@edenhofer.de>
2016-01-20 20:38:38 +0100

    Recompile of chat widget.

--- public/assets/chat/chat.js
+++ public/assets/chat/chat.js
@@ -1,151 +1,3 @@
-if (!window.zammadChatTemplates) {
-  window.zammadChatTemplates = {};
-}
-window.zammadChatTemplates["agent"] = function (__obj) {
-  if (!__obj) __obj = {};
-  var __out = [], __capture = function(callback) {
-    var out = __out, result;
-    __out = [];
-    callback.call(this);
-    result = __out.join('');
-    __out = out;
-    return __safe(result);
-  }, __sanitize = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else if (typeof value !== 'undefined' && value != null) {
-      return __escape(value);
-    } else {
-      return '';
-    }
-  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
-  __safe = __obj.safe = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else {
-      if (!(typeof value !== 'undefined' && value != null)) value = '';
-      var result = new String(value);
-      result.ecoSafe = true;
-      return result;
-    }
-  };
-  if (!__escape) {
-    __escape = __obj.escape = function(value) {
-      return ('' + value)
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;');
-    };
-  }
-  (function() {
-    (function() {
-      if (this.agent.avatar) {
-        __out.push('\n<img class="zammad-chat-agent-avatar" src="');
-        __out.push(__sanitize(this.agent.avatar));
-        __out.push('">\n');
-      }
-
-      __out.push('\n<span class="zammad-chat-agent-sentence">\n  <span class="zammad-chat-agent-name">');
-
-      __out.push(__sanitize(this.agent.name));
-
-      __out.push('</span>\n</span>');
-
-    }).call(this);
-
-  }).call(__obj);
-  __obj.safe = __objSafe, __obj.escape = __escape;
-  return __out.join('');
-};
-
-/*!
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <jevin9@gmail.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return. Jevin O. Sewaruth
- * ----------------------------------------------------------------------------
- *
- * Autogrow Textarea Plugin Version v3.0
- * http://www.technoreply.com/autogrow-textarea-plugin-3-0
- *
- * THIS PLUGIN IS DELIVERD ON A PAY WHAT YOU WHANT BASIS. IF THE PLUGIN WAS USEFUL TO YOU, PLEASE CONSIDER BUYING THE PLUGIN HERE :
- * https://sites.fastspring.com/technoreply/instant/autogrowtextareaplugin
- *
- * Date: October 15, 2012
- *
- * Zammad modification: remove overflow:hidden when maximum height is reached
- *
- */
-
-jQuery.fn.autoGrow = function(options) {
-  return this.each(function() {
-    var settings = jQuery.extend({
-      extraLine: true,
-    }, options);
-
-    var createMirror = function(textarea) {
-      jQuery(textarea).after('<div class="autogrow-textarea-mirror"></div>');
-      return jQuery(textarea).next('.autogrow-textarea-mirror')[0];
-    }
-
-    var sendContentToMirror = function (textarea) {
-      mirror.innerHTML = String(textarea.value)
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/ /g, '&nbsp;')
-        .replace(/\n/g, '<br />') +
-        (settings.extraLine? '.<br/>.' : '')
-      ;
-
-      if (jQuery(textarea).height() != jQuery(mirror).height()) {
-        jQuery(textarea).height(jQuery(mirror).height());
-
-        var maxHeight = parseInt(jQuery(textarea).css('max-height'), 10);
-        var overflow = jQuery(mirror).height() > maxHeight ? '' : 'hidden'
-        jQuery(textarea).css('overflow', overflow);
-      }
-    }
-
-    var growTextarea = function () {
-      sendContentToMirror(this);
-    }
-
-    // Create a mirror
-    var mirror = createMirror(this);
-
-    // Style the mirror
-    mirror.style.display = 'none';
-    mirror.style.wordWrap = 'break-word';
-    mirror.style.whiteSpace = 'normal';
-    mirror.style.padding = jQuery(this).css('paddingTop') + ' ' +
-      jQuery(this).css('paddingRight') + ' ' +
-      jQuery(this).css('paddingBottom') + ' ' +
-      jQuery(this).css('paddingLeft');
-
-    mirror.style.width = jQuery(this).css('width');
-    mirror.style.fontFamily = jQuery(this).css('font-family');
-    mirror.style.fontSize = jQuery(this).css('font-size');
-    mirror.style.lineHeight = jQuery(this).css('line-height');
-
-    console.log(mirror.style.lineHeight);
-
-    // Style the textarea
-    this.style.overflow = "hidden";
-    this.style.minHeight = this.rows+"em";
-
-    // Bind the textarea's event
-    this.onkeyup = growTextarea;
-
-    // Fire the event for text already present
-    sendContentToMirror(this);
-
-  });
-};
 var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   slice = [].slice,
   extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
@@ -1223,6 +1075,152 @@ var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); 
 if (!window.zammadChatTemplates) {
   window.zammadChatTemplates = {};
 }
+window.zammadChatTemplates["agent"] = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+      if (this.agent.avatar) {
+        __out.push('\n<img class="zammad-chat-agent-avatar" src="');
+        __out.push(__sanitize(this.agent.avatar));
+        __out.push('">\n');
+      }
+    
+      __out.push('\n<span class="zammad-chat-agent-sentence">\n  <span class="zammad-chat-agent-name">');
+    
+      __out.push(__sanitize(this.agent.name));
+    
+      __out.push('</span>\n</span>');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+};
+
+/*!
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <jevin9@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Jevin O. Sewaruth
+ * ----------------------------------------------------------------------------
+ *
+ * Autogrow Textarea Plugin Version v3.0
+ * http://www.technoreply.com/autogrow-textarea-plugin-3-0
+ * 
+ * THIS PLUGIN IS DELIVERD ON A PAY WHAT YOU WHANT BASIS. IF THE PLUGIN WAS USEFUL TO YOU, PLEASE CONSIDER BUYING THE PLUGIN HERE :
+ * https://sites.fastspring.com/technoreply/instant/autogrowtextareaplugin
+ *
+ * Date: October 15, 2012
+ *
+ * Zammad modification: remove overflow:hidden when maximum height is reached
+ *
+ */
+
+jQuery.fn.autoGrow = function(options) {
+  return this.each(function() {
+    var settings = jQuery.extend({
+      extraLine: true,
+    }, options);
+
+    var createMirror = function(textarea) {
+      jQuery(textarea).after('<div class="autogrow-textarea-mirror"></div>');
+      return jQuery(textarea).next('.autogrow-textarea-mirror')[0];
+    }
+
+    var sendContentToMirror = function (textarea) {
+      mirror.innerHTML = String(textarea.value)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/ /g, '&nbsp;')
+        .replace(/\n/g, '<br />') +
+        (settings.extraLine? '.<br/>.' : '')
+      ;
+
+      if (jQuery(textarea).height() != jQuery(mirror).height()) {
+        jQuery(textarea).height(jQuery(mirror).height());
+
+        var maxHeight = parseInt(jQuery(textarea).css('max-height'), 10);
+        var overflow = jQuery(mirror).height() > maxHeight ? '' : 'hidden'
+        jQuery(textarea).css('overflow', overflow);
+      }
+    }
+
+    var growTextarea = function () {
+      sendContentToMirror(this);
+    }
+
+    // Create a mirror
+    var mirror = createMirror(this);
+    
+    // Style the mirror
+    mirror.style.display = 'none';
+    mirror.style.wordWrap = 'break-word';
+    mirror.style.whiteSpace = 'normal';
+    mirror.style.padding = jQuery(this).css('paddingTop') + ' ' + 
+      jQuery(this).css('paddingRight') + ' ' + 
+      jQuery(this).css('paddingBottom') + ' ' + 
+      jQuery(this).css('paddingLeft');
+      
+    mirror.style.width = jQuery(this).css('width');
+    mirror.style.fontFamily = jQuery(this).css('font-family');
+    mirror.style.fontSize = jQuery(this).css('font-size');
+    mirror.style.lineHeight = jQuery(this).css('line-height');
+
+    // Style the textarea
+    this.style.overflow = "hidden";
+    this.style.minHeight = this.rows+"em";
+
+    // Bind the textarea's event
+    this.onkeyup = growTextarea;
+
+    // Fire the event for text already present
+    sendContentToMirror(this);
+
+  });
+};
+if (!window.zammadChatTemplates) {
+  window.zammadChatTemplates = {};
+}
 window.zammadChatTemplates["chat"] = function (__obj) {
   if (!__obj) __obj = {};
   var __out = [], __capture = function(callback) {
@@ -1263,45 +1261,45 @@ window.zammadChatTemplates["chat"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat');
-
+    
       if (this.flat) {
         __out.push(__sanitize(' zammad-chat--flat'));
       }
-
+    
       __out.push('"');
-
+    
       if (this.fontSize) {
         __out.push(__sanitize(" style='font-size: " + this.fontSize + "'"));
       }
-
+    
       __out.push('>\n  <div class="zammad-chat-header js-chat-open"');
-
+    
       if (this.background) {
         __out.push(__sanitize(" style='background: " + this.background + "'"));
       }
-
+    
       __out.push('>\n    <div class="zammad-chat-header-controls">\n      <span class="zammad-chat-agent-status zammad-chat-is-hidden" data-status="online"></span>\n      <span class="zammad-chat-header-icon">\n        <svg class="zammad-chat-header-icon-open" viewBox="0 0 13 7"><path d="M10.807 7l1.4-1.428-5-4.9L6.5-.02l-.7.7-4.9 4.9 1.414 1.413L6.5 2.886 10.807 7z" fill-rule="evenodd"/></svg>\n        <svg class="zammad-chat-header-icon-close js-chat-close" viewBox="0 0 13 13"><path d="m2.241.12l-2.121 2.121 4.243 4.243-4.243 4.243 2.121 2.121 4.243-4.243 4.243 4.243 2.121-2.121-4.243-4.243 4.243-4.243-2.121-2.121-4.243 4.243-4.243-4.243" fill-rule="evenodd"/></svg>\n      </span>\n    </div>\n    <div class="zammad-chat-agent zammad-chat-is-hidden">\n    </div>\n    <div class="zammad-chat-welcome">\n      <svg class="zammad-chat-icon" viewBox="0 0 24 24"><path d="M2 5C2 4 3 3 4 3h16c1 0 2 1 2 2v10C22 16 21 17 20 17H4C3 17 2 16 2 15V5zM12 17l6 4v-4h-6z" fill-rule="evenodd"/></svg>\n      <span class="zammad-chat-welcome-text">');
-
+    
       __out.push(this.T(this.title));
-
+    
       __out.push('</span>\n    </div>\n  </div>\n  <div class="zammad-chat-body"></div>\n  <form class="zammad-chat-controls">\n    <textarea class="zammad-chat-input" rows="1" placeholder="');
-
+    
       __out.push(this.T('Compose your message...'));
-
+    
       __out.push('"></textarea>\n    <button type="submit" class="zammad-chat-button zammad-chat-send"');
-
+    
       if (this.background) {
         __out.push(__sanitize(" style='background: " + this.background + "'"));
       }
-
+    
       __out.push('>');
-
+    
       __out.push(this.T('Send'));
-
+    
       __out.push('</button>\n  </form>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1350,7 +1348,7 @@ window.zammadChatTemplates["customer_timeout"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-modal">\n  <div class="zammad-chat-modal-text">\n    ');
-
+    
       if (this.agent) {
         __out.push('\n      ');
         __out.push(this.T('Since you didn\'t respond in the last %s minutes your conversation with <strong>%s</strong> got closed.', this.delay, this.agent));
@@ -1360,21 +1358,21 @@ window.zammadChatTemplates["customer_timeout"] = function (__obj) {
         __out.push(this.T('Since you didn\'t respond in the last %s minutes your conversation got closed.', this.delay));
         __out.push('\n    ');
       }
-
+    
       __out.push('\n    <br>\n    <div class="zammad-chat-button js-restart"');
-
+    
       if (this.background) {
         __out.push(__sanitize(" style='background: " + this.background + "'"));
       }
-
+    
       __out.push('>');
-
+    
       __out.push(this.T('Start new conversation'));
-
+    
       __out.push('</div>\n  </div>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1423,13 +1421,13 @@ window.zammadChatTemplates["loader"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-modal">\n  <span class="zammad-chat-loading-animation">\n    <span class="zammad-chat-loading-circle"></span>\n    <span class="zammad-chat-loading-circle"></span>\n    <span class="zammad-chat-loading-circle"></span>\n  </span>\n  <span class="zammad-chat-modal-text">');
-
+    
       __out.push(this.T('Connecting'));
-
+    
       __out.push('</span>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1478,25 +1476,25 @@ window.zammadChatTemplates["message"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-message zammad-chat-message--');
-
+    
       __out.push(__sanitize(this.from));
-
+    
       __out.push(__sanitize(this.unreadClass));
-
+    
       __out.push('">\n  <span class="zammad-chat-message-body"');
-
+    
       if (this.background && this.from === 'customer') {
         __out.push(__sanitize(" style='background: " + this.background + "'"));
       }
-
+    
       __out.push('>');
-
+    
       __out.push(this.message);
-
+    
       __out.push('</span>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1545,13 +1543,13 @@ window.zammadChatTemplates["status"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-status">\n  <div class="zammad-chat-status-inner">\n    ');
-
+    
       __out.push(this.status);
-
+    
       __out.push('\n  </div>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1600,17 +1598,17 @@ window.zammadChatTemplates["timestamp"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-timestamp"><strong>');
-
+    
       __out.push(__sanitize(this.label));
-
+    
       __out.push('</strong> ');
-
+    
       __out.push(__sanitize(this.time));
-
+    
       __out.push('</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1659,9 +1657,9 @@ window.zammadChatTemplates["typingIndicator"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-message zammad-chat-message--typing zammad-chat-message--agent">\n  <span class="zammad-chat-message-body">\n    <span class="zammad-chat-loading-animation">\n      <span class="zammad-chat-loading-circle"></span>\n      <span class="zammad-chat-loading-circle"></span>\n      <span class="zammad-chat-loading-circle"></span>\n    </span>\n  </span>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1710,17 +1708,17 @@ window.zammadChatTemplates["waiting"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-modal">\n  <div class="zammad-chat-modal-text">\n    <span class="zammad-chat-loading-animation">\n      <span class="zammad-chat-loading-circle"></span>\n      <span class="zammad-chat-loading-circle"></span>\n      <span class="zammad-chat-loading-circle"></span>\n    </span>\n    ');
-
+    
       __out.push(this.T('All colleagues are busy.'));
-
+    
       __out.push('<br>\n    ');
-
+    
       __out.push(this.T('You are on waiting list position <strong>%s</strong>.', this.position));
-
+    
       __out.push('\n  </div>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
@@ -1769,23 +1767,23 @@ window.zammadChatTemplates["waiting_list_timeout"] = function (__obj) {
   (function() {
     (function() {
       __out.push('<div class="zammad-chat-modal">\n  <div class="zammad-chat-modal-text">\n    ');
-
+    
       __out.push(this.T('We are sorry, it takes longer as expected to get an empty slot. Please try again later or send us an email. Thank you!'));
-
+    
       __out.push('\n    <br>\n    <div class="zammad-chat-button js-restart"');
-
+    
       if (this.background) {
         __out.push(__sanitize(" style='background: " + this.background + "'"));
       }
-
+    
       __out.push('>');
-
+    
       __out.push(this.T('Start new conversation'));
-
+    
       __out.push('</div>\n  </div>\n</div>');
-
+    
     }).call(this);
-
+    
   }).call(__obj);
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
