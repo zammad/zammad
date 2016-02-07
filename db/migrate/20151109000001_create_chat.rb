ea640474a3004d280775d702a94e9b5042687a77
Martin Edenhofer <me@edenhofer.de>
2016-02-07 16:17:45 +0100

    Merged migrations.

--- db/migrate/20151109000001_create_chat.rb
+++ db/migrate/20151109000001_create_chat.rb
@@ -77,6 +77,25 @@ class CreateChat < ActiveRecord::Migration
       created_by_id: 1,
     )
 
+    Scheduler.create_or_update(
+      name: 'Closed chat sessions where participients are offline.',
+      method: 'Chat.cleanup_close',
+      period: 60 * 15,
+      prio: 2,
+      active: true,
+      updated_by_id: 1,
+      created_by_id: 1,
+    )
+    Scheduler.create_or_update(
+      name: 'Cleanup closed sessions.',
+      method: 'Chat.cleanup',
+      period: 5.days,
+      prio: 2,
+      active: true,
+      updated_by_id: 1,
+      created_by_id: 1,
+    )
+
   end
 
   def down
--- db/migrate/20151213000001_update_ticket_preferences.rb
+++
@@ -1,5 +0,0 @@
-class UpdateTicketPreferences < ActiveRecord::Migration
-  def up
-    add_column :tickets, :preferences, :text, limit: 500.kilobytes + 1, null: true
-  end
-end
