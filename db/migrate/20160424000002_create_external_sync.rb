5812515fe5fc55a5d878844a57979394f3057cb7
Martin Edenhofer <me@edenhofer.de>
2016-04-26 01:14:37 +0200

    Fixed: Index name 'index_external_syncs_on_source_and_source_id_and_object_and_o_id' on table 'external_syncs' is too long; the limit is 63 characters.

--- db/migrate/20160424000002_create_external_sync.rb
+++ db/migrate/20160424000002_create_external_sync.rb
@@ -9,7 +9,7 @@ class CreateExternalSync < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_index :external_syncs, [:source, :source_id], unique: true
-    add_index :external_syncs, [:source, :source_id, :object, :o_id]
+    add_index :external_syncs, [:source, :source_id, :object, :o_id], name: 'index_external_syncs_on_source_and_source_id_and_object_o_id'
     add_index :external_syncs, [:object, :o_id]
   end
 end
