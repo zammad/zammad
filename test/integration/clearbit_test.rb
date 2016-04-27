8cfc048cdbb0f22851575e295fd889f909067fd5
Martin Edenhofer <me@edenhofer.de>
2016-04-27 09:35:42 +0200

    Applied new live call result.

--- test/integration/clearbit_test.rb
+++ test/integration/clearbit_test.rb
@@ -270,7 +270,7 @@ class ClearbitTest < ActiveSupport::TestCase
     assert_equal('', customer6_lookup.lastname)
     assert_equal('', customer6_lookup.note)
     assert_equal('http://clearbit.com', customer6_lookup.web)
-    assert_equal('', customer6_lookup.address)
+    assert_equal('601 4th St #310, San Francisco, CA 94107, USA', customer6_lookup.address)
 
     organization6_lookup = Organization.find_by(name: 'Clearbit')
     assert(ExternalSync.find_by(source: 'clearbit', object: 'Organization', o_id: organization6_lookup.id))
