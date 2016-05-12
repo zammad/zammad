5e7468e8a8e17bfe9fe98931250472f911294bf4
Martin Edenhofer <me@edenhofer.de>
2016-05-12 11:26:11 +0200

    Improved code style.

--- lib/session_helper.rb
+++ lib/session_helper.rb
@@ -6,10 +6,10 @@ module SessionHelper
 
     # load collections to deliver from external files
     dir = File.expand_path('../../', __FILE__)
-    files = Dir.glob( "#{dir}/app/controllers/sessions/collection_*.rb" )
+    files = Dir.glob( "#{dir}/app/controllers/sessions/collection_*.rb")
     files.each { |file|
       load file
-      (default_collection, assets ) = ExtraCollection.session( default_collection, assets, user )
+      (default_collection, assets) = ExtraCollection.session(default_collection, assets, user)
     }
 
     [default_collection, assets]
@@ -28,15 +28,15 @@ module SessionHelper
   def self.cleanup_expired
 
     # delete temp. sessions
-    ActiveRecord::SessionStore::Session.where('persistent IS NULL AND updated_at < ?', Time.zone.now - 2.hours ).delete_all
+    ActiveRecord::SessionStore::Session.where('persistent IS NULL AND updated_at < ?', Time.zone.now - 2.hours).delete_all
 
     # web sessions not updated the last x days
-    ActiveRecord::SessionStore::Session.where('updated_at < ?', Time.zone.now - 60.days ).delete_all
+    ActiveRecord::SessionStore::Session.where('updated_at < ?', Time.zone.now - 60.days).delete_all
 
   end
 
   def self.get(id)
-    ActiveRecord::SessionStore::Session.find_by( id: id )
+    ActiveRecord::SessionStore::Session.find_by(id: id)
   end
 
   def self.list(limit = 10_000)
@@ -44,7 +44,7 @@ module SessionHelper
   end
 
   def self.destroy(id)
-    session = ActiveRecord::SessionStore::Session.find_by( id: id )
+    session = ActiveRecord::SessionStore::Session.find_by(id: id)
     return if !session
     session.destroy
   end
