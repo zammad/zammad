cbb751d0f30b3627caeb9b7d957f2862e884c30f
Martin Edenhofer <me@edenhofer.de>
2016-05-21 12:02:38 +0200

    Split core browser tests into 5 slices.

--- script/build/test_slice_tests.sh
+++ script/build/test_slice_tests.sh
@@ -12,9 +12,19 @@ if [ "$LEVEL" == '1' ]; then
   rm test/browser/admin_channel_email_test.rb
   rm test/browser/admin_overview_test.rb
   # test/browser/agent_navigation_and_title_test.rb
-  rm test/browser/agent_organization_profile_test.rb
-  rm test/browser/agent_ticket_*.rb
-  # test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_ticket_actions_level0_test.rb
+  rm test/browser/agent_ticket_actions_level1_test.rb
+  rm test/browser/agent_ticket_actions_level2_test.rb
+  rm test/browser/agent_ticket_actions_level3_test.rb
+  rm test/browser/agent_ticket_actions_level4_test.rb
+  rm test/browser/agent_ticket_actions_level5_test.rb
+  rm test/browser/agent_ticket_actions_level6_test.rb
+  rm test/browser/agent_ticket_actions_level7_test.rb
+  rm test/browser/agent_ticket_actions_level8_test.rb
+  rm test/browser/agent_ticket_actions_level9_test.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_user_manage_test.rb
   rm test/browser/agent_user_profile_test.rb
   # test/browser/auth_test.rb
   rm test/browser/chat_test.rb
@@ -35,7 +45,7 @@ if [ "$LEVEL" == '1' ]; then
 elif [ "$LEVEL" == '2' ]; then
   echo "slicing level 2"
 
-  # only ticket action 1/2
+  # only ticket action 1/3
   rm test/browser/aab_basic_urls_test.rb
   rm test/browser/aab_unit_test.rb
   rm test/browser/aac_basic_richtext_test.rb
@@ -44,9 +54,20 @@ elif [ "$LEVEL" == '2' ]; then
   rm test/browser/admin_overview_test.rb
   rm test/browser/agent_navigation_and_title_test.rb
   rm test/browser/agent_organization_profile_test.rb
-  # test/browser/agent_ticket_actions_*.rb
-  rm test/browser/agent_ticket_overview_*.rb
-  rm test/browser/agent_user_*.rb
+  # test/browser/agent_ticket_actions_level0.rb
+  # test/browser/agent_ticket_actions_level1.rb
+  # test/browser/agent_ticket_actions_level2.rb
+  # test/browser/agent_ticket_actions_level3.rb
+  # test/browser/agent_ticket_actions_level4.rb
+  rm test/browser/agent_ticket_actions_level5.rb
+  rm test/browser/agent_ticket_actions_level6.rb
+  rm test/browser/agent_ticket_actions_level7.rb
+  rm test/browser/agent_ticket_actions_level8.rb
+  rm test/browser/agent_ticket_actions_level9.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
   rm test/browser/auth_test.rb
   rm test/browser/chat_test.rb
   rm test/browser/customer_ticket_create_test.rb
@@ -66,7 +87,7 @@ elif [ "$LEVEL" == '2' ]; then
 elif [ "$LEVEL" == '3' ]; then
   echo "slicing level 3"
 
-  # only ticket action 2/2
+  # only ticket action 2/3
   rm test/browser/aab_basic_urls_test.rb
   rm test/browser/aab_unit_test.rb
   rm test/browser/aac_basic_richtext_test.rb
@@ -75,12 +96,23 @@ elif [ "$LEVEL" == '3' ]; then
   rm test/browser/admin_overview_test.rb
   rm test/browser/agent_navigation_and_title_test.rb
   rm test/browser/agent_organization_profile_test.rb
-  rm test/browser/agent_ticket_actions_*.rb
-  # test/browser/agent_ticket_overview_*.rb
-  rm test/browser/agent_user_*.rb
+  rm test/browser/agent_ticket_actions_level0.rb
+  rm test/browser/agent_ticket_actions_level1.rb
+  rm test/browser/agent_ticket_actions_level2.rb
+  rm test/browser/agent_ticket_actions_level3.rb
+  rm test/browser/agent_ticket_actions_level4.rb
+  # test/browser/agent_ticket_actions_level5.rb
+  # test/browser/agent_ticket_actions_level6.rb
+  # test/browser/agent_ticket_actions_level7.rb
+  # test/browser/agent_ticket_actions_level8.rb
+  # test/browser/agent_ticket_actions_level9.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
   rm test/browser/auth_test.rb
-  # test/browser/chat_test.rb
-  # test/browser/customer_ticket_create_test.rb
+  rm test/browser/chat_test.rb
+  rm test/browser/customer_ticket_create_test.rb
   rm test/browser/first_steps_test.rb
   rm test/browser/form_test.rb
   rm test/browser/keyboard_shortcuts_test.rb
@@ -97,6 +129,48 @@ elif [ "$LEVEL" == '3' ]; then
 elif [ "$LEVEL" == '4' ]; then
   echo "slicing level 4"
 
+  # only ticket action 3/3
+  rm test/browser/aab_basic_urls_test.rb
+  rm test/browser/aab_unit_test.rb
+  rm test/browser/aac_basic_richtext_test.rb
+  # test/browser/abb_one_group_test.rb
+  rm test/browser/admin_channel_email_test.rb
+  rm test/browser/admin_overview_test.rb
+  rm test/browser/agent_navigation_and_title_test.rb
+  rm test/browser/agent_organization_profile_test.rb
+  rm test/browser/agent_ticket_actions_level0_test.rb
+  rm test/browser/agent_ticket_actions_level1_test.rb
+  rm test/browser/agent_ticket_actions_level2_test.rb
+  rm test/browser/agent_ticket_actions_level3_test.rb
+  rm test/browser/agent_ticket_actions_level4_test.rb
+  rm test/browser/agent_ticket_actions_level5_test.rb
+  rm test/browser/agent_ticket_actions_level6_test.rb
+  rm test/browser/agent_ticket_actions_level7_test.rb
+  rm test/browser/agent_ticket_actions_level8_test.rb
+  rm test/browser/agent_ticket_actions_level9_test.rb
+  # test/browser/agent_ticket_overview_level0_test.rb
+  # test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
+  rm test/browser/auth_test.rb
+  # test/browser/chat_test.rb
+  # test/browser/customer_ticket_create_test.rb
+  rm test/browser/first_steps_test.rb
+  rm test/browser/form_test.rb
+  rm test/browser/keyboard_shortcuts_test.rb
+  rm test/browser/maintenance_*.rb
+  rm test/browser/manage_test.rb
+  rm test/browser/prefereces_test.rb
+  rm test/browser/setting_test.rb
+  rm test/browser/signup_password_change_and_reset_test.rb
+  rm test/browser/switch_to_user_test.rb
+  rm test/browser/taskbar_session_test.rb
+  rm test/browser/taskbar_task_test.rb
+  rm test/browser/translation_test.rb
+
+elif [ "$LEVEL" == '5' ]; then
+  echo "slicing level 5"
+
   # only profile action & admin
   rm test/browser/aab_basic_urls_test.rb
   rm test/browser/aab_unit_test.rb
@@ -106,8 +180,19 @@ elif [ "$LEVEL" == '4' ]; then
   # test/browser/admin_overview_test.rb
   rm test/browser/agent_navigation_and_title_test.rb
   # test/browser/agent_organization_profile_test.rb
-  rm test/browser/agent_ticket_*.rb
-  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_ticket_actions_level0_test.rb
+  rm test/browser/agent_ticket_actions_level1_test.rb
+  rm test/browser/agent_ticket_actions_level2_test.rb
+  rm test/browser/agent_ticket_actions_level3_test.rb
+  rm test/browser/agent_ticket_actions_level4_test.rb
+  rm test/browser/agent_ticket_actions_level5_test.rb
+  rm test/browser/agent_ticket_actions_level6_test.rb
+  rm test/browser/agent_ticket_actions_level7_test.rb
+  rm test/browser/agent_ticket_actions_level8_test.rb
+  rm test/browser/agent_ticket_actions_level9_test.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  # test/browser/agent_user_manage_test.rb
   # test/browser/agent_user_profile_test.rb
   rm test/browser/auth_test.rb
   rm test/browser/chat_test.rb
@@ -126,7 +211,7 @@ elif [ "$LEVEL" == '4' ]; then
   rm test/browser/translation_test.rb
 
 else
-  echo "ERROR: Invalid level $LEVEL - 1, 2 or 3 is available"
+  echo "ERROR: Invalid level $LEVEL - 1, 2, 3, 4 or 5 is available"
   exit 1
 fi
 
