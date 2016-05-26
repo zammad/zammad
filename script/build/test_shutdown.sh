06d6df950723b1a584a6f0d91d2f681b026c8a88
Martin Edenhofer <me@edenhofer.de>
2016-05-27 00:40:27 +0200

    Improved shutdown down.

--- script/build/test_shutdown.sh
+++ script/build/test_shutdown.sh
@@ -3,10 +3,15 @@ RAILS_ENV=$1
 APP_PORT=$2
 WS_PORT=$3
 EXIT=$4 || 0
+SERVER_PID='tmp/pids/server.pid'
 
 script/scheduler.rb stop
 script/websocket-server.rb stop
-kill $(cat tmp/pids/server.pid)
+kill $(cat $SERVER_PID)
+sleep 5
+if [ -f $SERVER_PID ]; then
+   kill -9 $(cat $SERVER_PID)
+fi
 
 rake db:drop RAILS_ENV=test
 rake db:drop RAILS_ENV=production
