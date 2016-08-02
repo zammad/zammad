49b660d84f215407ed4dadaefa3b5c58b12f3389
Rolf Schmidt <rolf.schmidt@znuny.com>
2016-08-02 15:01:29 +0200

    Fixed #189 - Increase data size of tanslation strings.

---
+++ db/migrate/20160802144900_increase_column_length.rb
@@ -0,0 +1,9 @@
+class IncreaseColumnLength < ActiveRecord::Migration
+  def change
+    change_table(:translations) do |t|
+      t.change   :source,         :string, limit: 500, null: false
+      t.change   :target,         :string, limit: 500, null: false
+      t.change   :target_initial, :string, limit: 500, null: false
+    end
+  end
+end
