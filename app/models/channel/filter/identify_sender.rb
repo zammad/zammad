42b45e50c157a520b0809135e7fc5582952b5c40
Martin Edenhofer <me@edenhofer.de>
2016-08-23 08:35:23 +0200

    Reduced complexity (not needed code).

--- app/models/channel/filter/identify_sender.rb
+++ app/models/channel/filter/identify_sender.rb
@@ -8,19 +8,15 @@ module Channel::Filter::IdentifySender
     customer_user = nil
     if !customer_user_id.empty?
       customer_user = User.lookup(id: customer_user_id)
-      if !customer_user
-        Rails.logger.debug "Invalid x-zammad-customer-id header '#{customer_user_id}', no such user."
-      else
+      if customer_user
         Rails.logger.debug "Took customer form x-zammad-customer-id header '#{customer_user_id}'."
-        if customer_user
-          create_recipients(mail)
-          return
-        end
+      else
+        Rails.logger.debug "Invalid x-zammad-customer-id header '#{customer_user_id}', no such user."
       end
     end
 
     # check if sender exists in database
-    if !mail[ 'x-zammad-customer-login'.to_sym ].empty?
+    if !customer_user && !mail[ 'x-zammad-customer-login'.to_sym ].empty?
       customer_user = User.find_by(login: mail[ 'x-zammad-customer-login'.to_sym ])
     end
     if !customer_user && !mail[ 'x-zammad-customer-email'.to_sym ].empty?
