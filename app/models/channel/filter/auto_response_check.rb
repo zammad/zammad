e4a731dcad2e45091154aad97d7d09e3ea9579dd
Martin Edenhofer <me@edenhofer.de>
2016-08-23 08:34:15 +0200

    Improved detection to auto responder. Just skip sender (but not other recipients) of trigger notification it auto responder was used.

--- app/models/channel/filter/auto_response_check.rb
+++ app/models/channel/filter/auto_response_check.rb
@@ -8,6 +8,12 @@ module Channel::Filter::AutoResponseCheck
     mail[ 'x-zammad-send-auto-response'.to_sym ] = false
     mail[ 'x-zammad-is-auto-response'.to_sym ] = true
 
+    if !mail[ 'x-zammad-article-preferences'.to_sym ]
+      mail[ 'x-zammad-article-preferences'.to_sym ] = {}
+    end
+    mail[ 'x-zammad-article-preferences'.to_sym ]['send-auto-response'] = false
+    mail[ 'x-zammad-article-preferences'.to_sym ]['is-auto-response'] = true
+
     return if mail[ 'x-loop'.to_sym ] && mail[ 'x-loop'.to_sym ] =~ /(yes|true)/i
     return if mail[ 'precedence'.to_sym ] && mail[ 'precedence'.to_sym ] =~ /bulk/i
     return if mail[ 'auto-submitted'.to_sym ] && mail[ 'auto-submitted'.to_sym ] =~ /auto-(generated|replied)/i
@@ -15,6 +21,9 @@ module Channel::Filter::AutoResponseCheck
 
     mail[ 'x-zammad-send-auto-response'.to_sym ] = true
     mail[ 'x-zammad-is-auto-response'.to_sym ] = false
+
+    mail[ 'x-zammad-article-preferences'.to_sym ]['send-auto-response'] = true
+    mail[ 'x-zammad-article-preferences'.to_sym ]['is-auto-response'] = false
 
   end
 end
