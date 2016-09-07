e890bc552d0b6f269a82f78de1f4fcd1a0887c1c
Martin Edenhofer <me@edenhofer.de>
2016-09-07 08:49:52 +0200

    Added cleanup file.

---
+++ contrib/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+rm app/assets/javascripts/app/controllers/layout_ref.coffee
+rm -rf app/assets/javascripts/app/views/layout_ref/
+rm app/assets/javascripts/app/controllers/api.coffee
+rm app/assets/javascripts/app/controllers/_integration/slack.coffee
+rm app/assets/javascripts/app/controllers/_integration/sipgate.coffee
+rm app/assets/javascripts/app/controllers/karma.coffee
+rm app/assets/javascripts/app/controllers/_integration/nagios.coffee
+rm app/assets/javascripts/app/controllers/_integration/icinga.coffee
+rm app/assets/javascripts/app/controllers/_integration/clearbit.coffee
+rm app/assets/javascripts/app/controllers/role.coffee
+rm app/assets/javascripts/app/controllers/report.coffee
+rm app/assets/javascripts/app/controllers/report_profile.coffee
