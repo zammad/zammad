d7115a6be5a0c0eabc9e82ed50032533a6e85f0e
Martin Edenhofer <me@edenhofer.de>
2016-10-20 23:18:26 +0200

    Fixed issue#237 - added database version check at app boot sequence - thanks to @zotto and @qpwr

--- config/initializers/db_preferences.rb
+++ config/initializers/db_preferences.rb
@@ -4,14 +4,3 @@
 Rails.application.config.db_case_sensitive = false
 Rails.application.config.db_like = 'LIKE'
 Rails.application.config.db_4bytes_utf8 = true
-
-# postgresql
-if ActiveRecord::Base.connection_config[:adapter] == 'postgresql'
-  Rails.application.config.db_case_sensitive = true
-  Rails.application.config.db_like = 'ILIKE'
-end
-
-# mysql
-if ActiveRecord::Base.connection_config[:adapter] == 'mysql2'
-  Rails.application.config.db_4bytes_utf8 = false
-end
---
+++ config/initializers/db_preferences_postgresql.rb
@@ -0,0 +1,22 @@
+# postgresql
+if ActiveRecord::Base.connection_config[:adapter] == 'postgresql'
+  Rails.application.config.db_case_sensitive = true
+  Rails.application.config.db_like = 'ILIKE'
+
+  # postgresql version check
+  #  example output: "9.5.0"
+  server_version = ActiveRecord::Base.connection.select_rows('SHOW server_version;')[0][0]
+  (major, minor) = server_version.split('.')
+  if major.to_i < 9 || (major.to_i == 9 && minor.to_i < 1)
+
+    # rubocop:disable Rails/Output
+    # rubocop:disable Rails/Exit
+    p '+++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    p '+ I\'m sorry, PostgreSQL 9.1+ is required            +'
+    p '+++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    exit 1
+    # rubocop:enable Rails/Exit
+    # rubocop:enable Rails/Output
+
+  end
+end
