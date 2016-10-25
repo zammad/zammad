3ec2ba3a43c2c9a83bccd566f8cf4124f6e2ada6
André Bauer <abauer@magix.net>
2016-10-25 15:51:46 +0200

    pkgr changes

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-
+#
+# packager.io postinstall script
+#
 set -ex
 
 # create init scripts
 /usr/bin/zammad scale web=1 websocket=1 worker=1
 
-# start zammad
-systemctl start zammad
+# stop zammad
+systemctl stop zammad
 
-# start nginx
-systemctl start nginx
+# db migration
+if /usr/bin/zammad config:get DATABASE_URL ; then
+    /usr/bin/zammad run rake db:migrate
+fi
+
+# start zammad
+systemctl restart zammad
 
