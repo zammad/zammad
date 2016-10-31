b898d2de60cd2b086ab8ba113dacdba1b10d1523
André Bauer <monotek@users.noreply.github.com>
2016-10-31 14:04:40 +0100

    Update preinstall.sh

--- contrib/packager.io/preinstall.sh
+++ contrib/packager.io/preinstall.sh
@@ -7,8 +7,8 @@ PATH=/opt/zammad/bin:/opt/zammad/vendor/bundle/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # install epel-release & nginx on centos because it does not work via dependencies
 if [ -n "$(which yum)" ]; then
-    yum install -y epel-release
+    yum install -y -q epel-release
 
-    yum install -y nginx
+    yum install -y -q nginx
 fi
 
