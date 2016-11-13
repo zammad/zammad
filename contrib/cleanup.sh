b9f8c798188f5b2b155d5a943d3f197615a956ee
Martin Edenhofer <me@edenhofer.de>
2016-11-14 00:48:13 +0100

    Enabled integrations.

--- contrib/cleanup.sh
+++ contrib/cleanup.sh
@@ -4,10 +4,6 @@ set -ex
 
 rm app/assets/javascripts/app/controllers/layout_ref.coffee
 rm -rf app/assets/javascripts/app/views/layout_ref/
-rm app/assets/javascripts/app/controllers/api.coffee
-rm app/assets/javascripts/app/controllers/integrations.coffee
-rm app/assets/javascripts/app/controllers/_integration/slack.coffee
-rm app/assets/javascripts/app/controllers/_integration/sipgate_io.coffee
 rm app/assets/javascripts/app/controllers/karma.coffee
 rm app/assets/javascripts/app/controllers/_integration/nagios.coffee
 rm app/assets/javascripts/app/controllers/_integration/icinga.coffee
