29ea3199eefb64d11d156a5a4d9444df34b3d862
Martin Edenhofer <me@edenhofer.de>
2016-11-17 15:06:31 +0100

    Fixed issue#428 - Switching storage provider for attachments in UI has no effect.

---
+++ db/migrate/20161117000003_store_config_name_update_issue_428.rb
@@ -0,0 +1,30 @@
+class StoreConfigNameUpdateIssue428 < ActiveRecord::Migration
+  def up
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+    setting = Setting.find_by(name: 'storage')
+    return if !Setting
+    setting.name = 'storage_provider'
+    setting.options = {
+      form: [
+        {
+          display: '',
+          null: true,
+          name: 'storage_provider',
+          tag: 'select',
+          tranlate: true,
+          options: {
+            'DB' => 'Database',
+            'File' => 'Filesystem',
+          },
+        },
+      ],
+    }
+    setting.preferences = {
+      controller: 'SettingsAreaStorageProvider',
+      online_service_disable: true,
+      permission: ['admin.system'],
+    }
+    setting.save!
+  end
+end
