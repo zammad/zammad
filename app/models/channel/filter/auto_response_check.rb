026835a5e44e69dcb03b68f371574b83edb4073a
Martin Edenhofer <me@edenhofer.de>
2016-11-19 16:51:46 +0100

    Improved check for precedence header to send no auto responder.

--- app/models/channel/filter/auto_response_check.rb
+++ app/models/channel/filter/auto_response_check.rb
@@ -15,7 +15,7 @@ module Channel::Filter::AutoResponseCheck
     mail[ 'x-zammad-article-preferences'.to_sym ]['is-auto-response'] = true
 
     return if mail[ 'x-loop'.to_sym ] && mail[ 'x-loop'.to_sym ] =~ /(yes|true)/i
-    return if mail[ 'precedence'.to_sym ] && mail[ 'precedence'.to_sym ] =~ /bulk/i
+    return if mail[ 'precedence'.to_sym ] && mail[ 'precedence'.to_sym ] =~ /(bulk|list|junk)/i
     return if mail[ 'auto-submitted'.to_sym ] && mail[ 'auto-submitted'.to_sym ] =~ /auto-(generated|replied)/i
     return if mail[ 'x-auto-response-suppress'.to_sym ] && mail[ 'x-auto-response-suppress'.to_sym ] =~ /all/i
 
