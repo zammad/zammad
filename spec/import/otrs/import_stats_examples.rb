477fefc4e5114fe4cd8ac8b9b6aedd2bfe99d635
Thorsten Eckel <te@znuny.com>
2016-12-09 16:12:39 +0100

    Made OTRS import statistic method public to access it for plausibility check.

--- spec/import/otrs/import_stats_examples.rb
+++ spec/import/otrs/import_stats_examples.rb
@@ -2,4 +2,7 @@ RSpec.shared_examples 'Import::OTRS::ImportStats' do
   it 'responds to current_state' do
     expect(described_class).to respond_to('current_state')
   end
+  it 'responds to statistic' do
+    expect(described_class).to respond_to('statistic')
+  end
 end
