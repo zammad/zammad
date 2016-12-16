47c880f445183c76efb3a126c6c01af90d523717
Thorsten Eckel <te@znuny.com>
2016-12-16 11:12:15 +0100

    Fixed issue #548 - Empty created_at of customer user record brakes import.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # make usage of time travel helpers possible
+  config.include ActiveSupport::Testing::TimeHelpers
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
