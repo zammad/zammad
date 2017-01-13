fb64e186b59b169401b1fa3b60a43c370ced36ba
Martin Edenhofer <me@edenhofer.de>
2017-01-13 20:37:14 +0100

    Fixed issue #587 - Group restrictions not respected in slack notification. Thanks to @jepf

---
+++ db/migrate/20170113000002_slack_group_config_issue_587.rb
@@ -0,0 +1,20 @@
+class SlackGroupConfigIssue587 < ActiveRecord::Migration
+  def up
+
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+
+    setting = Setting.find_by(name: 'slack_config')
+    return if !setting
+
+    return if !setting.state_current['value']
+    return if !setting.state_current['value']['items']
+    config_item = setting.state_current['value']['items'].first
+    return if !config_item
+
+    return if !config_item.key?('group_id')
+    config_item['group_ids'] = config_item.delete('group_id')
+
+    setting.save!
+  end
+end
