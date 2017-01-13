6cf0f5cc6ce70cd2c9403bdefa351640d7d5c58e
Martin Edenhofer <me@edenhofer.de>
2017-01-13 20:24:38 +0100

    Fixed namespace.

--- db/migrate/20170113000001_object_manager_attribute_create_middle.rb
+++ db/migrate/20170113000001_object_manager_attribute_create_middle.rb
@@ -1,4 +1,4 @@
-class UpdateObjectManagerAttributeCreateMiddle < ActiveRecord::Migration
+class ObjectManagerAttributeCreateMiddle < ActiveRecord::Migration
   def up
     # return if it's a new setup
     return if !Setting.find_by(name: 'system_init_done')
