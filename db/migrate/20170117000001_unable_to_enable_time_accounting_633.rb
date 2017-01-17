d0b26eb55ae843ccc337d0c202e51e27388cbd2e
Martin Edenhofer <me@edenhofer.de>
2017-01-17 12:38:12 +0100

    Fixed issue #633 - Unable to activate time accounting in admin interface.

---
+++ db/migrate/20170117000001_unable_to_enable_time_accounting_633.rb
@@ -0,0 +1,52 @@
+class UnableToEnableTimeAccounting633 < ActiveRecord::Migration
+  def up
+
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+
+    Setting.create_if_not_exists(
+      title: 'Time Accounting',
+      name: 'time_accounting',
+      area: 'Web::Base',
+      description: 'Enable time accounting.',
+      options: {
+        form: [
+          {
+            display: '',
+            null: true,
+            name: 'time_accounting',
+            tag: 'boolean',
+            options: {
+              true  => 'yes',
+              false => 'no',
+            },
+          },
+        ],
+      },
+      preferences: {
+        authentication: true,
+        permission: ['admin.time_accounting'],
+      },
+      state: false,
+      frontend: true
+    )
+
+    Setting.create_if_not_exists(
+      title: 'Time Accounting Selector',
+      name: 'time_accounting_selector',
+      area: 'Web::Base',
+      description: 'Enable time accounting for this tickets.',
+      options: {
+        form: [
+          {},
+        ],
+      },
+      preferences: {
+        authentication: true,
+        permission: ['admin.time_accounting'],
+      },
+      state: {},
+      frontend: true
+    )
+  end
+end
