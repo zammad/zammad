f9de331bb49aaf2d2e1295c24bf21a1152f74d34
Oliver Grahl <og@grahl-software.com>
2017-01-18 09:39:23 +0100

    Fix #639 (Error 500 opening a ticket view)

--- app/models/taskbar.rb
+++ app/models/taskbar.rb
@@ -55,6 +55,7 @@ class Taskbar < ApplicationModel
     return true if local_update
 
     # find other same open tasks
+    preferences = {}
     preferences[:tasks] = []
     Taskbar.where(key: key).order(:created_at).each { |taskbar|
       if taskbar.id == id
