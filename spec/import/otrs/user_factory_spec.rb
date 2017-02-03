5a6e824c3ad4b34eac57fe8b4becdcc8ba5f5677
Thorsten Eckel <te@znuny.com>
2017-02-03 14:16:11 +0100

    Working on issue #689 - User root@localhost gets imported but should not.

--- spec/import/otrs/user_factory_spec.rb
+++ spec/import/otrs/user_factory_spec.rb
@@ -3,4 +3,12 @@ require 'import/factory_examples'
 
 RSpec.describe Import::OTRS::UserFactory do
   it_behaves_like 'Import::Factory'
+
+  it 'skips root@localhost' do
+
+    root_data = json_fixture('import/otrs/user/default')
+    expect(Import::OTRS::User).to_not receive(:new)
+
+    described_class.import([root_data])
+  end
 end
