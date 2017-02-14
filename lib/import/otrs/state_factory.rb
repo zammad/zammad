37e98b1a81c7b8f45e92e7f2b4c0b88cbc92980d
Thorsten Eckel <te@znuny.com>
2017-02-14 09:59:41 +0100

    Working on issue #689 - Ticket state_id and pending_time attributes won't get synced with new OTRS data.

--- lib/import/otrs/state_factory.rb
+++ lib/import/otrs/state_factory.rb
@@ -17,6 +17,71 @@ module Import
           state.save
         }
       end
+
+      def import_loop(records, *_args, &import_block)
+        super
+        update_ticket_state
+        update_ticket_pending_time
+      end
+
+      def update_ticket_state
+        agent_new = ::Ticket::State.where(
+          state_type_id: ::Ticket::StateType.where.not(name: %w(merged removed))
+        ).pluck(:id)
+
+        agent_edit = ::Ticket::State.where(
+          state_type_id: ::Ticket::StateType.where.not(name: %w(new merged removed))
+        ).pluck(:id)
+
+        customer_new = ::Ticket::State.where(
+          state_type_id: ::Ticket::StateType.where.not(name: %w(new closed))
+        ).pluck(:id)
+
+        customer_edit = ::Ticket::State.where(
+          state_type_id: ::Ticket::StateType.where.not(name: %w(open closed))
+        ).pluck(:id)
+
+        ticket_state_id = ::ObjectManager::Attribute.get(
+          object: 'Ticket',
+          name:   'state_id',
+        )
+
+        ticket_state_id[:data_option][:filter]               = agent_new
+        ticket_state_id[:screens][:create_middle][:Customer] = customer_new
+        ticket_state_id[:screens][:edit][:Agent]             = agent_edit
+        ticket_state_id[:screens][:edit][:Customer]          = customer_edit
+
+        update_ticket_attribute(ticket_state_id)
+      end
+
+      def update_ticket_pending_time
+        pending_state_ids = ::Ticket::State.where(
+          state_type_id: ::Ticket::StateType.where(name: ['pending reminder', 'pending action'])
+        ).pluck(:id)
+
+        ticket_pending_time = ::ObjectManager::Attribute.get(
+          object: 'Ticket',
+          name:   'pending_time',
+        )
+
+        ticket_pending_time[:data_option][:required_if][:state_id] = pending_state_ids
+        ticket_pending_time[:data_option][:required_if][:state_id] = pending_state_ids
+
+        update_ticket_attribute(ticket_pending_time)
+      end
+
+      def update_ticket_attribute(attribute)
+        ::ObjectManager::Attribute.add(
+          object_lookup_id: attribute[:object_lookup_id],
+          name:             attribute[:name],
+          display:          attribute[:display],
+          data_type:        attribute[:data_type],
+          data_option:      attribute[:data_option],
+          active:           attribute[:active],
+          screens:          attribute[:screens],
+          force:            true # otherwise _id as a name is not permitted
+        )
+      end
     end
   end
 end
--- spec/import/otrs/state_factory_spec.rb
+++ spec/import/otrs/state_factory_spec.rb
@@ -8,4 +8,41 @@ RSpec.describe Import::OTRS::StateFactory do
     expect(described_class).to receive(:backup)
     described_class.pre_import_hook([])
   end
+
+  def load_state_json(file)
+    json_fixture("import/otrs/state/#{file}")
+  end
+
+  it 'updates ObjectManager Ticket state_id and pending_time filter' do
+
+    states = %w(new open merged pending_reminder pending_auto_close_p pending_auto_close_n pending_auto_close_p closed_successful closed_unsuccessful closed_successful removed)
+
+    state_backend_param = []
+    states.each do |state|
+      state_backend_param.push(load_state_json(state))
+    end
+
+    ticket_state_id = ::ObjectManager::Attribute.get(
+      object: 'Ticket',
+      name:   'state_id',
+    )
+    ticket_pending_time = ::ObjectManager::Attribute.get(
+      object: 'Ticket',
+      name:   'pending_time',
+    )
+
+    expect {
+      described_class.import(state_backend_param)
+
+      # sync changes
+      ticket_state_id.reload
+      ticket_pending_time.reload
+    }.to change {
+      ticket_state_id.data_option
+    }.and change {
+      ticket_state_id.screens
+    }.and change {
+      ticket_pending_time.data_option
+    }
+  end
 end
