b5b66f8e4e436acab65ff2177a5c190712c70b4f
Martin Edenhofer <me@edenhofer.de>
2017-02-24 15:02:30 +0100

    Skip csrf token check for error controller.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,5 @@
 class ErrorsController < ApplicationController
+  skip_before_action :verify_csrf_token
   def routing
     not_found(ActionController::RoutingError.new("No route matches [#{request.method}] #{request.path}"))
   end
