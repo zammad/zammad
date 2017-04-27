dd3700418fbb3e431908b980ace6610ea3a87618
Rolf Schmidt <rolf.schmidt@znuny.com>
2017-04-27 09:54:38 +0200

    Fixed column size of reply_to to 300 chars.

--- db/migrate/20170426135500_add_reply_to.rb
+++ db/migrate/20170426135500_add_reply_to.rb
@@ -4,6 +4,6 @@ class AddReplyTo < ActiveRecord::Migration
     # return if it's a new setup
     return if !Setting.find_by(name: 'system_init_done')
 
-    add_column :ticket_articles, :reply_to, :string, limit: 500
+    add_column :ticket_articles, :reply_to, :string, limit: 300
   end
 end
