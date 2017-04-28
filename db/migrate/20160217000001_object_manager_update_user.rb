d5161405c0aae6874b0dd98c01df372c836f80e3
Thorsten Eckel <te@znuny.com>
2017-04-28 15:39:29 +0200

    Fixed bug: ObjectManager attributes are added twice when running all migrations and seeds.

--- db/migrate/20160217000001_object_manager_update_user.rb
+++ db/migrate/20160217000001_object_manager_update_user.rb
@@ -1,5 +1,9 @@
 class ObjectManagerUpdateUser < ActiveRecord::Migration
   def up
+
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+
     UserInfo.current_user_id = 1
     ObjectManager::Attribute.add(
       force: true,
