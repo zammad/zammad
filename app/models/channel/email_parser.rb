0b6cac5007b468ddb7400f8fb91c5e4fad926bdd
Martin Edenhofer <me@edenhofer.de>
2017-05-16 14:03:42 +0200

    Fixed issue #811 - Unprocessable Email (without email address in from header) and issue #969 - Mail processing error (with broken mail headers).

--- app/models/channel/email_parser.rb
+++ app/models/channel/email_parser.rb
@@ -712,6 +712,7 @@ module Mail
   class Field
     def raw_value
       value = Encode.conv('utf8', @raw_value)
+      return value if value.blank?
       value.sub(/^.+?:(\s|)/, '')
     end
   end
