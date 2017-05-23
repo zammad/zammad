a2a6309692d06b986e93da0e1137805f5f934987
Rolf Schmidt <rolf.schmidt@znuny.com>
2017-05-23 11:26:46 +0200

    Fixed issue #1034 - Update of object attribute leads to CSRF token verification failure.

--- app/controllers/object_manager_attributes_controller.rb
+++ app/controllers/object_manager_attributes_controller.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2012-2016 Zammad Foundation, http://zammad-foundation.org/
 
 class ObjectManagerAttributesController < ApplicationController
-  before_action { authentication_check(permission: 'admin.object') }
+  prepend_before_action { authentication_check(permission: 'admin.object') }
 
   # GET /object_manager_attributes_list
   def list
