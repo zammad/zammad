4ff2ef7c114681a66f5a9076b524a5e6f66fcdd0
Thorsten Eckel <te@znuny.com>
2017-05-30 18:32:28 +0200

    Fixed issue #1095 - Zendesk import: Error while importing ticket field with dash in name.

--- spec/lib/import/zendesk/ticket_field_spec.rb
+++ spec/lib/import/zendesk/ticket_field_spec.rb
@@ -3,4 +3,32 @@ require 'lib/import/zendesk/object_field_examples'
 
 RSpec.describe Import::Zendesk::TicketField do
   it_behaves_like 'Import::Zendesk::ObjectField'
+
+  it 'handles fields with dashes in title' do
+
+    zendesk_object = double(
+      id:                 1337,
+      title:              'Priority - Simple',
+      key:                'priority_simple',
+      type:               'text',
+      removable:          true,
+      active:             true,
+      position:           1,
+      required_in_portal: true,
+      visible_in_portal:  true,
+      required:           true,
+      description:        'Example field',
+    )
+
+    expect(ObjectManager::Attribute).to receive(:migration_execute).and_return(true)
+
+    expect do
+      described_class.new(zendesk_object)
+    end.not_to raise_error
+
+    ObjectManager::Attribute.remove(
+      object: 'Ticket',
+      name:   zendesk_object.key,
+    )
+  end
 end
