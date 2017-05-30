48e3da57ee06227c9b57f96a5afb56f132325aa1
Martin Edenhofer <me@edenhofer.de>
2017-05-30 08:55:51 +0200

    Improved email loop protection. Remember permanent delivery failed on user to prevent email loops.

---
+++ app/models/channel/filter/bounce_delivery_permanent_failed.rb
@@ -0,0 +1,73 @@
+# Copyright (C) 2012-2016 Zammad Foundation, http://zammad-foundation.org/
+
+module Channel::Filter::BounceDeliveryPermanentFailed
+
+  def self.run(_channel, mail)
+
+    return if !mail[:mail_instance]
+    return if !mail[:mail_instance].bounced?
+    return if !mail[:attachments]
+
+    # remember, do not send notifications to certain recipients again if failed permanent
+    mail[:attachments].each { |attachment|
+      next if !attachment[:preferences]
+      next if attachment[:preferences]['Mime-Type'] != 'message/rfc822'
+      next if !attachment[:data]
+
+      result = Channel::EmailParser.new.parse(attachment[:data])
+      next if !result[:message_id]
+      message_id_md5 = Digest::MD5.hexdigest(result[:message_id])
+      article = Ticket::Article.where(message_id_md5: message_id_md5).order('created_at DESC, id DESC').limit(1).first
+      next if !article
+
+      # check user preferences
+      next if mail[:mail_instance].action != 'failed'
+      next if mail[:mail_instance].retryable? != false
+      next if mail[:mail_instance].error_status != '5.1.1'
+
+      # get recipient of origin article, if only one - mark this user to not sent notifications anymore
+      recipients = []
+      if article.sender.name == 'System' || article.sender.name == 'Agent'
+        %w(to cc).each { |line|
+          next if article[line].blank?
+          recipients = []
+          begin
+            list = Mail::AddressList.new(article[line])
+            list.addresses.each { |address|
+              next if address.address.blank?
+              recipients.push address.address.downcase
+            }
+          rescue
+            Rails.logger.info "Unable to parse email address in '#{article[line]}'"
+          end
+        }
+        if recipients.count > 1
+          recipients = []
+        end
+      end
+
+      # get recipient bounce mail, mark this user to not sent notifications anymore
+      final_recipient = mail[:mail_instance].final_recipient
+      if final_recipient.present?
+        final_recipient.sub!(/rfc822;\s{0,10}/, '')
+        if final_recipient.present?
+          recipients.push final_recipient.downcase
+        end
+      end
+
+      # set user preferences
+      recipients.each { |recipient|
+        users = User.where(email: recipient)
+        users.each { |user|
+          next if !user
+          user.preferences[:mail_delivery_failed] = true
+          user.preferences[:mail_delivery_failed_data] = Time.zone.now
+          user.save!
+        }
+      }
+    }
+
+    true
+
+  end
+end
---
+++ db/migrate/20170529000002_setting_delivery_permanent_failed.rb
@@ -0,0 +1,35 @@
+class SettingDeliveryPermanentFailed < ActiveRecord::Migration
+  def up
+
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+
+    setting = Setting.find_by(name: '0900_postmaster_filter_bounce_check')
+    if setting
+      setting.name = '0900_postmaster_filter_bounce_follow_up_check'
+      setting.state = 'Channel::Filter::BounceFollowUpCheck'
+      setting.save!
+    else
+      Setting.create_if_not_exists(
+        title: 'Defines postmaster filter.',
+        name: '0900_postmaster_filter_bounce_follow_up_check',
+        area: 'Postmaster::PreFilter',
+        description: 'Defines postmaster filter to identify postmaster bounced - to handle it as follow-up of the original ticket.',
+        options: {},
+        state: 'Channel::Filter::BounceFollowUpCheck',
+        frontend: false
+      )
+    end
+    Setting.create_if_not_exists(
+      title: 'Defines postmaster filter.',
+      name: '0950_postmaster_filter_bounce_delivery_permanent_failed',
+      area: 'Postmaster::PreFilter',
+      description: 'Defines postmaster filter to identify postmaster bounced - disable sending notification on permanent deleivery failed.',
+      options: {},
+      state: 'Channel::Filter::BounceDeliveryPermanentFailed',
+      frontend: false
+    )
+
+  end
+
+end
