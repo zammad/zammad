30b7b6e58830216b95d6903f451c72505efc03d5
Thorsten Eckel <te@znuny.com>
2017-06-02 11:30:53 +0200

    Fixed issue #1153 - Using Zendesk basic priority feature breakes import.

--- lib/import/zendesk/ticket_field_factory.rb
+++ lib/import/zendesk/ticket_field_factory.rb
@@ -5,13 +5,14 @@ module Import
       extend Import::Zendesk::LocalIDMapperHook
 
       MAPPING = {
-        'subject'     => 'title',
-        'description' => 'note',
-        'status'      => 'state_id',
-        'tickettype'  => 'type',
-        'priority'    => 'priority_id',
-        'group'       => 'group_id',
-        'assignee'    => 'owner_id',
+        'subject'        => 'title',
+        'description'    => 'note',
+        'status'         => 'state_id',
+        'tickettype'     => 'type',
+        'priority'       => 'priority_id',
+        'basic_priority' => 'priority_id',
+        'group'          => 'group_id',
+        'assignee'       => 'owner_id',
       }.freeze
 
       # rubocop:disable Style/ModuleFunction
