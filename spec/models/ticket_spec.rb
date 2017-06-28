6ed57162cbb95008996e9943613a6c2e7a700953
Thorsten Eckel <te@znuny.com>
2017-06-28 10:25:25 +0200

    Fixed bug: Invalid new states should be handled by ActiveRecord and don't cause Ticket#reset_pending_time to fail.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -35,4 +35,27 @@ RSpec.describe Ticket do
 
   end
 
+  context 'callbacks' do
+
+    describe '#reset_pending_time' do
+
+      it 'resets the pending time on state change' do
+        ticket = create(:ticket,
+                        state:        Ticket::State.lookup(name: 'pending reminder'),
+                        pending_time: Time.zone.now + 2.days)
+        expect(ticket.pending_time).not_to be nil
+
+        ticket.update_attribute(:state, Ticket::State.lookup(name: 'open'))
+        expect(ticket.pending_time).to be nil
+      end
+
+      it 'lets handle ActiveRecord nil as new value' do
+        ticket = create(:ticket)
+        expect do
+          ticket.update_attribute(:state, nil)
+        end.to raise_error(ActiveRecord::StatementInvalid)
+      end
+
+    end
+  end
 end
