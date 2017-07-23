72a0af74157a094fc75c0394fbd7e8f5fab793da
André Bauer <monotek23@gmail.com>
2017-07-23 13:28:47 +0200

    fixed travis.yml

---
+++ contrib/travis-ci.org/trigger-docker-compose-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# trigger build of https://github.com/zammad/zammad-docker-compose on https://travis-ci.org/zammad/zammad-docker-compose and upload it to https://hub.docker.com/r/zammad/zammad-docker-compose/
+#
+
+if [ ${TRAVIS_BRANCH} == 'stable' ]; then
+  curl -X POST -H "Content-Type: application/json" -H "Travis-API-Version: 3" -H "Accept: application/json" -H "Authorization: ${TRAVIS_API_TOKEN}" -d '{"request": {"branch": "master"}}' 'https://api.travis-ci.org/repo/zammad%2Fzammad-docker-compose/requests'
+fi
