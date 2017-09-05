b3e178de2785a0f4bf2a4661bba57c9cbe931d1f
Rolf Schmidt <rolf.schmidt@znuny.com>
2017-09-05 16:54:22 +0200

    Fixed issue #1318 - 'Access-Control-Allow-Origin' error when submitting web form.
    Conflicts:
    	config/routes/form.rb

--- config/routes/form.rb
+++ config/routes/form.rb
@@ -2,7 +2,7 @@ Zammad::Application.routes.draw do
   api_path = Rails.configuration.api_path
 
   # forms
-  match api_path + '/form_submit',      to: 'form#submit',    via: :post
-  match api_path + '/form_config',      to: 'form#config',    via: :get
+  match api_path + '/form_submit',      to: 'form#submit',        via: :post
+  match api_path + '/form_config',      to: 'form#configuration', via: :post
 
 end
