ad56a6c983692f4b9e3dfb4059fd27151f298302
André Bauer <monotek23@gmail.com>
2017-09-17 02:15:22 +0200

    added trigger to build docker image on travis

--- contrib/travis-ci.org/trigger-docker-build.sh
+++ contrib/travis-ci.org/trigger-docker-build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# trigger build of https://github.com/zammad/zammad-docker-compose on https://travis-ci.org/zammad/zammad-docker-compose and upload it to https://hub.docker.com/r/zammad/zammad-docker-compose/
+# trigger build of https://github.com/zammad/zammad on https://travis-ci.org/zammad/zammad and upload it to https://hub.docker.com/r/zammad/zammad
 #
 
 REPO_USER="zammad"
-REPO="zammad-docker-compose"
-BRANCH="master"
+REPO="zammad"
+BRANCH="develop"
 
-if [ "${TRAVIS_BRANCH}" == 'stable' ]; then
+if [ "${TRAVIS_BRANCH}" == 'develop' ]; then
   curl -X POST \
     -H "Content-Type: application/json" \
     -H "Travis-API-Version: 3" \
