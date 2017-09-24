dd830e56e28ad25bc55572ee37eea2a5e6c133b0
Rolando <rolando.torres2810@gmail.com>
2017-09-24 22:34:43 +0200

    Backend validation for reopening a ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,6 +5,7 @@ class TicketsController < ApplicationController
   include TicketStats
 
   prepend_before_action :authentication_check
+  before_action :reopen_role_validation, only: :update
 
   # GET /api/v1/tickets
   def index
@@ -601,6 +602,17 @@ class TicketsController < ApplicationController
       tags: tags,
       form_meta: attributes_to_change[:form_meta],
     }
+  end
+
+  def reopen_role_validation
+    ticket = Ticket.find(params[:id])
+    new_state = params[:state][:id] if params[:state]
+
+    # Validate that a ticket's customer is also not an agent
+    is_customer = ticket.customer.role?('Customer')
+
+    # Validate against ID in case of I18n
+    raise Exceptions::NotAuthorized, 'Not authorized' if ticket.state_id == 4 && new_state.to_i == 2 && is_customer
   end
 
 end
