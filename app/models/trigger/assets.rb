524d1fe4074785c31b9269c994d4c37579909de1
Martin Edenhofer <me@edenhofer.de>
2017-11-23 09:29:12 +0100

    Applied rubocop.

--- app/models/trigger/assets.rb
+++ app/models/trigger/assets.rb
@@ -37,7 +37,7 @@ returns
         data = assets_of_selector('condition', data)
         data = assets_of_selector('perform', data)
       end
-      %w[created_by_id updated_by_id].each do |local_user_id|
+      %w(created_by_id updated_by_id).each do |local_user_id|
         next if !self[ local_user_id ]
         next if data[ app_model_user ][ self[ local_user_id ] ]
         user = User.lookup(id: self[ local_user_id ])
