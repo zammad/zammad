66b9a3c6e1f9e0de058d8458a218ac87c289db5c
Muhammad Nuzaihan <muhammad@taqisystems.com>
2017-12-25 01:18:13 +0800

    add additional checks if IP set in argument is available on the machine

--- script/websocket-server.rb
+++ script/websocket-server.rb
@@ -121,7 +121,11 @@ end
 Rails.configuration.interface = 'websocket'
 EventMachine.run do
  
-  @options[:b] = IPAddr.new @options[:b]
+  available_addr = Socket.ip_address_list.collect { |address| address.ip_address } # fetch all available IPs on machine
+  available_addr += ["0.0.0.0", "[::]"] # add IP binding for all interfaces
+  @options[:b] = IPAddr.new @options[:b] # check if it is valid IPv4 or IPv6 address
+  raise Exceptions::UnprocessableEntity, 'IP address is not available on this machine.' if available_addr.exclude? @options[:b] # raise if IP Address is not available
+
   EventMachine::WebSocket.start(host: @options[:b].to_s, port: @options[:p], secure: @options[:s], tls_options: tls_options) do |ws|
 
     # register client connection
