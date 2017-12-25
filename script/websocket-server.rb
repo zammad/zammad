e6201fb2e938b48bd6bca52fabb5586c1a2b22e2
Muhammad Nuzaihan <muhammad@taqisystems.com>
2017-12-25 18:51:56 +0800

    fix rubocop offenses

--- script/websocket-server.rb
+++ script/websocket-server.rb
@@ -120,9 +120,8 @@ end
 @clients = {}
 Rails.configuration.interface = 'websocket'
 EventMachine.run do
- 
-  available_addr = Socket.ip_address_list.collect { |address| address.ip_address } # fetch all available IPs on machine
-  available_addr += ["0.0.0.0", "::"] # add IP binding for all interfaces
+  available_addr = Socket.ip_address_list.collect(&:ip_address) # fetch all available IPs on machine
+  available_addr += ['0.0.0.0', '::'] # add IP binding for all interfaces
   @options[:b] = IPAddr.new @options[:b] # check if it is valid IPv4 or IPv6 address
   raise Exceptions::UnprocessableEntity, 'IP address is not available on this machine.' if available_addr.exclude? @options[:b].to_s # raise if IP Address is not available
 
