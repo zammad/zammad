cef163fcc0dddb267b8ea9e759eb22d75428e87f
Martin Edenhofer <me@edenhofer.de>
2018-01-16 06:56:40 +0100

    Added support to ignore system id for follow up checks.

--- db/seeds/settings.rb
+++ db/seeds/settings.rb
@@ -1672,6 +1672,34 @@ Setting.create_if_not_exists(
   },
   frontend: false
 )
+Setting.create_if_not_exists(
+  title: 'Ticket Number ignore system_id',
+  name: 'ticket_number_ignore_system_id',
+  area: 'Ticket::Core',
+  description: '-',
+  options: {
+    form: [
+      {
+        display: 'Ignore system_id',
+        null: true,
+        name: 'ticket_number_ignore_system_id',
+        tag: 'boolean',
+        options: {
+          true  => 'yes',
+          false => 'no',
+        },
+      },
+    ],
+  },
+  state: {
+    ticket_number_ignore_system_id: false
+  },
+  preferences: {
+    permission: ['admin.ticket'],
+    hidden: true,
+  },
+  frontend: false
+)
 
 Setting.create_if_not_exists(
   title: 'Enable Ticket creation',
