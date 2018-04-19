426fce716079eda4a3ce38b147f94b8b64bb10ca
Ryan Lue <hello@ryanlue.com>
2018-04-19 15:51:19 +0800

    Add wait before retrying failed jobs (Fix #1950)

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -245,6 +245,7 @@ class Scheduler < ApplicationModel
 
     # restart job again
     if try_run_max > try_count
+      sleep(try_count) # wait between retries (see https://github.com/zammad/zammad/issues/1950)
       _start_job(job, try_count, try_run_time)
     else
       # release thread lock and remove thread handle
