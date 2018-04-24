f2bf4cbd029bbfdc79888c188d2a29d1ebc5f27d
Thorsten Eckel <te@znuny.com>
2018-04-24 15:26:09 +0200

    Refactored Channel::Filter RSpec test and added helper.

--- app/models/channel/filter/follow_up_merged.rb
+++ app/models/channel/filter/follow_up_merged.rb
@@ -5,8 +5,7 @@ module Channel::Filter::FollowUpMerged
   def self.run(_channel, mail)
     return if mail[:'x-zammad-ticket-id'].blank?
 
-    # get ticket# from subject
-    ticket = Ticket.find(mail[:'x-zammad-ticket-id'])
+    ticket = Ticket.find_by(id: mail[:'x-zammad-ticket-id'])
     return if ticket.blank?
 
     ticket = find_merge_follow_up_ticket(ticket)
@@ -29,13 +28,12 @@ module Channel::Filter::FollowUpMerged
       next if link['link_type'] != 'parent'
       next if link['link_object'] != 'Ticket'
 
-      check_ticket = Ticket.find(link['link_object_value'])
-
+      check_ticket = Ticket.find_by(id: link['link_object_value'])
       next if check_ticket.blank?
+
       next if check_ticket.state.name == 'merged'
 
       merge_ticket = check_ticket
-
       break
     end
     merge_ticket
