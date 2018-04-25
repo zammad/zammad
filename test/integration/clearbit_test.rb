7e3a10af2620e2e05b3f0f1ef8de09cc9a735a9e
Martin Edenhofer <me@edenhofer.de>
2018-04-25 17:23:42 +0200

    Updated test with new realtime data.

--- test/integration/clearbit_test.rb
+++ test/integration/clearbit_test.rb
@@ -269,8 +269,10 @@ class ClearbitTest < ActiveSupport::TestCase
     assert_equal('', customer6.firstname)
     assert_equal('', customer6.lastname)
     assert_equal('', customer6.note)
-    assert_equal('http://clearbit.com', customer6.web)
-    assert_equal('3030 16th St, San Francisco, CA 94103, USA', customer6.address)
+    assert_equal('', customer6.web)
+    #assert_equal('http://clearbit.com', customer6.web)
+    assert_equal('San Francisco, CA 94103, USA', customer6.address)
+    #assert_equal('3030 16th St, San Francisco, CA 94103, USA', customer6.address)
     #assert_equal('San Francisco, CA, USA', customer6.address)
 
     organization6 = Organization.find_by(name: 'APIHub, Inc')
