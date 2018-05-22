8fb7d9903f870e150b870a9a320a2d97e1675103
Thorsten Eckel <te@znuny.com>
2018-05-22 18:40:39 +0200

    Fixed issue #2037: Restart of processes form application context fails.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# DO NOT REMOVE THIS LINE - see issue #2037
+Bundler.setup
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
