e0f616846f2e2d1450924132d9d5c17c21b37eee
Thorsten Eckel <te@znuny.com>
2018-05-23 18:46:01 +0200

    Fixed issue #2039: Import fails for ObjectManager Attribute with non-ASCII name.

--- lib/sequencer/unit/import/common/object_attribute/sanitized_name.rb
+++ lib/sequencer/unit/import/common/object_attribute/sanitized_name.rb
@@ -12,12 +12,14 @@ class Sequencer
               # model_nos
               # model_name
               # model_name
+              # model_name
               without_double_underscores.gsub(/_id(s?)$/, '_no\1')
             end
 
             def without_double_underscores
               # model_id
               # model_ids
+              # model_name
               # model_name
               # model_name
               without_spaces_and_slashes.gsub(/_{2,}/, '_')
@@ -28,7 +30,17 @@ class Sequencer
               # model_ids
               # model___name
               # model_name
-              unsanitized_name.gsub(%r{[\s\/]}, '_').underscore
+              # model_name
+              transliterated.gsub(%r{[\s\/]}, '_').underscore
+            end
+
+            def transliterated
+              # Model ID
+              # Model IDs
+              # Model / Name
+              # Model Name
+              # Model Name
+              ::ActiveSupport::Inflector.transliterate(unsanitized_name, '_'.freeze)
             end
 
             def unsanitized_name
@@ -36,6 +48,9 @@ class Sequencer
               # Model IDs
               # Model / Name
               # Model Name
+              # rubocop:disable Style/AsciiComments
+              # Mödel Nâmé
+              # rubocop:enable Style/AsciiComments
               raise 'Missing implementation for unsanitized_name method'
             end
           end
