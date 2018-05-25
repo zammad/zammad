a989921b15101bd894d7f2d330061969141ee9d8
Mantas <mantas@idev.lt>
2018-05-25 12:04:32 +0300

    Follow up for #1990 improves tests

--- test/browser/agent_ticket_create_cc_tokenizer_test.rb
+++ test/browser/agent_ticket_create_cc_tokenizer_test.rb
@@ -5,7 +5,7 @@ require 'browser_test_helper'
 #
 # Ensure that CC field when creating a new ticket is autocompleting user emails
 
-class AgentTicketCreateResetCustomerSelectionTest < TestCase
+class AgentTicketCreateCcTokenizerTest < TestCase
   def test_tokenizer
     @browser = browser_instance
 
@@ -20,15 +20,14 @@ class AgentTicketCreateResetCustomerSelectionTest < TestCase
       css: 'a[href="#ticket/create"]'
     )
 
-    @browser.find_element(:css, 'li[data-type=email-out]').click
+    email_out_css = '.content.active li[data-type=email-out]'
 
-    elem = @browser.find_element(:name, 'cc')
-    elem.send_keys 'test@example.com'
-    elem.send_keys :enter
-
-    exists(
-      css: '.token-label',
-      value: 'test@example.com'
+    watch_for(
+      css: email_out_css
     )
+
+    click(css: email_out_css)
+
+    token_verify('.content.active input[name=cc]', 'test@example.com')
   end
 end
