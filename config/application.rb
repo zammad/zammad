20ea94d34b54a5537d22ac4e2c922101569af42c
Thorsten Eckel <te@znuny.com>
2018-06-25 16:20:09 +0200

    Fixed regression: Escalation logic is not active.

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Zammad
       'observer::_ticket::_ref_object_touch',
       'observer::_ticket::_online_notification_seen',
       'observer::_ticket::_stats_reopen',
+      'observer::_ticket::_escalation_update',
       'observer::_tag::_ticket_history',
       'observer::_user::_ref_object_touch',
       'observer::_user::_ticket_organization',
