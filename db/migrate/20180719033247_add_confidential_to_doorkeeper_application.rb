156d837573e87c5fe35678d5b0b1506e56b4e501
Ryan Lue <hello@ryanlue.com>
2018-07-19 11:37:08 +0800

    Update doorkeeper gem (security issue)

---
+++ db/migrate/20180719033247_add_confidential_to_doorkeeper_application.rb
@@ -0,0 +1,11 @@
+class AddConfidentialToDoorkeeperApplication < ActiveRecord::Migration[5.1]
+  def change
+    add_column(
+      :oauth_applications,
+      :confidential,
+      :boolean,
+      null: false,
+      default: true # maintaining backwards compatibility: require secrets
+    )
+  end
+end
