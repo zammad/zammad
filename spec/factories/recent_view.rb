6bc72e6b468dfeb8830ac91c860d6e05aad9d412
Ryan Lue <hello@ryanlue.com>
2018-09-10 20:38:33 +0800

    Migrate RecentView test from Minitest to RSpec

--- spec/factories/recent_view.rb
+++ spec/factories/recent_view.rb
@@ -1,9 +1,21 @@
 FactoryBot.define do
   factory :recent_view do
-    recent_view_object_id { ObjectLookup.by_name('User') }
-    o_id 1
-    created_by_id 1
-    created_at Time.zone.now
-    updated_at Time.zone.now
+    transient do
+      type { :ticket }
+      user_role { :agent }
+    end
+
+    recent_view_object_id { ObjectLookup.by_name(type.to_s.camelcase) }
+
+    # select a random record of the given object class
+    o_id do
+      type.to_s.camelcase.constantize.order('RANDOM()').first.id
+    end
+
+    # assign to an existing user, if possible
+    created_by_id do
+      User.find { |u| u.role?(user_role.capitalize) }&.id ||
+        create("#{user_role}_user").id
+    end
   end
 end
