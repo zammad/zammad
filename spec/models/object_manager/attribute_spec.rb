dc8748896d26e402259fed9316934c7fd3191e05
Billy Zhou <bz@zammad.com>
2018-09-14 22:32:02 +0800

    Fixed #2236 by adding instance_method_already_implemented? check for invalid attribute names (fixes #2236)

--- spec/models/object_manager/attribute_spec.rb
+++ spec/models/object_manager/attribute_spec.rb
@@ -49,4 +49,51 @@ RSpec.describe ObjectManager::Attribute, type: :model do
       end
     end
   end
+
+  describe 'check name' do
+    it 'rejects ActiveRecord reserved word "attribute"' do
+      expect do
+        ObjectManager::Attribute.add attributes_for :object_manager_attribute_text, name: 'attribute'
+      end.to raise_error 'attribute is a reserved word, please choose a different one'
+    end
+
+    it 'rejects Zammad reserved word "table"' do
+      expect do
+        ObjectManager::Attribute.add attributes_for :object_manager_attribute_text, name: 'table'
+      end.to raise_error 'table is a reserved word, please choose a different one'
+    end
+
+    it 'rejects duplicate attribute name of conflicting types' do
+      attribute = attributes_for :object_manager_attribute_text
+      ObjectManager::Attribute.add attribute
+      attribute[:data_type] = 'boolean'
+      expect do
+        ObjectManager::Attribute.add attribute
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'accepts duplicate attribute name on the same types (editing an existing attribute)' do
+      attribute = attributes_for :object_manager_attribute_text
+      ObjectManager::Attribute.add attribute
+      expect do
+        ObjectManager::Attribute.add attribute
+      end.to_not raise_error
+    end
+
+    it 'accepts duplicate attribute name on compatible types (editing the type of an existing attribute)' do
+      attribute = attributes_for :object_manager_attribute_text
+      ObjectManager::Attribute.add attribute
+      attribute[:data_type] = 'select'
+      attribute[:data_option_new] = { default: '', options: { 'a' => 'a' } }
+      expect do
+        ObjectManager::Attribute.add attribute
+      end.to_not raise_error
+    end
+
+    it 'accepts valid attribute names' do
+      expect do
+        ObjectManager::Attribute.add attributes_for :object_manager_attribute_text
+      end.to_not raise_error
+    end
+  end
 end
