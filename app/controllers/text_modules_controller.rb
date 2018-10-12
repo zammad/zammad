402b28b3e74ae3b901f5fbb488fe9cd653173609
Martin Edenhofer <me@edenhofer.de>
2018-10-12 11:36:46 +0200

    Fixed issue #2285 - Permission issue: response templates only usable when admin is also agent.

--- app/controllers/text_modules_controller.rb
+++ app/controllers/text_modules_controller.rb
@@ -49,7 +49,7 @@ curl http://localhost/api/v1/text_modules.json -v -u #{login}:#{password}
 =end
 
   def index
-    permission_check('ticket.agent')
+    permission_check(['admin.text_module', 'ticket.agent'])
     model_index_render(TextModule, params)
   end
 
@@ -71,7 +71,7 @@ curl http://localhost/api/v1/text_modules/#{id}.json -v -u #{login}:#{password}
 =end
 
   def show
-    permission_check('ticket.agent')
+    permission_check(['admin.text_module', 'ticket.agent'])
     model_show_render(TextModule, params)
   end
 
