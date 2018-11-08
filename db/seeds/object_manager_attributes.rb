cd65dcdb401a2cbc5dac5ec006dd41d28438071b
Billy Zhou <bz@zammad.com>
2018-11-08 20:53:41 +0800

    Fixed #2333 - Object country already exists by adding the corresponding country attribute to the existing country column

--- db/seeds/object_manager_attributes.rb
+++ db/seeds/object_manager_attributes.rb
@@ -967,6 +967,41 @@ ObjectManager::Attribute.add(
 ObjectManager::Attribute.add(
   force: true,
   object: 'User',
+  name: 'country',
+  display: 'Country',
+  data_type: 'input',
+  data_option: {
+    type: 'text',
+    maxlength: 100,
+    null: true,
+    item_class: 'formGroup--halfSize',
+  },
+  editable: true,
+  active: false,
+  screens: {
+    signup: {},
+    invite_agent: {},
+    invite_customer: {},
+    edit: {
+      '-all-' => {
+        null: true,
+      },
+    },
+    view: {
+      '-all-' => {
+        shown: true,
+      },
+    },
+  },
+  to_create: false,
+  to_migrate: false,
+  to_delete: false,
+  position: 1325,
+)
+
+ObjectManager::Attribute.add(
+  force: true,
+  object: 'User',
   name: 'address',
   display: 'Address',
   data_type: 'textarea',
