7ffa03731ececde057ab8f573b5700f640264a17
Thorsten Eckel <te@znuny.com>
2018-12-03 12:33:27 +0100

    Refactoring: Make sure Puma environment matches RAILS_ENV ENV.

--- config/puma.rb
+++ config/puma.rb
@@ -3,6 +3,8 @@ threads_count_min = Integer(ENV['MIN_THREADS'] || 5)
 threads_count_max = Integer(ENV['MAX_THREADS'] || 30)
 threads threads_count_min, threads_count_max
 
+environment ENV.fetch('RAILS_ENV', 'development')
+
 preload_app!
 
 on_worker_boot do
