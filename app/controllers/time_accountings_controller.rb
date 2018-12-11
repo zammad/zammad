3ca697dab768b2eaf6ae801b1e418f0b39e17396
Billy Zhou <bz@zammad.com>
2018-12-11 22:16:27 +0800

    Fixed #2398 - Missing custom object in database causes error on export in time_accounting

--- app/controllers/time_accountings_controller.rb
+++ app/controllers/time_accountings_controller.rb
@@ -165,6 +165,7 @@ class TimeAccountingsController < ApplicationController
       ]
       objects = ObjectManager::Attribute.where(editable: true,
                                                active: true,
+                                               to_create: false,
                                                object_lookup_id: ObjectLookup.lookup(name: 'Ticket').id)
                                         .pluck(:name, :display, :data_type, :data_option)
                                         .map { |name, display, data_type, data_option| { name: name, display: display, data_type: data_type, data_option: data_option } }
