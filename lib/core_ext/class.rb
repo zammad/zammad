b361b43836231896188ee5d39dffe4af5a5858a8
Thorsten Eckel <te@znuny.com>
2018-12-22 15:43:03 +0100

    Refactoring: Improving performance by memorizing app_model string generation.

--- lib/core_ext/class.rb
+++ lib/core_ext/class.rb
@@ -1,15 +1,15 @@
 class Class
   def to_app_model_url
-    camel_cased_word = to_s
-    camel_cased_word.gsub(/::/, '_')
-                    .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-                    .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-                    .tr('-', '_')
-                    .downcase
+    @to_app_model_url ||= begin
+      to_s.gsub(/::/, '_')
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+          .tr('-', '_')
+          .downcase
+    end
   end
 
   def to_app_model
-    camel_cased_word = to_s
-    camel_cased_word.gsub(/::/, '').to_sym
+    @to_app_model ||= to_s.gsub(/::/, '').to_sym
   end
 end
