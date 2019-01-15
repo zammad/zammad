3a5d597ab9b952e34fe9f0c8357a3c37f9189881
Thorsten Eckel <te@znuny.com>
2019-01-15 13:21:04 +0100

    Adding ActiveJob RSpec helper for using Test queue adapter.

---
+++ spec/support/active_job.rb
@@ -0,0 +1,38 @@
+module ZammadActiveJobHelper
+
+  delegate :enqueued_jobs, :performed_jobs, to: :queue_adapter
+
+  def queue_adapter
+    ::ActiveJob::Base.queue_adapter
+  end
+
+  def clear_jobs
+    enqueued_jobs.clear
+    performed_jobs.clear
+  end
+end
+
+RSpec.configure do |config|
+
+  activate_for = {
+    type:          :job, # actual Job examples
+    performs_jobs: true, # examples performing Jobs
+  }
+
+  activate_for.each do |key, value|
+    config.include ZammadActiveJobHelper, key => value
+    config.include RSpec::Rails::JobExampleGroup, key => value
+
+    config.around(:each, key => value) do |example|
+
+      default_queue_adapter           = ::ActiveJob::Base.queue_adapter
+      ::ActiveJob::Base.queue_adapter = :test
+
+      clear_jobs
+
+      example.run
+
+      ::ActiveJob::Base.queue_adapter = default_queue_adapter
+    end
+  end
+end
