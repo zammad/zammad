205a99c56a668bb14e2d74e64c021163db4aa78c
Ryan Lue <hello@ryanlue.com>
2019-01-23 14:42:54 +0800

    Fix checks_import_examples.rb when applied to non-importable classes

--- spec/models/application_model/checks_import_examples.rb
+++ spec/models/application_model/checks_import_examples.rb
@@ -1,17 +1,8 @@
 RSpec.shared_examples 'ApplicationModel::ChecksImport' do
   subject { build(described_class.name.underscore, id: unused_id) }
-  let(:unused_id) { described_class.pluck(:id).max * 2 }
+  let(:unused_id) { (described_class.pluck(:id).max || 1) * 2 }
 
-  context 'when Setting.get("system_init_done") is true AND Setting.get("import_mode") is false' do
-    before { Setting.set('system_init_done', true) }
-    before { Setting.set('import_mode', false) }
-
-    it 'prevents explicit setting of #id attribute' do
-      expect { subject.save }.to change { subject.id }
-    end
-  end
-
-  context 'when Setting.get("system_init_done") is false' do
+  context 'when Setting.get("system_init_done") is false (regardless of import_mode)' do
     before { Setting.set('system_init_done', false) }
 
     it 'allows explicit setting of #id attribute' do
@@ -19,21 +10,33 @@ RSpec.shared_examples 'ApplicationModel::ChecksImport' do
     end
   end
 
-  context 'when Setting.get("import_mode") is true' do
-    before { Setting.set('import_mode', true) }
+  context 'when Setting.get("system_init_done") is true' do
+    before { Setting.set('system_init_done', true) }
 
-    shared_examples 'importable classes' do
-      it 'allows explicit setting of #id attribute' do
-        expect { subject.save }.not_to change { subject.id }
-      end
-    end
+    context 'and Setting.get("import_mode") is false' do
+      before { Setting.set('import_mode', false) }
 
-    shared_examples 'non-importable classes' do
       it 'prevents explicit setting of #id attribute' do
         expect { subject.save }.to change { subject.id }
       end
     end
 
-    include_examples described_class.importable? ? 'importable classes' : 'non-importable classes'
+    context 'and Setting.get("import_mode") is true' do
+      before { Setting.set('import_mode', true) }
+
+      shared_examples 'importable classes' do
+        it 'allows explicit setting of #id attribute' do
+          expect { subject.save }.not_to change { subject.id }
+        end
+      end
+
+      shared_examples 'non-importable classes' do
+        it 'prevents explicit setting of #id attribute' do
+          expect { subject.save }.to change { subject.id }
+        end
+      end
+
+      include_examples described_class.importable? ? 'importable classes' : 'non-importable classes'
+    end
   end
 end
