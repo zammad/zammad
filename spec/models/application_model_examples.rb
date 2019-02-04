8e52cb52a8f94fe1468cd80b615a34044ee006ac
Ryan Lue <hello@ryanlue.com>
2019-02-04 17:47:35 +0800

    Remove unneeded options on ApplicationModel shared examples

--- spec/models/application_model_examples.rb
+++ spec/models/application_model_examples.rb
@@ -1,16 +1,5 @@
 require 'models/application_model/checks_import_examples'
 
 RSpec.shared_examples 'ApplicationModel' do
-  include_examples 'ApplicationModel::ChecksImport',
-                   importable: described_class.name.in?(
-                     %w[Group
-                        History
-                        Role
-                        Ticket
-                        Ticket::Article
-                        Ticket::Priority
-                        Ticket::State
-                        Ticket::StateType
-                        User]
-                   )
+  include_examples 'ApplicationModel::ChecksImport'
 end
