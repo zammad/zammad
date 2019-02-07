b3cb67836df67b3fbbe136c47b047df97368eb79
Ryan Lue <hello@ryanlue.com>
2019-02-07 22:46:16 +0800

    Refactoring: Migrate email_address_test.rb to RSpec

--- spec/factories/email_address.rb
+++ spec/factories/email_address.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :email_address do
-    sequence(:email) { |n| "zammad#{n}@localhost.com" }
+    association :channel, factory: :email_channel
+    sequence(:email)    { |n| "zammad#{n}@localhost.com" }
     sequence(:realname) { |n| "zammad#{n}" }
-    channel       { create(:email_channel) }
-    created_by_id 1
-    updated_by_id 1
+    created_by_id       { 1 }
+    updated_by_id       { 1 }
   end
 end
