31adff6a425342891cd3ae27ba3b5a1d04a3549e
Ryan Lue <hello@ryanlue.com>
2019-02-15 13:00:27 +0800

    Refactoring: Migrate ticket_article_time_accounting_test to RSpec

--- spec/factories/ticket/time_accounting.rb
+++ spec/factories/ticket/time_accounting.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
-  factory :ticket_time_accounting, class: Ticket::TimeAccounting do
+  factory :'ticket/time_accounting', aliases: %i[ticket_time_accounting] do
     ticket
     time_unit { rand(100) }
     created_by_id 1
+
+    trait :for_article do
+      ticket_article { create(:'ticket/article', ticket: ticket) }
+    end
   end
 end
---
+++ spec/models/ticket/time_accounting_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Ticket::TimeAccounting, type: :model do
+  subject(:time_accounting) { create(:'ticket/time_accounting') }
+
+  describe 'Associations:' do
+    describe '#ticket_article' do
+      subject!(:time_accounting) { create(:'ticket/time_accounting', :for_article) }
+
+      context 'when destroyed' do
+        it 'destroys self' do
+          expect { time_accounting.ticket_article.destroy }
+            .to change { time_accounting.persisted? }.to(false)
+            .and change { Ticket::TimeAccounting.count }.by(-1)
+        end
+
+        it 'does not destroy other TimeAccountings for same ticket' do
+          create(:'ticket/time_accounting', ticket: time_accounting.ticket)
+          create(:'ticket/time_accounting', :for_article, ticket: time_accounting.ticket)
+
+          expect { time_accounting.ticket_article.destroy }
+            .to change { Ticket::TimeAccounting.count }.by(-1)
+        end
+      end
+    end
+  end
+end
