7700817143537569f518e011129baf26fd1541d7
Ryan Lue <hello@ryanlue.com>
2019-03-14 19:03:20 +0800

    Refactoring: Migrate db_auto_increment to RSpec

--- spec/models/application_model_examples.rb
+++ spec/models/application_model_examples.rb
@@ -1,5 +1,6 @@
 require 'models/application_model/can_assets_examples'
 require 'models/application_model/can_associations_examples'
+require 'models/application_model/can_creates_and_updates_examples'
 require 'models/application_model/can_latest_change_examples'
 require 'models/application_model/can_lookup_examples'
 require 'models/application_model/checks_import_examples'
@@ -7,6 +8,7 @@ require 'models/application_model/checks_import_examples'
 RSpec.shared_examples 'ApplicationModel' do |options = {}|
   include_examples 'ApplicationModel::CanAssets', options[:can_assets]
   include_examples 'ApplicationModel::CanAssociations'
+  include_examples 'ApplicationModel::CanCreatesAndUpdates'
   include_examples 'ApplicationModel::CanLatestChange'
   include_examples 'ApplicationModel::CanLookup'
   include_examples 'ApplicationModel::ChecksImport'
