d3bbdd3b0061b8491cda2cb21aa55f7874f50676
Ryan Lue <hello@ryanlue.com>
2019-04-02 22:43:11 +0800

    Refactoring: Migrate organization_ref_object_touch_test to RSpec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -12,12 +12,58 @@ RSpec.describe Organization, type: :model do
   it_behaves_like 'HasXssSanitizedNote', model_factory: :organization
   it_behaves_like 'HasObjectManagerAttributesValidation'
 
-  describe '.where_or_cis' do
-    it 'finds instance by querying multiple attributes case insensitive' do
-      # search for Zammad Foundation
-      organizations = described_class.where_or_cis(%i[name note], '%zammad%')
-      expect(organizations).not_to be_blank
+  subject(:organization) { create(:organization) }
+
+  describe 'Class methods:' do
+    describe '.where_or_cis' do
+      it 'finds instance by querying multiple attributes case insensitive' do
+        # search for Zammad Foundation
+        organizations = described_class.where_or_cis(%i[name note], '%zammad%')
+        expect(organizations).not_to be_blank
+      end
     end
   end
 
+  describe 'Callbacks, Observers, & Async Transactions -' do
+    describe 'Touching associations on update:' do
+      let!(:member) { create(:customer_user, organization: organization) }
+      let!(:member_ticket) { create(:ticket, customer: member) }
+
+      context 'when basic attributes are updated' do
+        it 'touches its members and their tickets' do
+          expect { organization.update(name: 'foo') }
+            .to change { member.reload.updated_at }
+            .and change { member_ticket.reload.updated_at }
+        end
+      end
+
+      context 'when member associations are added' do
+        let(:user) { create(:customer_user) }
+
+        it 'is touched, and touches its other members (but not their tickets)' do
+          expect { organization.members.push(user) }
+            .to change { organization.reload.updated_at }
+            .and change { member.reload.updated_at }
+            .and not_change { member_ticket.reload.updated_at }
+        end
+      end
+
+      context 'with 100+ members' do
+        let!(:members) { create_list(:user, 101, organization: organization) }
+        let!(:member_ticket) { create(:ticket, customer: members.first) }
+
+        # This _should_ be split into two separate examples,
+        # but setup is slow and expensive.
+        it 'does not perform any association updates' do
+          expect { organization.update(name: 'foo') }
+            .to not_change { members.map(&:reload).map(&:updated_at) }
+            .and not_change { member_ticket.reload.updated_at }
+
+          expect { organization.members.push(member) }
+            .to not_change { organization.reload.updated_at }
+            .and not_change { members.map(&:reload).map(&:updated_at) }
+        end
+      end
+    end
+  end
 end
