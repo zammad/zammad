c1160c5ee847bda71acb7582f439c304f6a049d6
Thorsten Eckel <te@znuny.com>
2019-10-08 16:40:28 +0200

    Fixes #2768 - Unable to process email where group has option follow_up_possible: 'new_ticket' and arriving email has no subject header.

---
+++ spec/models/channel/filter/follow_up_possible_check_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Channel::Filter::FollowUpPossibleCheck, type: :channel_filter do
+
+  context 'new_ticket follow_up_possible for group' do
+
+    let(:group) { create(:group, follow_up_possible: 'new_ticket') }
+
+    context 'ticket closed' do
+      let(:ticket) { create(:ticket, group: group, state: Ticket::State.find_by(name: 'closed')) }
+
+      it 'prevents follow up' do
+        mail = {
+          'x-zammad-ticket-id': ticket.id
+        }
+
+        filter(mail)
+
+        expect(mail[:'x-zammad-ticket-id']).not_to eq(ticket.id)
+      end
+    end
+
+    context 'ticket open' do
+      let(:ticket) { create(:ticket, group: group, state: Ticket::State.find_by(name: 'new')) }
+
+      it 'allows follow up' do
+        mail = {
+          'x-zammad-ticket-id': ticket.id
+        }
+
+        filter(mail)
+
+        expect(mail[:'x-zammad-ticket-id']).to eq(ticket.id)
+      end
+    end
+  end
+end
