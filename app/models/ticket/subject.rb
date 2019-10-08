2ab17db3e42b68665ca49b2c6219550fce59819a
Martin Edenhofer <me@zammad.com>
2019-10-08 15:13:27 +0200

    Fixes #2768 - Unable to process email where group has option follow_up_possible: 'new_ticket' and arriving email has no subject header.

--- app/models/ticket/subject.rb
+++ app/models/ticket/subject.rb
@@ -50,6 +50,8 @@ returns
 =end
 
   def subject_clean(subject)
+    return '' if subject.blank?
+
     ticket_hook         = Setting.get('ticket_hook')
     ticket_hook_divider = Setting.get('ticket_hook_divider')
     ticket_subject_size = Setting.get('ticket_subject_size')
