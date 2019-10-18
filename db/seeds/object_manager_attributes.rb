33d546dc4bf3fb76b7920ec8cc4eda768576e8b8
Aleksandr Saprykin <47255226+Selfady@users.noreply.github.com>
2019-10-18 13:57:42 +0300

    Added alert and repeat_count

--- db/seeds/object_manager_attributes.rb
+++ db/seeds/object_manager_attributes.rb
@@ -323,6 +323,64 @@ ObjectManager::Attribute.add(
 )
 
 ObjectManager::Attribute.add(
+  force:       false,
+  object:      'Ticket',
+  name:        'alert',
+  display:     'alert',
+  data_type:   'input',
+  data_option: {
+    type:      'text',
+    maxlength: 2000,
+    null:      true,
+    translate: false,
+  },
+  editable:    true,
+  active:      true,
+  screens:     {
+    create_middle: {
+      '-all-' => {
+        null: false,
+      },
+    },
+    edit:       {},
+  },
+  to_create:   false,
+  to_migrate:  false,
+  to_delete:   false,
+  position:    101,
+)
+
+ObjectManager::Attribute.add(
+  force:       false,
+  object:      'Ticket',
+  name:        'repeat_count',
+  display:     'repeat_count',  
+  data_type:   'integer',
+  data_option: {
+    maxlength: 150,
+    null:      true,
+    note:      'The number of the same alerts fired',
+    min:       0,
+    max:       999_999_999,
+  },
+  editable:    true,
+  active:      true,
+  screens:     {
+    create_middle: {
+      '-all-' => {
+        null: false,
+      },
+    },
+    edit:       {},
+  },
+  to_create:   false,
+  to_migrate:  false,
+  to_delete:   false,
+  position:    102,
+)
+
+
+ObjectManager::Attribute.add(
   force:       true,
   object:      'TicketArticle',
   name:        'type_id',
