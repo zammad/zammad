649a862795ac8cf486431dfa420ee76d292ba2c8
kvitaly2005 <kvitaly2005@live.com>
2019-10-24 15:11:32 +0300

    logo update

--- db/seeds/triggers.rb
+++ db/seeds/triggers.rb
@@ -24,15 +24,15 @@ Trigger.create_or_update(
   },
   perform:       {
     'notification.email' => {
-      'body'      => '<div>Your request <b>(#{config.ticket_hook}#{ticket.number})</b> has been received and will be reviewed by support personel.</div>
+      'body'      => '<div>Your request <b>(#{config.ticket_hook}#{ticket.number})</b> has been received and will be reviewed by our support staff.</div>
 <br/>
-<div>To provide additional information, please reply to this email or click on the link (for initial login, please request a new password):
+<div>To provide additional information, please reply to this email or click on the following link (for initial login, please request a new password):
 <a href="#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}">#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}</a>
 </div>
 <br/>
-<div>VIAcode Incident Management System</div>
+<div>Your #{config.product_name} Team</div>
 <br/>
-<div><p>Let VIAcode deal with these alerts & manage your Azure cloud operation for free; <a href="https://www.viacode.com/services/azure-managed-services/?utm_source=product&utm_medium=email&utm_campaign=VIMS&utm_content=passwordchangeemail">activate here</a></p></div>',
+<div><i><a href="https://zammad.com">Zammad</a>, your customer support system</i></div>',
       'recipient' => 'article_last_sender',
       'subject'   => 'Thanks for your inquiry (#{ticket.title})', # rubocop:disable Lint/InterpolationCheck
     },
@@ -63,15 +63,15 @@ Trigger.create_or_update(
   },
   perform:       {
     'notification.email' => {
-      'body'      => '<div>Your follow up for <b>(#{config.ticket_hook}#{ticket.number})</b> has been received and will be reviewed by support personel.</div>
+      'body'      => '<div>Your follow up for <b>(#{config.ticket_hook}#{ticket.number})</b> has been received and will be reviewed by our support staff.</div>
 <br/>
 <div>To provide additional information, please reply to this email or click on the following link:
 <a href="#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}">#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}</a>
 </div>
 <br/>
-<div>VIAcode Incident Management System</div>
+<div>Your #{config.product_name} Team</div>
 <br/>
-<div><p>Let VIAcode deal with these alerts & manage your Azure cloud operation for free; <a href="https://www.viacode.com/services/azure-managed-services/?utm_source=product&utm_medium=email&utm_campaign=VIMS&utm_content=passwordchangeemail">activate here</a></p></div>',
+<div><i><a href="https://zammad.com">Zammad</a>, your customer support system</i></div>',
       'recipient' => 'article_last_sender',
       'subject'   => 'Thanks for your follow up (#{ticket.title})', # rubocop:disable Lint/InterpolationCheck
     },
@@ -99,9 +99,7 @@ Trigger.create_or_update(
 <a href="#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}">#{config.http_type}://#{config.fqdn}/#ticket/zoom/#{ticket.id}</a>
 </p>
 <br/>
-<div>VIAcode Incident Management System</div>
-</br>
-<div><p>Let VIAcode deal with these alerts & manage your Azure cloud operation for free; <a href="https://www.viacode.com/services/azure-managed-services/?utm_source=product&utm_medium=email&utm_campaign=VIMS&utm_content=passwordchangeemail">activate here</a></p></div>',
+<p><i><a href="https://zammad.com">Zammad</a>, your customer support system</i></p>',
       'recipient' => 'ticket_customer',
       'subject'   => 'Owner has changed (#{ticket.title})', # rubocop:disable Lint/InterpolationCheck
     },
