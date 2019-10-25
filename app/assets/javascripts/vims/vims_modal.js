530417069feb7ce67af9392f24cfc1e7c062ccf1
Georgy Plotnikov <gplotnikov@viacode.com>
2019-10-25 21:05:27 +0300

    update

---
+++ app/assets/javascripts/vims/vims_modal.js
@@ -0,0 +1,244 @@
+/*
+    A simple jQuery modal (http://github.com/kylefox/jquery-modal)
+    Version 0.9.1
+*/
+
+(function (factory) {
+    // Making your jQuery plugin work better with npm tools
+    // http://blog.npmjs.org/post/112712169830/making-your-jquery-plugin-work-better-with-npm
+    if(typeof module === "object" && typeof module.exports === "object") {
+      factory(require("jquery"), window, document);
+    }
+    else {
+      factory(jQuery, window, document);
+    }
+  }(function($, window, document, undefined) {
+  
+    var modals = [],
+        getCurrent = function() {
+          return modals.length ? modals[modals.length - 1] : null;
+        },
+        selectCurrent = function() {
+          var i,
+              selected = false;
+          for (i=modals.length-1; i>=0; i--) {
+            if (modals[i].$blocker) {
+              modals[i].$blocker.toggleClass('current',!selected).toggleClass('behind',selected);
+              selected = true;
+            }
+          }
+        };
+  
+    $.vims_modal = function(el, options) {
+      var remove, target;
+      this.$body = $('#vims');
+      this.options = $.extend({}, $.vims_modal.defaults, options);
+      this.options.doFade = !isNaN(parseInt(this.options.fadeDuration, 10));
+      this.$blocker = null;
+      if (this.options.closeExisting)
+        while ($.vims_modal.isActive())
+          $.vims_modal.close(); // Close any open modals.
+      modals.push(this);
+      if (el.is('a')) {
+        target = el.attr('href');
+        this.anchor = el;
+        //Select element by id from href
+        if (/^#/.test(target)) {
+          this.$elm = $(target);
+          if (this.$elm.length !== 1) return null;
+          this.$body.append(this.$elm);
+          this.open();
+        //AJAX
+        } else {
+          this.$elm = $('<div>');
+          this.$body.append(this.$elm);
+          remove = function(event, modal) { modal.elm.remove(); };
+          this.showSpinner();
+          el.trigger($.vims_modal.AJAX_SEND);
+          $.get(target).done(function(html) {
+            if (!$.vims_modal.isActive()) return;
+            el.trigger($.vims_modal.AJAX_SUCCESS);
+            var current = getCurrent();
+            current.$elm.empty().append(html).on($.vims_modal.CLOSE, remove);
+            current.hideSpinner();
+            current.open();
+            el.trigger($.vims_modal.AJAX_COMPLETE);
+          }).fail(function() {
+            el.trigger($.vims_modal.AJAX_FAIL);
+            var current = getCurrent();
+            current.hideSpinner();
+            modals.pop(); // remove expected modal from the list
+            el.trigger($.vims_modal.AJAX_COMPLETE);
+          });
+        }
+      } else {
+        this.$elm = el;
+        this.anchor = el;
+        this.$body.append(this.$elm);
+        this.open();
+      }
+    };
+  
+    $.vims_modal.prototype = {
+      constructor: $.vims_modal,
+  
+      open: function() {
+        var m = this;
+        this.block();
+        this.anchor.blur();
+        if(this.options.doFade) {
+          setTimeout(function() {
+            m.show();
+          }, this.options.fadeDuration * this.options.fadeDelay);
+        } else {
+          this.show();
+        }
+        $(document).off('keydown.modal').on('keydown.modal', function(event) {
+          var current = getCurrent();
+          if (event.which === 27 && current.options.escapeClose) current.close();
+        });
+        if (this.options.clickClose)
+          this.$blocker.click(function(e) {
+            if (e.target === this)
+              $.vims_modal.close();
+          });
+      },
+  
+      close: function() {
+        modals.pop();
+        this.unblock();
+        this.hide();
+        if (!$.vims_modal.isActive())
+          $(document).off('keydown.modal');
+      },
+  
+      block: function() {
+        this.$elm.trigger($.vims_modal.BEFORE_BLOCK, [this._ctx()]);
+        this.$body.css('overflow','hidden');
+        this.$blocker = $('<div class="' + this.options.blockerClass + ' vims-blocker current"></div>').appendTo(this.$body);
+        selectCurrent();
+        if(this.options.doFade) {
+          this.$blocker.css('opacity',0).animate({opacity: 1}, this.options.fadeDuration);
+        }
+        this.$elm.trigger($.vims_modal.BLOCK, [this._ctx()]);
+      },
+  
+      unblock: function(now) {
+        if (!now && this.options.doFade)
+          this.$blocker.fadeOut(this.options.fadeDuration, this.unblock.bind(this,true));
+        else {
+          this.$blocker.children().appendTo(this.$body);
+          this.$blocker.remove();
+          this.$blocker = null;
+          selectCurrent();
+          if (!$.vims_modal.isActive())
+            this.$body.css('overflow','');
+        }
+      },
+  
+      show: function() {
+        this.$elm.trigger($.vims_modal.BEFORE_OPEN, [this._ctx()]);
+        if (this.options.showClose) {
+          this.closeButton = $('<a href="#close-modal" rel="modal:close" class="close-modal ' + this.options.closeClass + '">' + this.options.closeText + '</a>');
+          this.$elm.append(this.closeButton);
+        }
+        this.$elm.addClass(this.options.modalClass).appendTo(this.$blocker);
+        if(this.options.doFade) {
+          this.$elm.css({opacity: 0, display: 'inline-block'}).animate({opacity: 1}, this.options.fadeDuration);
+        } else {
+          this.$elm.css('display', 'inline-block');
+        }
+        this.$elm.trigger($.vims_modal.OPEN, [this._ctx()]);
+      },
+  
+      hide: function() {
+        this.$elm.trigger($.vims_modal.BEFORE_CLOSE, [this._ctx()]);
+        if (this.closeButton) this.closeButton.remove();
+        var _this = this;
+        if(this.options.doFade) {
+          this.$elm.fadeOut(this.options.fadeDuration, function () {
+            _this.$elm.trigger($.vims_modal.AFTER_CLOSE, [_this._ctx()]);
+          });
+        } else {
+          this.$elm.hide(0, function () {
+            _this.$elm.trigger($.vims_modal.AFTER_CLOSE, [_this._ctx()]);
+          });
+        }
+        this.$elm.trigger($.vims_modal.CLOSE, [this._ctx()]);
+      },
+  
+      showSpinner: function() {
+        if (!this.options.showSpinner) return;
+        this.spinner = this.spinner || $('<div class="' + this.options.modalClass + '-spinner"></div>')
+          .append(this.options.spinnerHtml);
+        this.$body.append(this.spinner);
+        this.spinner.show();
+      },
+  
+      hideSpinner: function() {
+        if (this.spinner) this.spinner.remove();
+      },
+  
+      //Return context for custom events
+      _ctx: function() {
+        return { elm: this.$elm, $elm: this.$elm, $blocker: this.$blocker, options: this.options };
+      }
+    };
+  
+    $.vims_modal.close = function(event) {
+      if (!$.vims_modal.isActive()) return;
+      if (event) event.preventDefault();
+      var current = getCurrent();
+      current.close();
+      return current.$elm;
+    };
+  
+    // Returns if there currently is an active modal
+    $.vims_modal.isActive = function () {
+      return modals.length > 0;
+    };
+  
+    $.vims_modal.getCurrent = getCurrent;
+  
+    $.vims_modaldefaults = {
+      closeExisting: true,
+      escapeClose: true,
+      clickClose: true,
+      closeText: 'Close',
+      closeClass: '',
+      modalClass: "vims-modal",
+      blockerClass: "vims-blocker",
+      spinnerHtml: '<div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div>',
+      showSpinner: true,
+      showClose: true,
+      fadeDuration: null,   // Number of milliseconds the fade animation takes.
+      fadeDelay: 1.0        // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+    };
+  
+    // Event constants
+    $.vims_modal.BEFORE_BLOCK = 'vims-modal:before-block';
+    $.vims_modal.BLOCK = 'vims-modal:block';
+    $.vims_modal.BEFORE_OPEN = 'vims-modal:before-open';
+    $.vims_modal.OPEN = 'vims-modal:open';
+    $.vims_modal.BEFORE_CLOSE = 'vims-modal:before-close';
+    $.vims_modal.CLOSE = 'vims-modal:close';
+    $.vims_modal.AFTER_CLOSE = 'vims-modal:after-close';
+    $.vims_modal.AJAX_SEND = 'vims-modal:ajax:send';
+    $.vims_modal.AJAX_SUCCESS = 'vims-modal:ajax:success';
+    $.vims_modal.AJAX_FAIL = 'vims-modal:ajax:fail';
+    $.vims_modal.AJAX_COMPLETE = 'vims-modal:ajax:complete';
+  
+    $.fn.vims_modal = function(options){
+      if (this.length === 1) {
+        new $.vims_modal(this, options);
+      }
+      return this;
+    };
+  
+    // Automatically bind links with rel="modal:close" to, well, close the modal.
+    $(document).on('click.vims-modal', 'a[rel~="modal:close"]', $.vims_modal.close);
+    $(document).on('click.vims-modal', 'a[rel~="modal:open"]', function(event) {
+      event.preventDefault();
+      $(this).vims_modal();
+    });
+  }));
