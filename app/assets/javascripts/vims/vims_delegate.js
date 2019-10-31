ee72d08326b1416e2df1aac3b2ab9fae4751803c
Georgy Plotnikov <gplotnikov@viacode.com>
2019-10-31 18:16:52 +0300

    add modal for alerts

--- app/assets/javascripts/vims/vims_delegate.js
+++ app/assets/javascripts/vims/vims_delegate.js
@@ -2,7 +2,10 @@ class DelegateModal {
 
     static delegate(azInfo){
 		let url = 'https://vimsorchestrator.azurewebsites.net/api/azuredevops';
-		$.post( url, { azproject: azInfo.azProject, azarea: azInfo.azArea, aztoken: azInfo.azToken, vimsid: azInfo.vimsId } );
+		$.post( url, { azproject: azInfo.azProject, azarea: azInfo.azArea, aztoken: azInfo.azToken, vimsid: azInfo.vimsId }, function(data){
+			new AlertModal().show(data);
+			DelegateModal.DelegateIncident();		
+		});
     }
 
 	static DelegateIncident(){
@@ -13,8 +16,7 @@ class DelegateModal {
 }
 
 DelegateModal.html = `
-<div id="vims">
-	<div id="delegateModal" class="vims-modal">
+	<div id="vims-delegateModal" class="vims-modal">
 		<p>Azure project: <input type="text" id="vims-az-project"/></p>
 		<p>Azure project area: <input type="text" id="vims-az-project-area"/></p>
 		<p>Azure access token: <input type="text" id="vims-az-token"/></p>
@@ -25,8 +27,9 @@ DelegateModal.html = `
 			<input type="button" value="Ok" onclick="SendDelegation()"/>
 		</p>
 	</div>
-</div>
 `;
+
+DelegateModal.modalElement = any;
 
 DelegateModal.css = '<link id="cssModal" rel="stylesheet" href="https://combinatronics.com/GeorgePlotnikov/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_modal.css" />';
 
@@ -38,7 +41,6 @@ function SendDelegation(){
 	azInfo.vimsId = document.URL.substr(document.URL.lastIndexOf('/') + 1);
 
 	DelegateModal.delegate(azInfo);
-	DelegateModal.DelegateIncident();
 	$.vims_modal.close();
 }
 
@@ -47,4 +49,25 @@ class AzDevOpsConnectionInfo {
 	azProject = '';
 	azArea = '';
 	vimsId = 0;
+}
+
+class AlertModal {
+	
+	alertModalHtml = `
+	<div id="vims-alertModal" class="vims-modal">
+		<span id="vims-alertModal-text"></span>
+		<p>
+			<input type="button" value="Ok" onclick="SendDelegation()"/>
+		</p>
+	</div>
+	`;
+
+	show(text){
+		$('#vims').append(alertModalHtml);
+		$('#vims-alertModal-text').val(text);
+		$('#vims-alertModal').vims_modal();		
+		$('#vims-alertModal').on($.vims_modal.AFTER_CLOSE, function(event, modal){
+			$('#vims-alertModal').remove();
+		});
+	}
 }
