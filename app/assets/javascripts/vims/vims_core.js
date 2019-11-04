1ec3d8db6bb0875a699a55a1c69bd39f12f136fa
Georgy Plotnikov <gplotnikov@viacode.com>
2019-11-04 19:26:41 +0300

    add settings support

--- app/assets/javascripts/vims/vims_core.js
+++ app/assets/javascripts/vims/vims_core.js
@@ -1,12 +1,12 @@
 (function() {	
 	let modalJQueryScript = document.createElement("SCRIPT");
 	modalJQueryScript.type = 'text/javascript';
-	modalJQueryScript.src = "https://combinatronics.com/GeorgePlotnikov/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_modal.js";
+	modalJQueryScript.src = "https://combinatronics.com/viacode/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_modal.js";
 	modalJQueryScript.defer = "defer";
 	$('body').append(modalJQueryScript);	
 
 	let vimsDelegateScript = document.createElement("SCRIPT");
-	vimsDelegateScript.src = "https://combinatronics.com/GeorgePlotnikov/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_delegate.js";
+	vimsDelegateScript.src = "https://combinatronics.com/viacode/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_delegate.js";
 	vimsDelegateScript.type = 'text/javascript';
 	vimsDelegateScript.defer = "defer";
 	$('body').append(vimsDelegateScript);		
@@ -18,7 +18,7 @@
 
 	observer.observe((document.documentElement || document.body), {
 		childList: true,
-    	subtree: true,
+    subtree: true,
 		characterDataOldValue: false // pass old data to callback
 	});
 
@@ -33,15 +33,20 @@
 				continue;
 			}
 			console.log("menu found");
-			menu.append('<li><a id="vimsDelegateLi" role="menuitem" tabindex="-1" href="#delegateModal" rel="vims-modal:open">Delegate</a></li>');
+			menu.append('<li><a id="vimsDelegateLi" role="menuitem" tabindex="-1" href="#vimsDelegateModal" rel="vims-modal:open">Delegate</a></li>');
 		}	
 		try {
 			if(DelegateModal){
 			  if($('#cssModal').length == 0){
 				  $('head').append(DelegateModal.css);
 			  }
-			  if($('#delegateModal').length == 0){
-				$('body').append(DelegateModal.html);
+			  if($('#vimsDelegateModal').length == 0){
+				$('#vims').append(DelegateModal.html);
+				$.getJSON('https://vimsorchestrator.azurewebsites.net/api/azuredevopssettings', function(response){
+					$("#vims-az-token").val(response.azToken);
+					$("#vims-az-project").val(response.azProject);
+					$("#vims-az-project-area").val(response.azArea);
+				});
 			  }	
 			}
 		} catch (e) {
--- app/assets/javascripts/vims/vims_delegate.js
+++ app/assets/javascripts/vims/vims_delegate.js
@@ -1,50 +1,65 @@
 class DelegateModal {
 
-  static delegate(azInfo){
-		let url = 'https://vimsorchestrator.azurewebsites.net/api/azuredevops';
-		$.post( url, { azproject: azInfo.azProject, azarea: azInfo.azArea, aztoken: azInfo.azToken, vimsid: azInfo.vimsId } );
-    }
-
-	static DelegateIncident(){
-		var stateDd = $('[name="vims_status"]');
-		stateDd.val('delegated');
-		stateDd.change();
+	static delegate(azInfo, needSaveSettings){
+		  let url = 'https://vimsorchestrator.azurewebsites.net/api/azuredevops';
+		  $.post( url, { azproject: azInfo.azProject, azarea: azInfo.azArea, aztoken: azInfo.azToken, vimsid: azInfo.vimsId, saveSettings: needSaveSettings }, function(data){
+			  new AlertModal().show(data);
+			  DelegateModal.DelegateIncident();		
+		  });
 	}
-}
-
-DelegateModal.html = `
-<div id="vims">
-	<div id="delegateModal" class="vims-modal">
-		<p>Azure project: <input type="text" id="vims-az-project"/></p>
-		<p>Azure project area: <input type="text" id="vims-az-project-area"/></p>
-		<p>Azure access token: <input type="text" id="vims-az-token"/></p>
-		<p>Save settings &nbsp;<input type="checkbox" id="vims-save-settings"/></p>
-		<p>
-			<a href="#" rel="vims-modal:close">Close</a>
-			&nbsp;
-			<input type="button" value="Ok" onclick="SendDelegation()"/>
-		</p>
-	</div>
-</div>
-`;
-
-DelegateModal.css = '<link id="cssModal" rel="stylesheet" href="https://combinatronics.com/GeorgePlotnikov/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_modal.css" />';
-
-function SendDelegation(){
-	let azInfo = new AzDevOpsConnectionInfo();
-	azInfo.azToken = $("#vims-az-token").val();
-	azInfo.azProject = $("#vims-az-project").val();
-	azInfo.azArea = $("#vims-az-project-area").val();
-	azInfo.vimsId = document.URL.substr(document.URL.lastIndexOf('/') + 1);
-
-	DelegateModal.delegate(azInfo);
-	DelegateModal.DelegateIncident();
-	$.vims_modal.close();
-}
-
-class AzDevOpsConnectionInfo {
-	azToken = '';
-	azProject = '';
-	azArea = '';
-	vimsId = 0;
-}
+	
+	  static DelegateIncident(){
+		  var stateDd = $('[name="vims_status"]');
+		  stateDd.val('delegated');
+		  stateDd.change();
+	  }
+  }
+  
+  DelegateModal.html = `
+  <div id="vims">
+	  <div id="vimsDelegateModal" class="vims-modal">
+		  <p>Azure project: <input type="text" id="vims-az-project"/></p>
+		  <p>Azure project area: <input type="text" id="vims-az-project-area"/></p>
+		  <p>Azure access token: <input type="password" id="vims-az-token"/></p>
+		  <p>Save settings &nbsp;<input type="checkbox" id="vims-save-settings"/></p>
+		  <p>
+			  <a href="#" rel="vims-modal:close">Close</a>
+			  &nbsp;
+			  <input type="button" value="Ok" onclick="SendDelegation()"/>
+		  </p>
+	  </div>
+  </div>
+  `;
+  
+  DelegateModal.css = '<link id="cssModal" rel="stylesheet" href="https://combinatronics.com/GeorgePlotnikov/VIAcode-Incident-Management-System/develop/app/assets/javascripts/vims/vims_modal.css" />';
+  
+  function SendDelegation(){
+	  let azInfo = new AzDevOpsConnectionInfo();
+	  azInfo.azToken = $("#vims-az-token").val();
+	  azInfo.azProject = $("#vims-az-project").val();
+	  azInfo.azArea = $("#vims-az-project-area").val();
+	  azInfo.vimsId = document.URL.substr(document.URL.lastIndexOf('/') + 1);
+  
+	  DelegateModal.delegate(azInfo, $('#vims-save-settings').prop('checked'));
+	  $.vims_modal.close();
+  }
+  
+  class AzDevOpsConnectionInfo {
+	  azToken = '';
+	  azProject = '';
+	  azArea = '';
+	  vimsId = 0;  
+  }
+  
+  class AlertModal {
+	  
+	  show(text){
+		  $('#vims').append('<div id="vims-alertModal" class="vims-modal"><span id="vims-alertModal-text"></span></div>');
+		  $('#vims-alertModal-text').text(text);
+		  $('#vims-alertModal').vims_modal();		
+		  $('#vims-alertModal').on($.vims_modal.AFTER_CLOSE, function(event, modal){
+			  $('#vims-alertModal').remove();
+		  });
+	  }
+  }
+  
