40d7be74607b283fbc72b0745786cbf79aa30d65
Vladimir Machnevich <VMachnevich@VIACODE.COM>
2019-12-04 23:41:58 +0300

    fix elastic search 7.5

--- db/seeds/roles_groups.rb
+++ db/seeds/roles_groups.rb
@@ -1,1 +1,2 @@
 RoleGroup.create_if_not_exists(role: Role.find_by(name: 'Agent'), group: Group.find_by(name: 'Incoming'))
+RoleGroup.create_if_not_exists(role: Role.find_by(name: 'Connector'), group: Group.find_by(name: 'Incoming'))
