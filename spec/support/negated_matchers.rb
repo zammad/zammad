64c882b090941d79152e370a2612ef534027d23f
Ryan Lue <rl@zammad.com>
2019-12-18 14:39:45 +0800

    Refactoring: Add coverage for TwitterSync#process_webhook
    Prior to this commit,
    the TwitterSync#process_webhook method had only partial test coverage
    (in `spec/requests/integration/twitter_webhook_spec.rb`).
    This commit replaces the existing, incomplete tests
    with a direct spec of the TwitterSync class,
    giving individual test cases for all reasonable edge cases
    of the #process_webhook method.
    
    Since this method is responsible for a lot of complex behavior,
    there are a _lot_ of such test cases.
    Putting them all in the same spec file makes it hard to read through,
    so a future goal is to break them up into smaller methods
    and move some of these methods into other, more appropriate classes.
    
    This could not be achieved with a request spec,
    since they enforce testing at a single interface /
    level of abstraction (i.e., HTTP requests).
    
    === Note: This commit will not pass CI.
    
    Some bugs in the existing code were uncovered
    in the process of adding comprehensive test coverage
    for the #process_webhook method.
    
    In the interest of isolating changes into discrete chunks, fixes for
    these bugs have been reserved for the next commit.

--- spec/support/negated_matchers.rb
+++ spec/support/negated_matchers.rb
@@ -1,3 +1,4 @@
 RSpec::Matchers.define_negated_matcher :not_change, :change
 RSpec::Matchers.define_negated_matcher :not_include, :include
 RSpec::Matchers.define_negated_matcher :not_eq, :eq
+RSpec::Matchers.define_negated_matcher :not_raise_error, :raise_error
