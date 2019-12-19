8868edbc54879cfc4e9f54a215ee039c2dfe3846
Andrey Epifanov <aepifanov@viacode.com>
2019-12-19 15:30:30 +0300

    Adde admin.system permission to Connector role

---
+++ db/migrate/20191219122800_add_system_permission_to_connector_role.rb
@@ -0,0 +1,10 @@
+class AddSystemPermissionToConnectorRole < ActiveRecord::Migration[5.1]
+    def up
+        
+        return if !Setting.find_by(name: 'system_init_done')
+        
+        connectorRole = Role.find_by(name: 'Connector')
+        connectorRole.permission_grant('admin.system')
+        connectorRole.save!
+    end
+end
