c7ab3e1fe2238299e9bd5f59a9669387fd782a0f
Mantas Masalskis <mm@zammad.com>
2020-02-19 12:43:58 +0100

    Github PR #2935 - Fixes #2781 - Users can't remove their Sessions from Device list.

---
+++ spec/system/profile/devices_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Profile > Devices', type: :system, authenticated: true do
+  subject!(:device) { create(:user_device, user_id: User.find_by(login: 'master@example.com').id) }
+
+  it 'allows to remove device' do
+    visit 'profile/devices'
+
+    within(:active_content) do
+      find('td', text: device.name)
+        .ancestor('tr')
+        .find('.settings-list-control')
+        .click
+    end
+
+    wait(5).until { expect(page).to have_no_css('td', text: device.name) }
+  end
+end
