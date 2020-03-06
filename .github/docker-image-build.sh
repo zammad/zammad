584680e7e483bc2cc4227ec1af83ea7f8b195d6d
André Bauer <monotek23@gmail.com>
2020-03-06 13:10:59 +0100

    Follow up - 0798e7d47ed2505ea43dccdb9c1ef867d700f720 - moved from circle ci to github actions. Extract REPO_USER to GitHub secret to make it reusable in Zammad Docker Omnibus and Compose repository.

--- .github/docker-image-build.sh
+++ .github/docker-image-build.sh
@@ -7,7 +7,6 @@ set -o pipefail
 
 DOCKER_REGISTRY="index.docker.io"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-REPO_USER="zammad"
 ZAMMAD_VERSION="$(git describe --tags | sed -e 's/-[a-z0-9]\{8,\}.*//g')"
 
 # dockerhub auth
@@ -21,7 +20,7 @@ cd "${REPO_ROOT}/${DOCKER_GITHUB_REPOSITORY}"
 
 # shellcheck disable=SC2153
 for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
-  echo "Build Zammad Docker image ${DOCKER_IMAGE} with version ${ZAMMAD_VERSION} for DockerHubs ${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE} repo"
+  echo "Build Zammad Docker image ${DOCKER_IMAGE} with version ${ZAMMAD_VERSION} for DockerHubs ${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY} repo"
 
   if [ "${DOCKER_REPOSITORY}" == "zammad-docker-compose" ]; then
     docker build --pull --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE}" -t "${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE}-latest" -t "${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE}-${ZAMMAD_VERSION}" -f "containers/${DOCKER_IMAGE}/Dockerfile" .
