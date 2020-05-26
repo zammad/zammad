zammad/zammad

commit becbdb1baa58805422f5214b589502f8d3807014

Refactoring: Replaced home-rolled authorization logic in Controllers with Pundit.

app/controllers/application_controller/authenticates.rb
@@ -4,6 +4,8 @@ module ApplicationController::Authenticates
   private
 
   def permission_check(key)
+    ActiveSupport::Deprecation.warn("Method 'permission_check' is deprecated. Use Pundit policy and `authorize!` instead.")
+
     if @_token_auth
       user = Token.check(
         action:     'api',
@@ -76,6 +78,8 @@ module ApplicationController::Authenticates
         inactive_user: true,
       )
       if user && auth_param[:permission]
+        ActiveSupport::Deprecation.warn("Paramter ':permission' is deprecated. Use Pundit policy and `authorize!` instead.")
+
         user = Token.check(
           action:        'api',
           name:          token_string,
@@ -95,6 +99,8 @@ module ApplicationController::Authenticates
            Time.zone.today >= token.expires_at
           raise Exceptions::NotAuthorized, 'Not authorized (token expired)!'
         end
+
+        @_token = token # remember for Pundit authorization / permit!
       end
 
       @_token_auth = token_string # remember for permission_check
@@ -152,7 +158,14 @@ module ApplicationController::Authenticates
   def authentication_check_prerequesits(user, auth_type, auth_param)
     raise Exceptions::NotAuthorized, 'Maintenance mode enabled!' if in_maintenance_mode?(user)
     raise Exceptions::NotAuthorized, 'User is inactive!' if !user.active
-    raise Exceptions::NotAuthorized, 'Not authorized (user)!' if auth_param[:permission] && !user.permissions?(auth_param[:permission])
+
+    if auth_param[:permission]
+      ActiveSupport::Deprecation.warn("Parameter ':permission' is deprecated. Use Pundit policy and `authorize!` instead.")
+
+      if !user.permissions?(auth_param[:permission])
+        raise Exceptions::NotAuthorized, 'Not authorized (user)!'
+      end
+    end
 
     current_user_set(user, auth_type)
     user_device_log(user, auth_type)

commit 95de420a35109d89b7399b6a5da0077d32127239

Fixes #3064: Double-render error on HTTP Basic auth
This commit fixes a regression[0]
introduced in becbdb1ba (the Pundit migration).

Specifically, `CalendarSubscriptionsController` appears to be
the only controller that supports HTTP Basic authentication
(for calendar clients like Thunderbird Lightning or Calendar.app),
and the migration changed the control flow
for HTTP Basic authentication + authorization:

    # before
    authentication_check({ basic_auth_promt: true, permission: 'user_preferences.calendar' })

    # after
    authentication_check(basic_auth_promt: true) && authorize!

After this change, `#authentication_check` is expected
to communicate success or failure through its return value,
but prior to this bugfix, its return value was always truthy.
This led to a double-render error,
where a response code and message were set twice,
upon the failures of both authentication and authorization.

This fix adds a `return false` in the authorization failure case,
short-circuiting the `#authorize!` call and eliminating the error.

[0]: https://github.com/zammad/zammad/issues/3064

app/controllers/application_controller/authenticates.rb
@@ -27,7 +27,8 @@ module ApplicationController::Authenticates
 
     # check if basic_auth fallback is possible
     if auth_param[:basic_auth_promt] && !user
-      return request_http_basic_authentication
+      request_http_basic_authentication
+      return false
     end
 
     # return auth not ok

spec/requests/calendar_subscriptions_spec.rb
@@ -1,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'iCal endpoints', type: :request do
+  context 'with no existing session' do
+    it 'gives HTTP Basic auth prompt (#3064)' do
+      get '/ical/tickets'
+
+      expect(response.body).to eq("HTTP Basic: Access denied.\n")
+    end
+  end
+end