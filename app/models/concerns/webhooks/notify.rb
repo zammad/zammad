2c5562a1a8a45815129f68d01f27d64b96452d89
Emerson Almeida <emersonalmeidax@gmail.com>
2020-09-15 17:35:29 -0300

    add tests to trigges webhooks

--- app/models/concerns/webhooks/notify.rb
+++ app/models/concerns/webhooks/notify.rb
@@ -6,19 +6,32 @@ module Webhooks
     included do
       after_create   :schedule_webhook_notification_create
       after_update   :schedule_webhook_notification_update
-      before_destroy :schedule_webhook_notification_destroy
+      after_destroy :schedule_webhook_notification_destroy
     end
 
     def schedule_webhook_notification_create
-
+      schedule_webhook_notification('created')
     end
 
     def schedule_webhook_notification_update
-
+      schedule_webhook_notification('updated')
     end
 
     def schedule_webhook_notification_destroy
+      schedule_webhook_notification('destroyed')
+    end
 
+    private
+
+    def schedule_webhook_notification(type)
+      Webhook.find_each do |webhook|
+        Webhooks::NotificationJob.perform_later(
+          resource_type: self.class.name.underscore,
+          resource_id:   id,
+          webhook_id:    webhook.id,
+          event:         type
+        )
+      end
     end
   end
 end
--- spec/models/concerns/trigges_webhooks_examples.rb
+++ spec/models/concerns/trigges_webhooks_examples.rb
@@ -1,32 +1,53 @@
 RSpec.shared_examples 'TriggesWebhooks' do
   subject { create(described_class.name.underscore) }
 
-  context "when a webhook are created" do
-    describe 'call the webhook URLs' do
-      context 'on creation', performs_jobs: true do
-        it 'schedules the webhooks notification job' do
-          expect { subject }.to have_enqueued_job(Webhooks::NotificationJob)
-        end
+  context 'when a webhook are created' do
+    let(:webhook) { create(:webhook) }
+
+    context 'on creation', performs_jobs: true do
+      it 'schedules the webhooks notification job' do
+        expect { subject }.to have_enqueued_job(Webhooks::NotificationJob).with(resource_type: described_class.name.underscore, resource_id: anything, webhook_id: webhook.id, event: 'created')
       end
+    end
 
-      context 'on updation', performs_jobs: true do
-        it 'schedules the webhooks notification job' do
-          subject
+    context 'on updation', performs_jobs: true do
+      it 'schedules the webhooks notification job' do
+        subject
 
-          expect { subject.update(subject: "My new subject") }.to have_enqueued_job(Webhooks::NotificationJob)
-        end
+        expect { subject.update(title: 'My new title') }.to have_enqueued_job(Webhooks::NotificationJob).with(resource_type: described_class.name.underscore, resource_id: subject.id, webhook_id: webhook.id, event: 'updated')
       end
+    end
 
-      context 'on destroyng', performs_jobs: true do
-        it 'schedules the webhooks notification job' do
-          subject
+    context 'on destroyng', performs_jobs: true do
+      it 'schedules the webhooks notification job' do
+        subject
 
-          expect { subject.destroy }.to have_enqueued_job(Webhooks::NotificationJob)
-        end
+        expect { subject.destroy }.to have_enqueued_job(Webhooks::NotificationJob).with(resource_type: described_class.name.underscore, resource_id: subject.id, webhook_id: webhook.id, event: 'destroyed')
       end
     end
   end
 
-  context "when no webhook are created" do
+  context 'when no webhook are created' do
+    context 'on creation', performs_jobs: true do
+      it 'does not schedule webhooks notification job' do
+        expect { subject }.not_to have_enqueued_job(Webhooks::NotificationJob)
+      end
+    end
+
+    context 'on updation', performs_jobs: true do
+      it 'does not schedule webhooks notification job' do
+        subject
+
+        expect { subject.update(title: 'My new title') }.not_to have_enqueued_job(Webhooks::NotificationJob)
+      end
+    end
+
+    context 'on destroyng', performs_jobs: true do
+      it 'does not schedule webhooks notification job' do
+        subject
+
+        expect { subject.destroy }.not_to have_enqueued_job(Webhooks::NotificationJob)
+      end
+    end
   end
 end
