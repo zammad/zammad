c4c43b3df43e0df30fc453580b730586cd143ca6
Rolf Schmidt <rolf.schmidt@znuny.com>
2020-09-15 09:35:49 +0200

    Fixes #3191 - data privacy obsolete column name.

---
+++ db/migrate/20200911070327_data_privacy_delete_name.rb
@@ -0,0 +1,9 @@
+class DataPrivacyDeleteName < ActiveRecord::Migration[5.2]
+  def change
+
+    # return if it's a new setup
+    return if !Setting.exists?(name: 'system_init_done')
+
+    remove_column :data_privacy_tasks, :name, :string, limit: 150
+  end
+end
