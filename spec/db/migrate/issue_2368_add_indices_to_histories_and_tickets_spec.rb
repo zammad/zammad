3533339e7bc88e0b55c23c2279f0c8fed04c8730
Thorsten Eckel <te@zammad.com>
2020-09-23 09:48:34 +0200

    Maintenance: Leaking DB changes of tests cause following tests to fail.

--- spec/db/migrate/issue_2368_add_indices_to_histories_and_tickets_spec.rb
+++ spec/db/migrate/issue_2368_add_indices_to_histories_and_tickets_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Issue2368AddIndicesToHistoriesAndTickets, type: :db_migration do
-  self.use_transactional_tests = false # see comments on #without_index method
+RSpec.describe Issue2368AddIndicesToHistoriesAndTickets, type: :db_migration, db_strategy: :reset do
 
   before { without_index(table, column: columns) }
 
