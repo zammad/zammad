2ffa821d659e8bd602e4d6188e9d13e5bd191394
Mantas <mantas@idev.lt>
2020-10-22 15:52:26 +0300

    Maintenance: Applied workaround to Ticket Macro QUnit test to avoid failure leading up to DST switch.

--- public/assets/tests/ticket_macro.js
+++ public/assets/tests/ticket_macro.js
@@ -1,5 +1,5 @@
 test( "ticket macro pending time check", function() {
-  var test_relative = function(rules, target, description){
+  var test_relative = function(rules, target, description, offset){
     var ticket = new App.Ticket()
 
     App.Ticket.macro({
@@ -12,10 +12,13 @@ test( "ticket macro pending time check", function() {
     var compare_against = new Date()
     var travel = Math.abs( new Date(ticket.pending_time) - compare_against)
 
-    var diff = Math.abs(target - travel)
+    var diff = Math.abs(target - travel) - offset*1000
 
     ok(diff < 1000, description)
   }
+
+  var data = document.getElementsByTagName('data')[0].dataset
+  debugger
 
   var rules = {
     operator: "relative",
@@ -23,7 +26,7 @@ test( "ticket macro pending time check", function() {
     value: 5
   }
 
-  test_relative(rules, 60 * 60 * 24 * 5 * 1000, '5 days')
+  test_relative(rules, 60 * 60 * 24 * 5 * 1000, '5 days', data['offset-5Days'])
 
   var rules = {
     operator: "relative",
@@ -31,7 +34,7 @@ test( "ticket macro pending time check", function() {
     value: 3
   }
 
-  test_relative(rules, 60 * 3 * 1000, '5 minutes')
+  test_relative(rules, 60 * 3 * 1000, '5 minutes', data['offset-3Minutes'])
 
   var rules = {
     operator: "relative",
@@ -39,5 +42,5 @@ test( "ticket macro pending time check", function() {
     value: 10
   }
 
-  test_relative(rules, 60 * 60 * 10 * 1000, '10 hours')
+  test_relative(rules, 60 * 60 * 10 * 1000, '10 hours', data['offset-10Hours'])
 })
