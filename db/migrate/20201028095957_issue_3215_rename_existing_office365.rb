8e479ef4eea8e314433c4457531bb726e71e8c6c
MrGeneration <github@thehomeofanime.de>
2020-10-28 11:29:45 +0100

    Follow up - a9984adbc530c27c769ae6153ec6fd984d938e6e - Fixes #3215: Channel attribute `name` does not exist and should be `area` instead.

--- db/migrate/20201028095957_issue_3215_rename_existing_office365.rb
+++ db/migrate/20201028095957_issue_3215_rename_existing_office365.rb
@@ -7,7 +7,7 @@ class Issue3215RenameExistingOffice365 < ActiveRecord::Migration[5.2]
     # migrate existing Channels and ExternalCredential to new name
 
     # rubocop:disable Rails/SkipsModelValidations
-    Channel.where(area: 'Office365::Account').update_all(name: 'Microsoft365::Account')
+    Channel.where(area: 'Office365::Account').update_all(area: 'Microsoft365::Account')
     ExternalCredential.where(name: 'office365').update_all(name: 'microsoft365')
     # rubocop:enable Rails/SkipsModelValidations
   end
