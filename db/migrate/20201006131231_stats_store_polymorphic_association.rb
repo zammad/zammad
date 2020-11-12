f7de6855c987b285d6f26a95b3009c7a47068014
Thorsten Eckel <te@zammad.com>
2020-11-12 12:14:37 +0100

    Follow up - c3fce714dc2ae15785db66950eeaf60d27e1ea8b - Running Scheduler has outdated column information and disables Stats.generate job.

--- db/migrate/20201006131231_stats_store_polymorphic_association.rb
+++ db/migrate/20201006131231_stats_store_polymorphic_association.rb
@@ -8,13 +8,21 @@ class StatsStorePolymorphicAssociation < ActiveRecord::Migration[5.2]
     # create empty, indexed polymorphic association columns
     add_reference :stats_stores, :stats_storable, polymorphic: true, index: true
 
+    # set last run 20 min in future to avoid scheduler errors until restart
+    Scheduler.find_by(method: 'Stats.generate').update(last_run: 20.minutes.from_now)
+
     # migrate column data in the most performance way
     object_lookup_map.each do |id, model|
       StatsStore.where(stats_store_object_id: id)
                 .update_all("stats_storable_id = o_id, stats_storable_type = '#{model}'") # rubocop:disable Rails/SkipsModelValidations
     end
 
-    # rubocop:disable Rails/ReversibleMigration
+    remove_unneeded_columns
+  end
+
+  private
+
+  def remove_unneeded_columns
     # remove home made "polymorphic association" columns
     remove_column :stats_stores, :o_id
     remove_column :stats_stores, :stats_store_object_id
@@ -22,6 +30,5 @@ class StatsStorePolymorphicAssociation < ActiveRecord::Migration[5.2]
     # remove unused/obsolete columns
     remove_column :stats_stores, :related_stats_store_object_id
     remove_column(:stats_stores, :related_o_id) if StatsStore.column_names.include?('related_o_id')
-    # rubocop:enable Rails/ReversibleMigration
   end
 end
