22bc5de409ebfd9c99f9345ed35c07b593819aec
Mantas <mantas@idev.lt>
2020-11-18 17:48:45 +0200

    Fixes #3291 - Date and Datetime widgets can not be disabled

--- public/assets/tests/form_datetime.js
+++ public/assets/tests/form_datetime.js
@@ -11,18 +11,25 @@ test("DateTime timepicker focuses hours", function(assert) {
     model:     {
       configure_attributes: [
         { name: 'datetime1', display: 'Datetime1', tag: 'datetime', null: true },
+        { name: 'datetime2', display: 'Datetime2', tag: 'datetime', null: true, disabled: true },
+        { name: 'date3',     display: 'Date3',     tag: 'date',     null: true, disabled: true },
       ]
     },
     autofocus: true
   });
 
-  let timepicker = el.find('[data-item=time]')
+  let timepicker1 = el.find('[data-name=datetime1] [data-item=time]')
+  //debugger
 
-  timepicker.focus()
+  timepicker1.focus()
 
   setTimeout(function(){ // give it time to apply focus
-    equal(timepicker[0].selectionStart, 0)
-    equal(timepicker[0].selectionEnd, 2)
+    equal(timepicker1[0].selectionStart, 0)
+    equal(timepicker1[0].selectionEnd, 2)
+
+    equal(el.find('[data-name=datetime2] [data-item=date]')[0].disabled, true)
+    equal(el.find('[data-name=datetime2] [data-item=time]')[0].disabled, true)
+    equal(el.find('[data-name=date3]     [data-item=date]')[0].disabled, true)
 
     done()
   }, 100)
