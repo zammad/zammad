4398b0f58f099eae21a9a5bacd2e43fda56884b4
Thorsten Eckel <te@zammad.com>
2021-01-12 15:03:51 +0100

    Maintenance: `ApplicationHandleInfo.current = 'application_server'` leaks into following test on raised exceptions while processing Controller action.

--- app/controllers/application_controller/handles_transitions.rb
+++ app/controllers/application_controller/handles_transitions.rb
@@ -2,20 +2,21 @@ module ApplicationController::HandlesTransitions
   extend ActiveSupport::Concern
 
   included do
-    before_action :transaction_begin
-    after_action  :transaction_end
+    around_action :handle_transaction
   end
 
   private
 
-  def transaction_begin
+  def handle_transaction
     ApplicationHandleInfo.current = 'application_server'
     PushMessages.init
-  end
 
-  def transaction_end
+    yield
+
     Observer::Transaction.commit
     PushMessages.finish
     ActiveSupport::Dependencies::Reference.clear!
+  ensure
+    ApplicationHandleInfo.current = nil
   end
 end
