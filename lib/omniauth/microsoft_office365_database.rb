1efd09ac82c9fec3c2476ae8f54b86076b4035dc
Boris Manojlovic <boris@steki.net>
2021-03-10 00:58:29 +0100

    oh it is ugly auth...

--- lib/omniauth/microsoft_office365_database.rb
+++ lib/omniauth/microsoft_office365_database.rb
@@ -1,18 +1,17 @@
 class MicrosoftOffice365Database < OmniAuth::Strategies::MicrosoftOffice365
   option :name, 'microsoft_office365'
-
+  config  = Setting.get('auth_microsoft_office365_credentials') || {}
+  option :client_options, {
+    site:          "https://login.microsoftonline.com",
+    authorize_url: "/#{config['app_tenant']}/oauth2/v2.0/authorize",
+    token_url:     "/#{config['app_tenant']}/oauth2/v2.0/token",
+  }
   def initialize(app, *args, &block)
 
     # database lookup
     config  = Setting.get('auth_microsoft_office365_credentials') || {}
     args[0] = config['app_id']
     args[1] = config['app_secret']
-
-    option :client_options, {
-      site:          "https://login.microsoftonline.com",
-      authorize_url: "/#{config['app_tenant']}/oauth2/v2.0/authorize",
-      token_url:     "/#{config['app_tenant']}/oauth2/v2.0/token",
-    }
     super
   end
 
