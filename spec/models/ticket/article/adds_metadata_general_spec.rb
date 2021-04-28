e90dd791f0d2aab8e84929078f0d7184a11c113d
Thorsten Eckel <te@zammad.com>
2021-04-28 07:36:59 +0000

    Maintenance: Rework stored 'from' attribute for web and phone articles.

--- spec/models/ticket/article/adds_metadata_general_spec.rb
+++ spec/models/ticket/article/adds_metadata_general_spec.rb
@@ -2,17 +2,33 @@
 
 require 'rails_helper'
 
-RSpec.describe Ticket::Article::AddsMetadataGeneral, current_user_id: -> { agent.id } do
+RSpec.describe Ticket::Article::AddsMetadataGeneral do
   let(:agent) { create(:agent) }
 
-  context 'when customer is agent' do
-    let(:customer) { create(:agent) }
+  context 'when Agent creates Article' do
+    shared_examples 'not including email in from' do |factory|
+      subject(:article) { create(:ticket_article, factory, ticket: ticket, created_by_id: agent.id, updated_by_id: agent.id ) }
 
-    it 'show customer agent details in from field' do
-      ticket  = create(:ticket, customer_id: customer.id)
-      article = create(:ticket_article, :inbound_phone, ticket: ticket)
+      let(:ticket) { create(:ticket) }
+      let!(:agent) { create(:agent, groups: [ticket.group]) }
 
-      expect(article.from).to include customer.email
+      it "doesn't include email in from" do
+        expect(article.from).not_to include agent.email
+      end
+    end
+
+    it_behaves_like 'not including email in from', :outbound_phone
+    it_behaves_like 'not including email in from', :outbound_web
+
+    context 'when as Customer' do
+      subject(:article) { create(:ticket_article, :inbound_phone, ticket: ticket) }
+
+      let(:customer) { agent }
+      let(:ticket) { create(:ticket, customer_id: customer.id) }
+
+      it 'includes email in from' do
+        expect(article.from).not_to include agent.email
+      end
     end
   end
 end
