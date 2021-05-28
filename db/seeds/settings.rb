01b414f9fca8b8f3a2079c452e6b081a09f56a45
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-05-28 11:28:06 +0000

    Fixes #3575 - Display minutes for session timeouts instead of seconds.

--- db/seeds/settings.rb
+++ db/seeds/settings.rb
@@ -1041,6 +1041,8 @@ Setting.create_if_not_exists(
   },
   frontend:    true
 )
+
+options = [ { value: '0', name: 'disabled' }, { value: 1.hour.seconds, name: '1 hour' }, { value: 2.hours.seconds, name: '2 hours' }, { value: 1.day.seconds, name: '1 day' }, { value: 7.days.seconds, name: '1 week' }, { value: 14.days.seconds, name: '2 weeks' }, { value: 21.days.seconds, name: '3 weeks' }, { value: 28.days.seconds, name: '4 weeks' } ]
 Setting.create_if_not_exists(
   title:       'Session Timeout',
   name:        'session_timeout',
@@ -1049,28 +1051,36 @@ Setting.create_if_not_exists(
   options:     {
     form: [
       {
-        display: 'Default',
-        null:    false,
-        name:    'default',
-        tag:     'input',
+        display:   'Default',
+        null:      false,
+        name:      'default',
+        tag:       'select',
+        options:   options,
+        translate: true,
       },
       {
-        display: 'admin',
-        null:    false,
-        name:    'admin',
-        tag:     'input',
+        display:   'admin',
+        null:      false,
+        name:      'admin',
+        tag:       'select',
+        options:   options,
+        translate: true,
       },
       {
-        display: 'ticket.agent',
-        null:    false,
-        name:    'ticket.agent',
-        tag:     'input',
+        display:   'ticket.agent',
+        null:      false,
+        name:      'ticket.agent',
+        tag:       'select',
+        options:   options,
+        translate: true,
       },
       {
-        display: 'ticket.customer',
-        null:    false,
-        name:    'ticket.customer',
-        tag:     'input',
+        display:   'ticket.customer',
+        null:      false,
+        name:      'ticket.customer',
+        tag:       'select',
+        options:   options,
+        translate: true,
       },
     ],
   },
