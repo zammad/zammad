51bf33dab60859bc078ddf70e7ec8ce27694d692
Martin Gruner <mg@zammad.com>
2021-06-01 10:12:09 +0200

    Fixes #3580 - Modify Package.link and Package.install to not execute package migrations any more.

---
+++ lib/tasks/zammad/package/migrate.rake
@@ -0,0 +1,2 @@
+require_dependency 'tasks/zammad/package/migrate.rb'
+Tasks::Zammad::Package::Migrate.register_rake_task
---
+++ lib/tasks/zammad/package/migrate.rb
@@ -0,0 +1,20 @@
+require_dependency 'tasks/zammad/command.rb'
+
+module Tasks
+  module Zammad
+    module Package
+      # Package migrations must not be executed in the same process that also executed
+      #   Package.install or Package.link, as the codebase is in an inconsistent state.
+      # This is enforced by Tasks:Zammad::Command which prevents command chaining.
+      class Migrate < Tasks::Zammad::Command
+
+        def self.task_handler
+          puts 'Executing all pending package migrations...'
+          ::Package.migration_execute
+          puts 'done.'
+        end
+
+      end
+    end
+  end
+end
