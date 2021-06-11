129646e76077d565347f945e90e4dfccb00439d6
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-06-11 12:17:25 +0000

    Fixes #3605 - session timeout does regulary logout active users before timeout because of dead old sessions.

--- app/jobs/session_timeout_job.rb
+++ app/jobs/session_timeout_job.rb
@@ -2,58 +2,36 @@
 
 class SessionTimeoutJob < ApplicationJob
   def perform
-    sessions.find_each do |session|
+    sessions.each do |session|
       perform_session(session)
     end
   end
 
   def perform_session(session)
-    return if !session.data['user_id']
 
     # user is optional because it can be deleted already
-    user = User.find_by(id: session.data['user_id'])
-    if user
-      timeout = get_timeout(user)
-      return if timeout < 1
-      return if session.data['ping'] > timeout.seconds.ago
+    if session.user?
+      return if session.active?
+
+      # if the user has no active sessions then we
+      # make sure to definitely log him out if there
+      # is any frontends opened
+      if !active_session(session.user)
+        session.frontend_timeout
+      end
     end
 
-    self.class.destroy_session(user, session)
-  end
-
-  def self.destroy_session(user, session)
-
-    # user is optional because it can be deleted already
-    if user
-      PushMessages.send_to(user.id, { event: 'session_timeout' })
-    end
     session.destroy
   end
 
+  def active_session(user)
+    @active_session ||= {}
+    return @active_session[user.id] if @active_session[user.id].present?
+
+    @active_session[user.id] = sessions.detect { |session| session.active? && session.user.id == user.id }
+  end
+
   def sessions
-    ActiveRecord::SessionStore::Session.where('updated_at < ?', config.values.map(&:to_i).min.seconds.ago)
-  end
-
-  def config
-    Setting.get('session_timeout')
-  end
-
-  def get_timeout(user)
-    permissions = Permission.where(id: user.permissions_with_child_ids).pluck(:name)
-
-    timeout = -1
-    config.each do |key, value|
-      next if key == 'default'
-      next if permissions.exclude?(key)
-      next if value.to_i < timeout
-
-      timeout = value.to_i
-    end
-
-    if timeout < 1
-      timeout = config['default'].to_i
-    end
-
-    timeout
+    @sessions ||= ActiveRecord::SessionStore::Session.order(updated_at: :desc).limit(10_000).map { |session| SessionTimeoutJob::Session.new(session) }
   end
 end
