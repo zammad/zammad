1cb5b04d9680220e72549f926c9cc7cfdaa2c600
Martin Gruner <mg@zammad.com>
2021-06-16 10:12:40 +0000

    Maintenance: switch to Docker based OTRS import testing rather than using external services.

--- script/build/database_config.rb
+++ script/build/database_config.rb
@@ -1,6 +1,7 @@
 # Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
 
 require 'yaml'
+require 'resolv'
 
 cnf = YAML.load_file(File.join(__dir__, '../../config/database/database.yml'))
 
@@ -8,6 +9,7 @@ cnf.delete('default')
 
 database = ENV['ENFORCE_DB_SERVICE']
 
+# Lookup in /etc/hosts first: gitlab uses that if FF_NETWORK_PER_BUILD is not set.
 if !database
   hostsfile = '/etc/hosts'
   database  = %w[postgresql mysql].shuffle.find do |possible_database|
@@ -15,7 +17,21 @@ if !database
   end
 end
 
-raise "Can't find any supported database in #{hostsfile}." if database.nil?
+# Lookup via DNS if needed: gitlab uses that if FF_NETWORK_PER_BUILD is enabled.
+if !database
+  dns = Resolv::DNS.new
+  dns.timeouts = 3
+  database = %w[postgresql mysql].shuffle.find do |possible_database|
+    # Perform a lookup of the database host to check if it is configured as a service.
+    if dns.getaddress possible_database
+      next possible_database
+    end
+  rescue Resolv::ResolvError
+    # Ignore DNS lookup errors
+  end
+end
+
+raise "Can't find any supported database." if database.nil?
 
 puts "NOTICE: Found/selected #{database} Database Service"
 
