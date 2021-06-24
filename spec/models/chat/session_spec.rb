5af09d73c79a7e96fe7270d0c1f34d03d57e54ad
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-06-24 13:58:16 +0100

    Fixes #3633 - The rebuild of the index is failing.

--- spec/models/chat/session_spec.rb
+++ spec/models/chat/session_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe Chat::Session, type: :model do
   describe '.search_index_attribute_lookup' do
     subject(:chat_session) { create(:'chat/session', user: chat_user, chat: chat) }
 
+    let(:chat_message) { create(:'chat/message', chat_session: chat_session) }
+
     let(:chat) { create(:chat) }
     let(:chat_user) { create(:agent) }
 
+    before do
+      chat_message
+    end
+
     it 'verify message attribute' do
-      expect(chat_session.search_index_attribute_lookup['messages']).to eq []
+      expect(chat_session.search_index_attribute_lookup['messages']).not_to eq []
     end
 
     it 'verify user attribute' do
@@ -21,6 +27,5 @@ RSpec.describe Chat::Session, type: :model do
     it 'verify chat attribute' do
       expect(chat_session.search_index_attribute_lookup['chat']['name']).to eq chat.name
     end
-
   end
 end
