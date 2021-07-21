90b59692ebcb140e54502ae24f876fcda990e8ed
Martin Gruner <mg@zammad.com>
2021-07-21 08:16:29 +0000

    Maintenance: Also perform browser-based OTRS import test via docker containers...

--- test/integration/otrs_import_browser_test.rb
+++ test/integration/otrs_import_browser_test.rb
@@ -3,12 +3,14 @@ require 'browser_test_helper'
 class OtrsImportBrowserTest < TestCase
   def test_import
 
-    if !ENV['IMPORT_BT_OTRS_ENDPOINT']
-      raise "ERROR: Need IMPORT_BT_OTRS_ENDPOINT - hint IMPORT_BT_OTRS_ENDPOINT='http://vz305.demo.znuny.com/otrs/public.pl?Action=ZammadMigrator'"
+    if !ENV['IMPORT_OTRS_ENDPOINT']
+      raise "ERROR: Need IMPORT_OTRS_ENDPOINT - hint IMPORT_OTRS_ENDPOINT='http://vz305.demo.znuny.com/otrs/public.pl?Action=ZammadMigrator'"
     end
-    if !ENV['IMPORT_BT_OTRS_ENDPOINT_KEY']
-      raise "ERROR: Need IMPORT_BT_OTRS_ENDPOINT_KEY - hint IMPORT_BT_OTRS_ENDPOINT_KEY='01234567899876543210'"
+    if !ENV['IMPORT_OTRS_ENDPOINT_KEY']
+      raise "ERROR: Need IMPORT_OTRS_ENDPOINT_KEY - hint IMPORT_OTRS_ENDPOINT_KEY='01234567899876543210'"
     end
+
+    puts "Using endpoint '#{ENV['IMPORT_OTRS_ENDPOINT']}'."
 
     @browser = browser_instance
     location(url: browser_url)
@@ -18,7 +20,7 @@ class OtrsImportBrowserTest < TestCase
     click(css: '.js-download')
     click(css: '.js-otrs-link')
 
-    invalid_key_url = "#{ENV['IMPORT_BT_OTRS_ENDPOINT']};Key=31337"
+    invalid_key_url = "#{ENV['IMPORT_OTRS_ENDPOINT']};Key=31337"
 
     set(
       css:   '#otrs-link',
@@ -31,7 +33,7 @@ class OtrsImportBrowserTest < TestCase
       value: 'Invalid API key.',
     )
 
-    import_url = "#{ENV['IMPORT_BT_OTRS_ENDPOINT']};Key=#{ENV['IMPORT_BT_OTRS_ENDPOINT_KEY']}"
+    import_url = "#{ENV['IMPORT_OTRS_ENDPOINT']};Key=#{ENV['IMPORT_OTRS_ENDPOINT_KEY']}"
     set(
       css:   '#otrs-link',
       value: import_url
