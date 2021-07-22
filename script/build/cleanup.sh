219f6c0c620d8f842866bc8474a2a01afd4146b1
Thorsten Eckel <te@zammad.com>
2021-07-22 09:45:32 +0200

    Fixes #3666 - Package installation fails because of too long file names.

--- script/build/cleanup.sh
+++ script/build/cleanup.sh
@@ -5,3 +5,25 @@ set -ex
 rm app/assets/javascripts/app/controllers/layout_ref.coffee
 rm -rf app/assets/javascripts/app/views/layout_ref/
 rm app/assets/javascripts/app/controllers/karma.coffee
+
+# tests
+rm -rf test
+rm -rf spec
+rm .rspec
+
+# CI
+rm -rf .github
+rm .gitlab-ci.yml
+
+# linting
+rm -rf .rubocop
+rm .csslintrc
+rm .eslintignore
+rm .eslintrc
+rm .rubocop.yml
+rm coffeelint.json
+rm .overcommit.yml
+
+# misc
+rm .codeclimate.yml
+rm .github_changelog_generator
