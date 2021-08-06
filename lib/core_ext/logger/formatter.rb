d11d1d4106773b975659cfde6c6b9c72a1027c1f
Dominik Klein <dk@zammad.com>
2021-08-06 17:16:20 +0200

    Fixes #3556 - Backtrace lines pollute log.

--- lib/core_ext/logger/formatter.rb
+++ lib/core_ext/logger/formatter.rb
@@ -1,6 +1,7 @@
 # Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
 
 # This customization adds the id of the current Thread to all log lines.
+# The #msg2str method will be extended, so that the "Rails.bracktrace_cleaner" can be used to clean the exceptions.
 # It was introduced to make it more easy to follow the execution of tasks in the log in threaded processes.
 #
 # before:
@@ -17,6 +18,20 @@ class Logger
 
     def call(severity, time, progname, msg)
       format(FORMAT_WITH_THREAD_ID, severity[0..0], format_datetime(time), Process.pid, Thread.current.object_id, severity, progname, msg2str(msg))
+    end
+
+    private
+
+    def msg2str(msg)
+      case msg
+      when ::String
+        msg
+      when ::Exception
+        # "#{ msg.message } (#{ msg.class })\n#{ msg.backtrace.join("\n") if msg.backtrace }"
+        "#{msg.message} (#{msg.class})\n#{Rails.backtrace_cleaner.clean(msg.backtrace).join("\n") if msg.backtrace}"
+      else
+        msg.inspect
+      end
     end
   end
 end
