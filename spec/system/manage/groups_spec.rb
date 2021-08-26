f38c1e8d4311bc52c643dee3e1431f5377a3fbcd
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-08-26 13:46:44 +0200

    Fixes #2544 - Can't remove auto assignment timeout.

--- spec/system/manage/groups_spec.rb
+++ spec/system/manage/groups_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe 'Manage > Groups', type: :system do
   context 'ajax pagination' do
     include_examples 'pagination', model: :group, klass: Group, path: 'manage/groups'
   end
+
+  context "Issue 2544 - Can't remove auto assignment timeout" do
+    before do
+      visit '/#manage/groups'
+    end
+
+    it 'is possible to reset the assignment timeout of a group' do
+      find('td', text: 'Users').click
+      fill_in 'Assignment Timeout', with: '30'
+      find('button', text: 'Submit').click
+      await_empty_ajax_queue
+      expect(Group.find_by(name: 'Users').assignment_timeout).to eq(30)
+
+      find('td', text: 'Users').click
+      fill_in 'Assignment Timeout', with: ''
+      find('button', text: 'Submit').click
+      await_empty_ajax_queue
+      expect(Group.find_by(name: 'Users').assignment_timeout).to be nil
+    end
+  end
 end
