c305ff89be982766d7b142249382a6478d95e2ed
Mantas <mantas@idev.lt>
2021-08-26 23:58:51 +0300

    Fixes #2157 - Ical escaltion update the calendar entry to 2:00 am

--- spec/requests/calendar_subscriptions_spec.rb
+++ spec/requests/calendar_subscriptions_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe 'iCal endpoints', type: :request do
       expect(response.body).to eq("HTTP Basic: Access denied.\n")
     end
   end
+
+  describe 'time zone', authenticated_as: :user do
+    let(:group) { create(:group) }
+    let(:user) { create(:agent) }
+
+    before do
+      user.groups << group
+
+      create(:ticket, group: group, owner: user, state_name: 'open', pending_time: 1.day.ago)
+    end
+
+    it 'returns zero offset time if no time zone set' do
+      get '/ical/tickets'
+
+      expect(response.body).to match %r{DTSTART:\d{8}T0{6}Z}
+    end
+
+    it 'returns selected time zone' do
+      Setting.set 'timezone_default', 'Europe/Vilnius'
+
+      get '/ical/tickets'
+
+      expect(response.body).to match %r{DTSTART;TZID=Europe/Vilnius:\d{8}T0{6}}
+    end
+  end
 end
