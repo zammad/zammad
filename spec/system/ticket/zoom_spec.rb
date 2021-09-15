cc29107ef78dfea5f8cbe01013963a9d9cd5823f
Dominik Klein <dk@zammad.com>
2021-09-15 11:26:57 +0200

    Maintenance: Try to improve the stabilzation of the ticket zoom selenium test.

--- spec/system/ticket/zoom_spec.rb
+++ spec/system/ticket/zoom_spec.rb
@@ -904,6 +904,8 @@ RSpec.describe 'Ticket zoom', type: :system do
 
     def forward
       within :active_content do
+        # binding.pry
+        wait.until_exists { find('.textBubble-content .richtext-content') }
         click '.js-ArticleAction[data-type=emailForward]'
         fill_in 'To', with: 'customer@example.com'
         find('.js-submit').click
