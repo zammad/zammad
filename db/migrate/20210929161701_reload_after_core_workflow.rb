4e4ba091d40484181df3dc503586dbf05feeccfa
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-09-29 15:18:47 +0100

    Fixes #3776 - Force users to reload after system migration.

---
+++ db/migrate/20210929161701_reload_after_core_workflow.rb
@@ -0,0 +1,11 @@
+# Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
+
+class ReloadAfterCoreWorkflow < ActiveRecord::Migration[4.2]
+  def up
+
+    # return if it's a new setup
+    return if !Setting.exists?(name: 'system_init_done')
+
+    AppVersion.set(true, 'app_version')
+  end
+end
