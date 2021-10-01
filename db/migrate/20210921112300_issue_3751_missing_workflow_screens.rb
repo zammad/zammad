07c6bdacfb194181525d1cd08eeebec216063398
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-10-01 12:25:06 +0200

    Fixes #3781 - ObjectManager Attribute without screen attribute causes CoreWorkflows migration to fail

--- db/migrate/20210921112300_issue_3751_missing_workflow_screens.rb
+++ db/migrate/20210921112300_issue_3751_missing_workflow_screens.rb
@@ -11,6 +11,8 @@ class Issue3751MissingWorkflowScreens < ActiveRecord::Migration[6.0]
   def fix_organization_screens_create
     %w[name shared domain_assignment active].each do |name|
       field = ObjectManager::Attribute.find_by(name: name, object_lookup: ObjectLookup.find_by(name: 'Organization'))
+      next if field.blank?
+
       field.screens['create'] ||= {}
       field.screens['create']['-all-'] ||= {}
       field.screens['create']['-all-']['null'] = false
@@ -21,6 +23,8 @@ class Issue3751MissingWorkflowScreens < ActiveRecord::Migration[6.0]
   def fix_user_screens_create
     %w[firstname lastname active].each do |name|
       field = ObjectManager::Attribute.find_by(name: name, object_lookup: ObjectLookup.find_by(name: 'User'))
+      next if field.blank?
+
       field.screens['create'] ||= {}
       field.screens['create']['-all-'] ||= {}
       field.screens['create']['-all-']['null'] = false
