c1cb4fdd43844434b85f741705905f55151a3ceb
Dominik Klein <dk@zammad.com>
2021-10-06 10:34:15 +0200

    Maintenance: Port maintenance app version test to capybara.

---
+++ spec/system/maintenance_app_version_spec.rb
@@ -0,0 +1,15 @@
+# Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
+
+require 'rails_helper'
+
+RSpec.describe 'Maintenance App Version', type: :system do
+  it 'check that new version is present' do
+    page.execute_script 'App.Event.trigger("maintenance", {type:"app_version", app_version:"1234:false"} )'
+
+    expect(page).to have_no_text('new version')
+
+    page.execute_script 'App.Event.trigger("maintenance", {type:"app_version", app_version:"1234:true"} )'
+
+    expect(page).to have_text('new version')
+  end
+end
