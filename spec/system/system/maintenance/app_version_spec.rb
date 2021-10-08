587f36d220290371b37ec32399adbe99e8007045
Dominik Klein <dk@zammad.com>
2021-10-08 18:48:08 +0200

    Maintenance: Try to improve the stabilzation of the maintenance app version selenium test.

--- spec/system/system/maintenance/app_version_spec.rb
+++ spec/system/system/maintenance/app_version_spec.rb
@@ -4,13 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'System > Maintenance - App Version', type: :system do
   it 'check that new version modal dialog is present' do
+    visit 'ticket/zoom/1'
+
     page.execute_script 'App.Event.trigger("maintenance", {type:"app_version", app_version:"1234:false"} )'
 
-    expect(page).to have_no_text('new version')
+    expect(page).to have_no_text('new version', wait: 10)
 
     page.execute_script 'App.Event.trigger("maintenance", {type:"app_version", app_version:"1234:true"} )'
 
-    modal_ready(timeout: 10)
+    modal_ready timeout: 10
 
     within '.modal-dialog' do
       expect(page).to have_text('new version')
