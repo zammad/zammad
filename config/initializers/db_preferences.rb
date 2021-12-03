2b532fe414a374248a5742053c09abffca76665c
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-12-03 11:13:40 +0100

    Fixes #3877 - Missing ticket updates on high load in MariaDB/MySQL environments.

--- config/initializers/db_preferences.rb
+++ config/initializers/db_preferences.rb
@@ -6,6 +6,13 @@ when 'mysql2'
   Rails.application.config.db_case_sensitive = false
   Rails.application.config.db_like = 'LIKE'
   Rails.application.config.db_null_byte = true
+
+  # Because of missing ticket updates in high load environments
+  # we changed the transaction isolation level equally to postgres
+  # to READ COMMITTED which fixed the problem entirely #3877
+  ActiveRecord::ConnectionAdapters::Mysql2Adapter.set_callback :checkout, :before do |conn|
+    conn.execute('SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED')
+  end
 when 'postgresql'
   Rails.application.config.db_4bytes_utf8 = true
   Rails.application.config.db_case_sensitive = true
