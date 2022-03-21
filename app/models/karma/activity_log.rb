46eb596125e6da5f4a525f921c1be587f04fbeef
Martin Gruner <mg@zammad.com>
2022-03-21 16:16:22 +0100

    Maintenance: Bump rubocop-rails from 2.13.2 to 2.14.2

--- app/models/karma/activity_log.rb
+++ app/models/karma/activity_log.rb
@@ -38,7 +38,9 @@ add karma activity log of an object
         o_id:             o_id,
         activity_id:      activity.id,
       ).find_by('created_at >= ?', Time.zone.now - activity.once_ttl.seconds)
-      return false if !force && latest_activity
+
+      # End transaction block
+      next if !force && latest_activity
 
       score_total = 0
       if last_activity
@@ -62,10 +64,10 @@ add karma activity log of an object
       if !force
         Cache.write("Karma::ActivityLog.add::#{activity.once_ttl.seconds}::#{action}::#{user.id}::#{object}::#{o_id}", true, expires_in: activity.once_ttl.seconds)
       end
-    end
 
-    # set new karma level
-    Karma::User.sync(user)
+      # set new karma level
+      Karma::User.sync(user)
+    end
 
     true
   end
