281a9d7684e75d79f665355c6686c627d294f564
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-04-04 18:30:03 +0100

    Maintenance: Run cache cleanup only if possible.

--- app/jobs/cache_clear_job.rb
+++ app/jobs/cache_clear_job.rb
@@ -4,7 +4,9 @@ class CacheClearJob < ApplicationJob
   include HasActiveJobLock
 
   def perform
-    Rails.cache.cleanup
+    # cleanup is not supported by every backend so
+    # try only if exists
+    Rails.cache.try(:cleanup)
   rescue => e
     Rails.logger.error "Scheduled cache cleanup failed! #{e.inspect}"
   end
