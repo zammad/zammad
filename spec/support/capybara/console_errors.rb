9c5c5a103f310dd894c3265c114af480c67f3b5c
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-05-20 11:07:57 +0200

    Maintenance: Save browser logs to log/browser.log in tests.

--- spec/support/capybara/console_errors.rb
+++ spec/support/capybara/console_errors.rb
@@ -4,12 +4,15 @@ RSpec.configure do |config|
   config.after(:each, type: :system) do
     next if page.driver.browser.browser != :chrome
 
-    errors = page.driver.browser.manage.logs.get(:browser).select { |m| m.level == 'SEVERE' && m.to_s =~ %r{EvalError|InternalError|RangeError|ReferenceError|SyntaxError|TypeError|URIError} }
+    logs   = page.driver.browser.manage.logs.get(:browser)
+    errors = logs.select { |m| m.level == 'SEVERE' && m.to_s =~ %r{EvalError|InternalError|RangeError|ReferenceError|SyntaxError|TypeError|URIError} }
     if errors.present?
       Rails.logger.error "JS ERRORS: #{errors.to_json}"
       errors.each do |error|
         puts "#{error.message}\n\n"
       end
+
+      File.write(Rails.root.join('log/browser.log'), logs.map { |l| "#{l.level}|#{l.message}" }.join("\n"))
     end
 
     expect(errors.length).to eq(0)
