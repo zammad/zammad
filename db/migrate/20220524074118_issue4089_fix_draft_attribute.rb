8041c0547fe8890baefa874d6599214140b2d6f6
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-05-25 09:39:14 +0100

    Fixes #4097 - Latest upgrade to 5.1 is not possible.

--- db/migrate/20220524074118_issue4089_fix_draft_attribute.rb
+++ db/migrate/20220524074118_issue4089_fix_draft_attribute.rb
@@ -5,6 +5,52 @@ class Issue4089FixDraftAttribute < ActiveRecord::Migration[5.0]
     # return if it's a new setup
     return if !Setting.exists?(name: 'system_init_done')
 
-    ObjectManager::Attribute.find_by(name: 'shared_drafts', object_lookup_id: ObjectLookup.by_name('Group')).update(editable: false)
+    field = ObjectManager::Attribute.find_by(name: 'shared_drafts', object_lookup_id: ObjectLookup.by_name('Group'))
+    if !field
+      add_field
+      return
+    end
+
+    field.update(editable: false)
+  end
+
+  def add_field
+    UserInfo.current_user_id = 1
+    ObjectManager::Attribute.add(
+      force:       true,
+      object:      'Group',
+      name:        'shared_drafts',
+      display:     'Shared Drafts',
+      data_type:   'active',
+      data_option: {
+        null:       false,
+        default:    true,
+        permission: ['admin.group'],
+      },
+      editable:    false,
+      active:      true,
+      screens:     {
+        create: {
+          '-all-' => {
+            null: true,
+          },
+        },
+        edit:   {
+          '-all-': {
+            null: false,
+          },
+        },
+        view:   {
+          '-all-' => {
+            shown: false,
+          },
+        },
+      },
+      to_create:   false,
+      to_migrate:  false,
+      to_delete:   false,
+      position:    1400,
+    )
+    ObjectManager::Attribute.migration_execute
   end
 end
