2b3f87da353c5f7092029f095ac68973be6b6ab7
Martin Gruner <mg@zammad.com>
2022-08-02 14:07:41 +0200

    Re-enable front end tests. Improve selection of ignored tests.

--- .github/workflows/ci/test.sh
+++ .github/workflows/ci/test.sh
@@ -6,13 +6,13 @@ set -o pipefail
 source /etc/profile.d/rvm.sh
 source .gitlab/environment.env
 
-# echo "Running front end tests"
-# yarn test
-# yarn test:ci:ct
+echo "Running front end tests"
+yarn test
+yarn test:ci:ct
 
 echo "Running basic rspec tests..."
 bundle exec rake zammad:db:init
-bundle exec rspec --exclude-pattern "spec/system/**/*_spec.rb" -t ~searchindex -t ~integration
+bundle exec rspec --exclude-pattern "spec/system/**/*_spec.rb" -t ~searchindex -t ~integration -t ~required_envs
 
 echo "Running basic minitest tests..."
 bundle exec rake zammad:db:reset
