4d84cdcd885ebae9a2da74029e6e8055b1ded874
Felix Niklas <mrflix@gmail.com>
2022-08-12 18:58:30 +0200

    Re-add rack livereload
    enable via:
    
    export RAKE_LIVE_RELOAD=true
    
    in your cli config (.bash_profile etc.).
    
    You also have to disable the content_security_policy.rb locally. For example by commenting out everything in there.

---
+++ Guardfile
@@ -0,0 +1,15 @@
+# Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
+
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard :livereload, port: '35738' do
+  watch(%r{app/views/.+\.(erb|haml|slim)$})
+  watch(%r{app/helpers/.+\.rb})
+  watch(%r{public/.+\.(css|js|html)})
+  watch(%r{config/locales/.+\.yml})
+  # Rails Assets Pipeline
+  watch(%r{(app|vendor)(/assets/\w+/(.+\.(css|js|coffee|html|png|jpg))).*}) { |m| "/assets/#{m[3]}" }
+  watch(%r{(app|vendor)(/assets/\w+/(.+)\.(scss))}) { |m| "/assets/#{m[3]}.css" }
+  watch(%r{(app|vendor)(/assets/\w+/(.+)\.(svg))}) { |m| "/assets/#{m[3]}.svg" }
+end
---
+++ config/initializers/rack_livereload.rb
@@ -0,0 +1,36 @@
+# Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
+
+if Rails.env.development? && ENV['RAKE_LIVE_RELOAD'].present?
+
+  require 'rack-livereload'
+
+  # strongly inspired by https://github.com/johnbintz/rack-livereload/issues/71#issuecomment-674899405
+  module BodyProcessorExtension
+    def process!(env)
+      @content_security_policy_nonce = if ActionDispatch::Request.new(env).respond_to?(:content_security_policy_nonce)
+                                         ActionDispatch::Request.new(env).content_security_policy_nonce
+                                       end
+
+      super
+    end
+
+    def template
+      orignal_template = ::File.read(::File.expand_path('../../../../skel/livereload.html.erb', method(:template).super_method.source_location[0]))
+      nonced_template  = orignal_template.gsub(%r{(<script type="text/javascript")}, '\1 nonce="<%= @content_security_policy_nonce %>"')
+
+      ERB.new(nonced_template)
+    end
+  end
+
+  Rack::LiveReload::BodyProcessor.prepend(BodyProcessorExtension)
+
+  # Automatically inject JavaScript needed for LiveReload
+  Rails.application.middleware.insert_after(
+    ActionDispatch::Static,
+    Rack::LiveReload,
+    no_swf:           true,
+    min_delay:        500,    # default 1000
+    max_delay:        10_000, # default 60_000
+    live_reload_port: 35_738
+  )
+end
