dd1befeb754754f86d686ac30364f71f0d3e3e1e
Dominik Klein <dk@zammad.com>
2022-08-15 11:19:03 +0200

    Maintenance: Improved mobile view selenium tests.

--- spec/system/apps/mobile/basic/logout_frontend_store_reset_spec.rb
+++ spec/system/apps/mobile/basic/logout_frontend_store_reset_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Mobile > Basic > Logout Frontend Store Reset', type: :system, ap
     let(:customer) { create(:customer) }
 
     it 'check that overviews are resetet for a different user', authenticated_as: :agent do
+      wait_for_gql 'shared/entities/ticket/graphql/queries/ticket/overviews.graphql'
       expect(find_all('a[href^="/mobile/tickets/view/"]').length).to eq(7)
 
       logout
@@ -17,7 +18,8 @@ RSpec.describe 'Mobile > Basic > Logout Frontend Store Reset', type: :system, ap
         password: 'test',
       )
 
-      expect(find_all('a[href^="/mobile/tickets/view"]').length).to eq(1)
+      wait_for_gql 'shared/entities/ticket/graphql/queries/ticket/overviews.graphql'
+      expect(find_all('a[href^="/mobile/tickets/view/"]').length).to eq(1)
     end
   end
 end
