033773974f26f977281d49d638e17db343e35b93
Martin Gruner <mg@zammad.com>
2022-08-24 15:50:54 +0200

    Maintenance: Improve orphaned bundle check to output dependencies for better overview.

--- lib/tasks/zammad/ci/bundle/orphaned.rb
+++ lib/tasks/zammad/ci/bundle/orphaned.rb
@@ -24,8 +24,9 @@ module Tasks
             end
 
             puts "The following bundled gems were released more than #{age} year(s) ago:"
-            orphaned_gems.each do |s|
+            orphaned_gems.sort_by(&:name).each do |s|
               puts "  #{s.name}:#{s.version} #{s.date.strftime('%F')}"
+              print_dependencies_of(s.name)
             end
             abort
           end
@@ -36,6 +37,29 @@ module Tasks
               abort "Please provide a valid number for 'age_in_years'.\n#{usage}"
             end
             age.to_i
+          end
+
+          def self.print_dependencies_of(name, level = 0)
+            deps = dependency_map[name]
+            return if !deps
+
+            deps.each do |dep|
+              puts "  #{'  ' * level} - #{dep}"
+              print_dependencies_of(dep, level + 1)
+            end
+          end
+
+          def self.dependency_map
+            return @dependencies if @dependencies
+
+            @dependencies = {}
+            Bundler.definition.specs.each do |spec|
+              spec.dependencies.each do |dep|
+                @dependencies[dep.name] ||= []
+                @dependencies[dep.name].push spec.name
+              end
+            end
+            @dependencies
           end
 
           def self.find_orphaned_gems(age)
