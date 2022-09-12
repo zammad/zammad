c052398083babdac9fb5b61e2a415e6dac45531a
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-09-12 13:55:03 +0100

    Maintenance: Stabilize profile overviews test.

--- spec/system/profile/overviews_spec.rb
+++ spec/system/profile/overviews_spec.rb
@@ -3,24 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Profile > Overviews', type: :system do
+  def overview_names
+    page.all('.overview-header .js-tabsHolder span.tab-name', visible: :all).map { |e| e.text(:all) }
+  end
+
   def move_assigned_tickets_overview
     visit 'profile/overviews'
     overview_from = page.find('tr.item', text: 'My Assigned Tickets')
     overview_to = page.find('tr.item', text: 'Escalated Tickets')
     overview_from.drag_to overview_to
+    await_empty_ajax_queue
     wait.until { User::OverviewSorting.where(user: current_user)&.last&.overview_id == Overview.find_by(name: 'My Assigned Tickets').id }
   end
 
   def expect_user_ticket_overview
     visit 'ticket/view'
-    overviews = page.all('.overview-header .js-tabsHolder span.tab-name', visible: :all).map { |e| e.text(:all) }
-    expect(overviews.index('My Assigned Tickets')).to be > overviews.index('Escalated Tickets')
+    wait.until { overview_names.index('My Assigned Tickets') > overview_names.index('Escalated Tickets') }
   end
 
   def expect_default_ticket_overview
     visit 'ticket/view'
-    overviews = page.all('.overview-header .js-tabsHolder span.tab-name', visible: :all).map { |e| e.text(:all) }
-    expect(overviews.index('My Assigned Tickets')).to be < overviews.index('Escalated Tickets')
+    wait.until { overview_names.index('My Assigned Tickets') < overview_names.index('Escalated Tickets') }
   end
 
   def reset_overview_order
