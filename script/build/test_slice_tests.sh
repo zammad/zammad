437f9123b88a4245579cdecd0540275311d5643f
Martin Gruner <mg@zammad.com>
2022-09-16 11:59:42 +0200

    Maintenance: Improved minitest browser test slicing for shorter pipeline durations.

--- script/build/test_slice_tests.sh
+++ script/build/test_slice_tests.sh
@@ -54,12 +54,11 @@ if [ "$LEVEL" == '1' ]; then
 elif [ "$LEVEL" == '2' ]; then
   echo "slicing level 2"
 
-  # only ticket action 2/3
   # test/browser/aaa_getting_started_test.rb
   # test/browser/abb_one_group_test.rb
   rm test/browser/admin_drag_drop_to_new_group_test.rb
   rm test/browser/admin_overview_test.rb
-  #rm test/browser/admin_role_test.rb
+  rm test/browser/admin_role_test.rb
   rm test/browser/agent_navigation_and_title_test.rb
   rm test/browser/agent_organization_profile_test.rb
   rm test/browser/agent_ticket_attachment_test.rb
@@ -73,7 +72,7 @@ elif [ "$LEVEL" == '2' ]; then
   rm test/browser/agent_ticket_email_signature_test.rb
   rm test/browser/agent_ticket_link_test.rb
   rm test/browser/agent_ticket_macro_test.rb
-  # test/browser/agent_ticket_merge_test.rb
+  rm test/browser/agent_ticket_merge_test.rb
   rm test/browser/agent_ticket_online_notification_test.rb
   rm test/browser/agent_ticket_overview_group_by_organization_test.rb
   rm test/browser/agent_ticket_overview_level0_test.rb
@@ -82,7 +81,7 @@ elif [ "$LEVEL" == '2' ]; then
   rm test/browser/agent_ticket_overview_tab_test.rb
   rm test/browser/agent_ticket_tag_test.rb
   rm test/browser/agent_ticket_task_changed_test.rb
-  # test/browser/agent_ticket_text_module_test.rb
+  rm test/browser/agent_ticket_text_module_test.rb
   # test/browser/agent_ticket_time_accounting_test.rb
   # rm test/browser/agent_ticket_update_with_attachment_refresh_test.rb
   # test/browser/agent_ticket_update_and_reload_test.rb
@@ -99,7 +98,6 @@ elif [ "$LEVEL" == '2' ]; then
 elif [ "$LEVEL" == '3' ]; then
   echo "slicing level 3"
 
-  # only ticket action 2/3
   # test/browser/aaa_getting_started_test.rb
   # test/browser/abb_one_group_test.rb
   rm test/browser/admin_drag_drop_to_new_group_test.rb
@@ -115,9 +113,9 @@ elif [ "$LEVEL" == '3' ]; then
   # test/browser/agent_ticket_create_reset_customer_selection_test.rb
   # test/browser/agent_ticket_create_template_test.rb
   # test/browser/agent_ticket_email_reply_keep_body_test.rb
-  # test/browser/agent_ticket_email_signature_test.rb
-  # test/browser/agent_ticket_link_test.rb
-  # test/browser/agent_ticket_macro_test.rb
+  # rm test/browser/agent_ticket_email_signature_test.rb
+  rm test/browser/agent_ticket_link_test.rb
+  rm test/browser/agent_ticket_macro_test.rb
   rm test/browser/agent_ticket_merge_test.rb
   rm test/browser/agent_ticket_online_notification_test.rb
   rm test/browser/agent_ticket_overview_group_by_organization_test.rb
@@ -125,8 +123,8 @@ elif [ "$LEVEL" == '3' ]; then
   rm test/browser/agent_ticket_overview_level1_test.rb
   rm test/browser/agent_ticket_overview_pending_til_test.rb
   rm test/browser/agent_ticket_overview_tab_test.rb
-  # test/browser/agent_ticket_tag_test.rb
-  # test/browser/agent_ticket_task_changed_test.rb
+  rm test/browser/agent_ticket_tag_test.rb
+  rm test/browser/agent_ticket_task_changed_test.rb
   rm test/browser/agent_ticket_text_module_test.rb
   rm test/browser/agent_ticket_time_accounting_test.rb
   rm test/browser/agent_ticket_update_with_attachment_refresh_test.rb
@@ -144,7 +142,6 @@ elif [ "$LEVEL" == '3' ]; then
 elif [ "$LEVEL" == '4' ]; then
   echo "slicing level 4"
 
-  # only ticket action 3/3
   # test/browser/aaa_getting_started_test.rb
   # test/browser/abb_one_group_test.rb
   rm test/browser/admin_drag_drop_to_new_group_test.rb
@@ -164,12 +161,12 @@ elif [ "$LEVEL" == '4' ]; then
   rm test/browser/agent_ticket_link_test.rb
   rm test/browser/agent_ticket_macro_test.rb
   rm test/browser/agent_ticket_merge_test.rb
-  # test/browser/agent_ticket_online_notification_test.rb
+  rm test/browser/agent_ticket_online_notification_test.rb
   # test/browser/agent_ticket_overview_group_by_organization_test.rb
-  # test/browser/agent_ticket_overview_level0_test.rb
-  # test/browser/agent_ticket_overview_level1_test.rb
-  # test/browser/agent_ticket_overview_pending_til_test.rb
-  # test/browser/agent_ticket_overview_tab_test.rb
+  # rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_ticket_overview_pending_til_test.rb
+  rm test/browser/agent_ticket_overview_tab_test.rb
   rm test/browser/agent_ticket_tag_test.rb
   rm test/browser/agent_ticket_task_changed_test.rb
   rm test/browser/agent_ticket_text_module_test.rb
@@ -179,7 +176,7 @@ elif [ "$LEVEL" == '4' ]; then
   rm test/browser/agent_ticket_zoom_hide_test.rb
   rm test/browser/agent_user_manage_test.rb
   rm test/browser/agent_user_profile_test.rb
-  # test/browser/customer_ticket_create_test.rb
+  rm test/browser/customer_ticket_create_test.rb
   rm test/browser/manage_test.rb
   rm test/browser/taskbar_session_test.rb
   rm test/browser/taskbar_task_test.rb
@@ -189,7 +186,6 @@ elif [ "$LEVEL" == '4' ]; then
 elif [ "$LEVEL" == '5' ]; then
   echo "slicing level 5"
 
-  # only profile action & admin
   # test/browser/abb_one_group_test.rb
   # rm test/browser/admin_drag_drop_to_new_group_test.rb
   # test/browser/admin_overview_test.rb
@@ -230,7 +226,139 @@ elif [ "$LEVEL" == '5' ]; then
   rm test/browser/user_access_permissions_test.rb
   rm test/browser/user_switch_cache_test.rb
 
+elif [ "$LEVEL" == '6' ]; then
+  echo "slicing level 6"
+
+  # test/browser/aaa_getting_started_test.rb
+  # test/browser/abb_one_group_test.rb
+  rm test/browser/admin_drag_drop_to_new_group_test.rb
+  rm test/browser/admin_overview_test.rb
+  #rm test/browser/admin_role_test.rb
+  rm test/browser/agent_navigation_and_title_test.rb
+  rm test/browser/agent_organization_profile_test.rb
+  rm test/browser/agent_ticket_attachment_test.rb
+  rm test/browser/agent_ticket_create_available_types_test.rb
+  rm test/browser/agent_ticket_create_attachment_missing_after_reload_test.rb
+  rm test/browser/agent_ticket_create_cc_tokenizer_test.rb
+  rm test/browser/agent_ticket_create_default_type_test.rb
+  rm test/browser/agent_ticket_create_reset_customer_selection_test.rb
+  rm test/browser/agent_ticket_create_template_test.rb
+  rm test/browser/agent_ticket_email_reply_keep_body_test.rb
+  rm test/browser/agent_ticket_email_signature_test.rb
+  rm test/browser/agent_ticket_link_test.rb
+  rm test/browser/agent_ticket_macro_test.rb
+  # test/browser/agent_ticket_merge_test.rb
+  # rm test/browser/agent_ticket_online_notification_test.rb
+  rm test/browser/agent_ticket_overview_group_by_organization_test.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_ticket_overview_pending_til_test.rb
+  rm test/browser/agent_ticket_overview_tab_test.rb
+  rm test/browser/agent_ticket_tag_test.rb
+  rm test/browser/agent_ticket_task_changed_test.rb
+  # test/browser/agent_ticket_text_module_test.rb
+  rm test/browser/agent_ticket_time_accounting_test.rb
+  rm test/browser/agent_ticket_update_with_attachment_refresh_test.rb
+  rm test/browser/agent_ticket_update_and_reload_test.rb
+  rm test/browser/agent_ticket_zoom_hide_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
+  rm test/browser/customer_ticket_create_test.rb
+  rm test/browser/manage_test.rb
+  rm test/browser/taskbar_session_test.rb
+  rm test/browser/taskbar_task_test.rb
+  rm test/browser/user_access_permissions_test.rb
+  rm test/browser/user_switch_cache_test.rb
+
+elif [ "$LEVEL" == '7' ]; then
+  echo "slicing level 7"
+
+  # test/browser/aaa_getting_started_test.rb
+  # test/browser/abb_one_group_test.rb
+  rm test/browser/admin_drag_drop_to_new_group_test.rb
+  rm test/browser/admin_overview_test.rb
+  rm test/browser/admin_role_test.rb
+  rm test/browser/agent_navigation_and_title_test.rb
+  rm test/browser/agent_organization_profile_test.rb
+  rm test/browser/agent_ticket_attachment_test.rb
+  rm test/browser/agent_ticket_create_available_types_test.rb
+  rm test/browser/agent_ticket_create_attachment_missing_after_reload_test.rb
+  rm test/browser/agent_ticket_create_cc_tokenizer_test.rb
+  rm test/browser/agent_ticket_create_default_type_test.rb
+  rm test/browser/agent_ticket_create_reset_customer_selection_test.rb
+  rm test/browser/agent_ticket_create_template_test.rb
+  rm test/browser/agent_ticket_email_reply_keep_body_test.rb
+  rm test/browser/agent_ticket_email_signature_test.rb
+  # test/browser/agent_ticket_link_test.rb
+  # test/browser/agent_ticket_macro_test.rb
+  rm test/browser/agent_ticket_merge_test.rb
+  rm test/browser/agent_ticket_online_notification_test.rb
+  rm test/browser/agent_ticket_overview_group_by_organization_test.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  rm test/browser/agent_ticket_overview_level1_test.rb
+  rm test/browser/agent_ticket_overview_pending_til_test.rb
+  rm test/browser/agent_ticket_overview_tab_test.rb
+  # test/browser/agent_ticket_tag_test.rb
+  # test/browser/agent_ticket_task_changed_test.rb
+  rm test/browser/agent_ticket_text_module_test.rb
+  rm test/browser/agent_ticket_time_accounting_test.rb
+  rm test/browser/agent_ticket_update_with_attachment_refresh_test.rb
+  rm test/browser/agent_ticket_update_and_reload_test.rb
+  rm test/browser/agent_ticket_zoom_hide_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
+  rm test/browser/customer_ticket_create_test.rb
+  rm test/browser/manage_test.rb
+  rm test/browser/taskbar_session_test.rb
+  rm test/browser/taskbar_task_test.rb
+  rm test/browser/user_access_permissions_test.rb
+  rm test/browser/user_switch_cache_test.rb
+
+elif [ "$LEVEL" == '8' ]; then
+  echo "slicing level 8"
+
+  # test/browser/aaa_getting_started_test.rb
+  # test/browser/abb_one_group_test.rb
+  rm test/browser/admin_drag_drop_to_new_group_test.rb
+  rm test/browser/admin_overview_test.rb
+  rm test/browser/admin_role_test.rb
+  rm test/browser/agent_navigation_and_title_test.rb
+  rm test/browser/agent_organization_profile_test.rb
+  rm test/browser/agent_ticket_attachment_test.rb
+  rm test/browser/agent_ticket_create_available_types_test.rb
+  rm test/browser/agent_ticket_create_attachment_missing_after_reload_test.rb
+  rm test/browser/agent_ticket_create_cc_tokenizer_test.rb
+  rm test/browser/agent_ticket_create_default_type_test.rb
+  rm test/browser/agent_ticket_create_reset_customer_selection_test.rb
+  rm test/browser/agent_ticket_create_template_test.rb
+  rm test/browser/agent_ticket_email_reply_keep_body_test.rb
+  rm test/browser/agent_ticket_email_signature_test.rb
+  rm test/browser/agent_ticket_link_test.rb
+  rm test/browser/agent_ticket_macro_test.rb
+  rm test/browser/agent_ticket_merge_test.rb
+  rm test/browser/agent_ticket_online_notification_test.rb
+  rm test/browser/agent_ticket_overview_group_by_organization_test.rb
+  rm test/browser/agent_ticket_overview_level0_test.rb
+  # test/browser/agent_ticket_overview_level1_test.rb
+  # test/browser/agent_ticket_overview_pending_til_test.rb
+  # test/browser/agent_ticket_overview_tab_test.rb
+  rm test/browser/agent_ticket_tag_test.rb
+  rm test/browser/agent_ticket_task_changed_test.rb
+  rm test/browser/agent_ticket_text_module_test.rb
+  rm test/browser/agent_ticket_time_accounting_test.rb
+  rm test/browser/agent_ticket_update_with_attachment_refresh_test.rb
+  rm test/browser/agent_ticket_update_and_reload_test.rb
+  rm test/browser/agent_ticket_zoom_hide_test.rb
+  rm test/browser/agent_user_manage_test.rb
+  rm test/browser/agent_user_profile_test.rb
+  # test/browser/customer_ticket_create_test.rb
+  rm test/browser/manage_test.rb
+  rm test/browser/taskbar_session_test.rb
+  rm test/browser/taskbar_task_test.rb
+  rm test/browser/user_access_permissions_test.rb
+  rm test/browser/user_switch_cache_test.rb
+
 else
-  echo "ERROR: Invalid level $LEVEL - 1, 2, 3, 4 or 5 is available"
+  echo "ERROR: Invalid level $LEVEL - 1..8 is available"
   exit 1
 fi
