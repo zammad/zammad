2ca30e5a34ecf12ffab065cfffd4ef7937a86156
Martin Gruner <mg@zammad.com>
2022-10-06 09:23:58 +0200

    Maintenance: Improve rspec-retry configuration to also trigger on Selenium::WebDriver::Error::UnknownError.
    (cherry picked from commit 174fe7f964182ffdf7617112731aa49d89db8c2b)

--- spec/support/capybara/driven_by.rb
+++ spec/support/capybara/driven_by.rb
@@ -76,7 +76,7 @@ RSpec.configure do |config|
     #    which may cause overhead and Net::OpenTimeout errors.
     WebMock.disable! if !use_vcr
     # rspec-retry
-    example.run_with_retry retry: 3, exceptions_to_retry: [Net::OpenTimeout, Net::ReadTimeout]
+    example.run_with_retry retry: 3, exceptions_to_retry: [Net::OpenTimeout, Net::ReadTimeout, Selenium::WebDriver::Error::UnknownError]
     WebMock.enable! if !use_vcr
   end
 end
