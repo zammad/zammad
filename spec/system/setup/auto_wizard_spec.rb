89c73ff9867d3d48feb1f017db8afc6a2eed1906
Bola Ahmed Buari <bb@zammad.com>
2022-10-12 22:31:24 +0200

    Maintenance: Port auto wizard integration minitest to rspec

--- spec/system/setup/auto_wizard_spec.rb
+++ spec/system/setup/auto_wizard_spec.rb
@@ -3,17 +3,106 @@
 require 'rails_helper'
 
 RSpec.describe 'Auto wizard', type: :system, set_up: false do
+  shared_examples 'showing that auto wizard is enabled' do
+    it 'shows the auto wizard enable message' do
+      visit '/'
 
-  it 'Automatic setup and login' do
+      within '.wizard.setup .wizard-slide' do
+        expect(page).to have_content('The auto wizard is enabled. Please use the provided auto wizard url.')
+      end
+    end
+  end
 
-    FileUtils.ln(
-      Rails.root.join('contrib/auto_wizard_test.json'),
-      Rails.root.join('auto_wizard.json'),
-      force: true
-    )
+  context 'with auto wizard enabled' do
+    before do
+      FileUtils.ln(
+        Rails.root.join('contrib/auto_wizard_test.json'),
+        Rails.root.join('auto_wizard.json'),
+        force: true
+      )
+    end
 
-    visit 'getting_started/auto_wizard'
+    it_behaves_like 'showing that auto wizard is enabled'
 
-    expect(current_login).to eq('admin@example.com')
+    it 'automatically set up and login' do
+      visit 'getting_started/auto_wizard'
+      # auto wizard is enabled
+      expect(current_login).to eq('admin@example.com')
+    end
+  end
+
+  context 'when auto wizard is enabled with secret token' do
+    before do
+      FileUtils.ln(
+        Rails.root.join('contrib/auto_wizard_example.json'),
+        Rails.root.join('auto_wizard.json'),
+        force: true
+      )
+    end
+
+    it_behaves_like 'showing that auto wizard is enabled'
+
+    it 'automatically setup and login with token params' do
+      visit 'getting_started/auto_wizard/secret_token'
+
+      # close clues
+      within '.js-modal--clue.modal--clue' do
+        find('.modal-close.js-close').click
+      end
+
+      expect(current_login).to eq('hans.atila@zammad.org')
+    end
+
+    it 'allows user to login and logout' do
+      visit 'getting_started/auto_wizard/secret_token'
+
+      # close clues
+      within '.js-modal--clue.modal--clue' do
+        find('.modal-close.js-close').click
+      end
+
+      visit 'logout'
+
+      expect(page).to have_current_route('login')
+
+      login(
+        username: 'hans.atila@zammad.org',
+        password: 'Z4mm4dr0ckZ!'
+      )
+
+      expect(page).to have_current_path('/')
+
+      refresh
+
+      expect(current_login).to eq('hans.atila@zammad.org')
+    end
+
+    context 'with organisation in the auto_wizard data', searchindex: true do
+      before do
+        visit 'getting_started/auto_wizard/secret_token'
+
+        searchindex_model_reload([::User, ::Organization])
+
+        # close clues
+        within '.js-modal--clue.modal--clue' do
+          find('.modal-close.js-close').click
+        end
+      end
+
+      it 'shows the organization from the auto wizard' do
+        fill_in id: 'global-search', with: 'Demo Organization'
+
+        click_on 'Show Search Details'
+
+        find('[data-tab-content=Organization]').click
+
+        find('.js-content .js-tableBody tr.item').click
+
+        within '.active .profile-window' do
+          expect(page).to have_content 'Demo Organization'
+          expect(page).to have_content 'Atila'
+        end
+      end
+    end
   end
 end
