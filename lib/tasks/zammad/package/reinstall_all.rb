effa1eb4439c663d9668414ad0b8e636be859bcf
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-10-26 16:44:25 +0200

    Fixes #4319 - Add command to reinstall all packages.

---
+++ lib/tasks/zammad/package/reinstall_all.rb
@@ -0,0 +1,20 @@
+# Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
+
+require_dependency 'tasks/zammad/command.rb'
+
+module Tasks
+  module Zammad
+    module Package
+      class ReinstallAll < Tasks::Zammad::Command
+        def self.task_handler
+          puts 'Reinstalling all packages...'
+          ::Package.find_each do |package|
+            puts "Reinstalling '#{package.name}' (#{package.version})..."
+            ::Package.reinstall(package.name)
+          end
+          puts 'done.'
+        end
+      end
+    end
+  end
+end
