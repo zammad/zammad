6d6f48fd4ace5b5f8a284a77d0c63031e7658f51
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-11-11 14:38:13 +0100

    Fixes #4306 - Updating group settings causes huge numbers of delayed jobs.
    (cherry picked from commit 4d81950ddf2e74051d1837661b2ebe3626ec9792)

--- db/seeds/settings.rb
+++ db/seeds/settings.rb
@@ -3164,15 +3164,6 @@ Setting.create_if_not_exists(
   preferences: { online_service_disable: true },
   frontend:    false
 )
-Setting.create_if_not_exists(
-  title:       __('Elasticsearch Excludes'),
-  name:        'es_excludes',
-  area:        'SearchIndex::Elasticsearch',
-  description: __('Defines if the search index is using excluded attributes.'),
-  state:       true,
-  preferences: { online_service_disable: true },
-  frontend:    false
-)
 
 Setting.create_if_not_exists(
   title:       __('Import Mode'),
