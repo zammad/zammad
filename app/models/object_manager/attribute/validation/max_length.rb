bc660122811e7a07512b625fef56689393840435
Mantas <mantas@idev.lt>
2022-11-11 19:38:50 +0200

    Fixes #2150 - Text attribute inputs longer than maxlength is silently truncated

---
+++ app/models/object_manager/attribute/validation/max_length.rb
@@ -0,0 +1,27 @@
+# Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
+
+class ObjectManager::Attribute::Validation::MaxLength < ObjectManager::Attribute::Validation::Backend
+
+  def validate
+    return if value.blank?
+    return if irrelevant_attribute?
+
+    validate_max_length
+  end
+
+  private
+
+  def irrelevant_attribute?
+    %w[input textarea].exclude? attribute.data_type
+  end
+
+  def maxlength
+    attribute.data_option[:maxlength]
+  end
+
+  def validate_max_length
+    return if value.length <= maxlength
+
+    invalid_because_attribute("is longer than the allowed length #{maxlength}")
+  end
+end
