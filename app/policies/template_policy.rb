1d665ee5eeacdcfca4165905acdb5a8f7ee235da
Dusan Vuckovic <dv@zammad.com>
2022-12-09 10:08:34 +0100

    Maintenance: Extend the template policy for future use.

--- app/policies/template_policy.rb
+++ app/policies/template_policy.rb
@@ -1,4 +1,28 @@
 # Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
 
 class TemplatePolicy < ApplicationPolicy
+  def show?
+    return true if admin?
+    return true if user.permissions?('ticket.agent') && record.active
+
+    false
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  private
+
+  def admin?
+    user.permissions?('admin.template')
+  end
 end
