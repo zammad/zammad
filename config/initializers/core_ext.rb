135384c43d41b7f4b76a8a812e389e1ea244fa44
Mantas Masalskis <mm@zammad.com>
2023-01-25 09:27:12 +0100

    Fixes #4347 - Zammad looses EXIF Tags from Images taken and sent with an iPhone

--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
 # load all core_ext extensions
-Rails.root.glob('lib/core_ext/**/*').each do |file|
+Rails.root.glob('lib/core_ext/**/*.rb').each do |file|
   if File.file?(file)
     require file
   end
