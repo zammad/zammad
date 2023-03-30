63fdfc734b9a815ac9b31569d786d66dbb18b80a
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-03-30 16:23:46 +0200

    Maintenance: Support zammad packages in package installations.

--- contrib/packager.io/preinstall.sh
+++ contrib/packager.io/preinstall.sh
@@ -10,3 +10,22 @@
 #   These duplicates can cause the application to fail, however.
 #
 rm -f /opt/zammad/public/assets/.sprockets-manifest-*.json || true
+
+# remove local files of the packages
+if [ -n "$(which zammad 2> /dev/null)" ]; then
+   PATH=/opt/zammad/bin:/opt/zammad/vendor/bundle/bin:/sbin:/bin:/usr/sbin:/usr/bin:
+
+   RAKE_TASKS=$(zammad run rake --tasks | grep "zammad:package:uninstall_all_files")
+
+   if [[ x$RAKE_TASKS == 'x' ]]; then
+      echo "# Code does not yet fit, skipping automatic package uninstall."
+      echo "... This is not an error and will work during your next upgrade ..."
+      exit 0
+   fi
+
+   if [ "$(zammad run rails r 'puts Package.count.positive?')" == "true" ] && [ -n "$(which yarn 2> /dev/null)" ] && [ -n "$(which node 2> /dev/null)" ]; then
+      echo "# Detected custom packages..."
+      echo "# Remove custom packages files temporarily..."
+      zammad run rake zammad:package:uninstall_all_files
+   fi
+fi
