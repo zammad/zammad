b53c3883b8421d3bbd0c718bbd8bd0993ee7de78
qcandymanp <hendrikherpers@gmail.com>
2023-04-12 14:57:39 +0200

    [AIZ-10] create backend controller to handle the api-requests

---
+++ app/controllers/integration/awork_controller.rb
@@ -0,0 +1,123 @@
+class Integration::AworkController < ApplicationController
+  prepend_before_action { authentication_check && authorize! }
+
+  def verify
+    awork = ::Awork.new(params[:endpoint], params[:api_token])
+
+    awork.verify
+
+    render json: {
+      result: 'ok',
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  def update
+    link_tasks(params[:linked_tasks])
+
+    render json: {
+      result:   'ok',
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  def create
+    config = Setting.get('awork_config')
+
+    awork = ::Awork.new(config['endpoint'], config['api_token'])
+
+    task = awork.create(params[:create_task])
+
+    link_tasks(params[:linked_tasks] + ", #{task.result[:id]}")
+
+    render json: {
+      result:   'ok',
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  def linked_tasks
+    config = Setting.get('awork_config')
+
+    awork = ::Awork.new(config['endpoint'], config['api_token'])
+
+    render json: {
+      result:   'ok',
+      response: awork.linked_tasks(params[:linked_tasks]),
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  def projects
+    config = Setting.get('awork_config')
+
+    awork = ::Awork.new(config['endpoint'], config['api_token'])
+
+    render json: {
+      result:   'ok',
+      response: awork.projects,
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  def tasks_by_project
+    config = Setting.get('awork_config')
+
+    awork = ::Awork.new(config['endpoint'], config['api_token'])
+
+    render json: {
+      result:   'ok',
+      response: awork.tasks_by_project(params[:project_id]),
+    }
+  rescue => e
+    logger.error e
+
+    render json: {
+      result:  'failed',
+      message: e.message,
+    }
+  end
+
+  private
+
+  def link_tasks(tasks)
+    ticket = Ticket.find(params[:ticket_id])
+    ticket.with_lock do
+      authorize!(ticket, :show?)
+      ticket.preferences[:awork] ||= {}
+      ticket.preferences[:awork][:task_links] = Array(tasks).uniq
+      ticket.save!
+    end
+  end
+
+end
