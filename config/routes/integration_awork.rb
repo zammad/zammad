b87de8c0631477153dfb04b57446f89c05373b76
qcandymanp <hendrikherpers@gmail.com>
2023-04-12 14:20:06 +0200

    [AIZ-11] create API-routes for the frontend

---
+++ config/routes/integration_awork.rb
@@ -0,0 +1,10 @@
+Zammad::Application.routes.draw do
+  api_path = Rails.configuration.api_path
+
+  match api_path + '/integration/awork/verify',                   to: 'integration/awork#verify',                 via: :post
+  match api_path + '/integration/awork/tasks',                    to: 'integration/awork#update',                 via: :post
+  match api_path + '/integration/awork/tasks/:ticket_id',         to: 'integration/awork#linked_tasks',           via: :get
+  match api_path + '/integration/awork/projects',                 to: 'integration/awork#projects',               via: :get
+  match api_path + '/integration/awork/projects/:id/tasks',       to: 'integration/awork#tasks_by_project',       via: :get
+
+end
