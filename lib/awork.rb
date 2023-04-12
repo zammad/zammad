56a4951e7701b2425ff8479b947ff8edc864d0d6
qcandymanp <hendrikherpers@gmail.com>
2023-04-12 22:33:55 +0200

    [AIZ-32] add functions for api classes

--- lib/awork.rb
+++ lib/awork.rb
@@ -10,18 +10,32 @@ class Awork
   end
 
   def linked_tasks(ids)
-
+    ids.map do |id|
+      Awork::Task.new(
+        client,
+        client.perform('get', "tasks/#{ id }")
+      )
+    end
   end
 
   def projects
-
+    result = client.perform('get', 'projects')
+    result.map { |entity| Awork::Project.new(client, entity) }
   end
 
   def tasks_by_project(id)
-
+    result = client.perform('get', "projects/#{id}/projecttasks")
+    result.map { |entity| Awork::Task.new(client, entity) }
   end
 
   def create(task)
+    result = client.perform('post', '/tasks', {
+      'name': task['name'],
+      'description': task['description'],
+      'baseType': 'projecttask',
+      'parentId': task['project_id']
+    })
 
+    Awork::Task.new(client, result)
   end
 end
