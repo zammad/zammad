af49798cb6eb6f3ad229c61eff4888d5bdaf61f3
qcandymanp <hendrikherpers@gmail.com>
2023-04-12 15:00:06 +0200

    [AIZ-11] add api route to create tasks

--- config/routes/integration_awork.rb
+++ config/routes/integration_awork.rb
@@ -2,7 +2,8 @@ Zammad::Application.routes.draw do
   api_path = Rails.configuration.api_path
 
   match api_path + '/integration/awork/verify',                   to: 'integration/awork#verify',                 via: :post
-  match api_path + '/integration/awork/tasks',                    to: 'integration/awork#update',                 via: :post
+  match api_path + '/integration/awork/tasks/create',             to: 'integration/awork#create',                 via: :post
+  match api_path + '/integration/awork/tasks/update',             to: 'integration/awork#update',                 via: :post
   match api_path + '/integration/awork/tasks/:ticket_id',         to: 'integration/awork#linked_tasks',           via: :get
   match api_path + '/integration/awork/projects',                 to: 'integration/awork#projects',               via: :get
   match api_path + '/integration/awork/projects/:id/tasks',       to: 'integration/awork#tasks_by_project',       via: :get
