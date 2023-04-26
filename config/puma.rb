7c0bb87f24e416ea24e01de4c43c73309e3c7cf9
Martin Gruner <mg@zammad.com>
2023-04-26 10:13:47 +0200

    Maintenance: Avoid warning of puma in single mode.

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 # Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 0)
+worker_count = Integer(ENV['WEB_CONCURRENCY'] || 0)
+workers worker_count
+
 threads_count_min = Integer(ENV['MIN_THREADS'] || 5)
 threads_count_max = Integer(ENV['MAX_THREADS'] || 30)
 threads threads_count_min, threads_count_max
@@ -9,6 +11,8 @@ environment ENV.fetch('RAILS_ENV', 'development')
 
 preload_app!
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
+if worker_count.positive?
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection
+  end
 end
