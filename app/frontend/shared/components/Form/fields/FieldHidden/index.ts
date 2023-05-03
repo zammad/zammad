2b62754bdbf65872d34738ca7bd3d9f84a835f53
Dominik Klein <dk@zammad.com>
2023-05-03 12:46:09 +0200

    Maintenance: Added missing global field definition to hidden field type.

--- app/frontend/shared/components/Form/fields/FieldHidden/index.ts
+++ app/frontend/shared/components/Form/fields/FieldHidden/index.ts
@@ -1,6 +1,12 @@
 // Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
 import { hidden as hiddenDefinition } from '@formkit/inputs'
+import initializeFieldDefinition from '#shared/form/core/initializeFieldDefinition.ts'
+import formUpdaterTrigger from '#shared/form/features/formUpdaterTrigger.ts'
+
+initializeFieldDefinition(hiddenDefinition, {
+  features: [formUpdaterTrigger()],
+})
 
 export default {
   fieldType: 'hidden',
