abd77d8ef3fc746057552f7f75c2ce562221797f
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-05-05 16:56:50 +0200

    Fixes #4601 - Microsoft 365 reauthenticate changes the "leave emails on server" to "no" by default.

--- spec/lib/external_credential/microsoft365_spec.rb
+++ spec/lib/external_credential/microsoft365_spec.rb
@@ -77,8 +77,7 @@ RSpec.describe ExternalCredential::Microsoft365 do
         create(:external_credential, name: provider, credentials: { client_id: client_id, client_secret: client_secret })
       end
 
-      it 'creates a Channel instance' do
-
+      it 'creates a Channel instance', :aggregate_failures do
         channel = described_class.link_account(request_token, authorization_payload)
 
         expect(channel.options).to include(
@@ -111,6 +110,12 @@ RSpec.describe ExternalCredential::Microsoft365 do
             'client_secret' => client_secret,
           ),
         )
+
+        channel.options[:inbound][:options][:keep_on_server] = true
+        channel.save
+
+        channel = described_class.link_account(request_token, authorization_payload.merge(channel_id: channel.id))
+        expect(channel.reload.options[:inbound][:options][:keep_on_server]).to be(true)
       end
     end
 
