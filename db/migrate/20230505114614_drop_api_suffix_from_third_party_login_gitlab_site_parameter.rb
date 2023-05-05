31a6bf589ad9d56d9af405afa7d7ae5a14e4042f
Dominik Klein <dk@zammad.com>
2023-05-05 14:57:13 +0200

    Fixes #4602 - GitLab third-party login is no longer working when api suffix is used inside site parameter.

---
+++ db/migrate/20230505114614_drop_api_suffix_from_third_party_login_gitlab_site_parameter.rb
@@ -0,0 +1,27 @@
+# Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
+
+class DropApiSuffixFromThirdPartyLoginGitLabSiteParameter < ActiveRecord::Migration[6.1]
+  def change
+    # return if it's a new setup
+    return if !Setting.exists?(name: 'system_init_done')
+
+    setting = Setting.find_by(name: 'auth_gitlab_credentials')
+    return if !setting
+
+    site_field_index = setting.options['form'].index { |field| field['name'] == 'site' }
+    setting.options['form'][site_field_index]['placeholder'] = 'https://gitlab.YOURDOMAIN.com/'
+
+    remove_api_suffix_from_current_value(setting)
+
+    setting.save!
+  end
+
+  private
+
+  def remove_api_suffix_from_current_value(setting)
+    current_value = setting.state_current['value']
+    return if current_value.blank?
+
+    current_value['site'].sub!('api/v4', '')
+  end
+end
---
+++ spec/db/migrate/drop_api_suffix_from_third_party_login_gitlab_site_parameter_spec.rb
@@ -0,0 +1,35 @@
+# Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
+
+require 'rails_helper'
+
+# rubocop:disable RSpec/FilePath
+RSpec.describe DropApiSuffixFromThirdPartyLoginGitLabSiteParameter, type: :db_migration do
+  # rubocop:enable RSpec/FilePath
+
+  before do
+    old_auth_gitlab_form = Setting.find_by(name: 'auth_gitlab_credentials').options[:form]
+    old_auth_gitlab_form[2][:placeholder] = 'https://gitlab.YOURDOMAIN.com/api/v4'
+
+    Setting.create_or_update(
+      title:       __('SAML App Credentials'),
+      name:        'auth_saml_credentials',
+      area:        'Security::ThirdPartyAuthentication::SAML',
+      description: __('Enables user authentication via SAML.'),
+      options:     {
+        form: old_auth_gitlab_form
+      }
+    )
+
+    Setting.set('auth_gitlab_credentials', { site: 'https://git.example.com/api/v4' })
+
+    migrate
+  end
+
+  it 'does migrate auth_gitlab_credentials setting placeholder' do
+    expect(Setting.find_by(name: 'auth_gitlab_credentials').options[:form][2][:placeholder]).to eq('https://gitlab.YOURDOMAIN.com/')
+  end
+
+  it 'does migrate auth_gitlab_credentials setting site value' do
+    expect(Setting.get('auth_gitlab_credentials')['site']).to eq('https://git.example.com/')
+  end
+end
