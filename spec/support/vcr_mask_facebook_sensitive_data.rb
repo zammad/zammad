484a9612f016267eedb8e3755996d2beb94c4101
Dusan Vuckovic <dv@zammad.com>
2023-05-09 15:17:56 +0200

    Follow-up 1e95bbd6 - Maintenance: Facebook - Fix tests failing if ran without cassettes.

---
+++ spec/support/vcr_mask_facebook_sensitive_data.rb
@@ -0,0 +1,9 @@
+# Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
+
+VCR.configure do |c|
+  c.before_record do |i|
+    if i.response.body.include?('"access_token":"')
+      i.response.body.gsub!(%r{"access_token":"[^"]+"}, '"access_token":"***"')
+    end
+  end
+end
