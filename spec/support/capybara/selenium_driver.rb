87e34244ea33dedbe4606df66f83aac5af4fdecb
Tobias Schäfer <github@blackox.org>
2023-05-17 10:21:06 +0200

    Maintenance: Disabled shm usage in the selenium chrome driver.

--- spec/support/capybara/selenium_driver.rb
+++ spec/support/capybara/selenium_driver.rb
@@ -15,7 +15,8 @@ Capybara.register_driver(:zammad_chrome) do |app|
       'intl.accept_languages'                                => 'en-US',
       'profile.default_content_setting_values.notifications' => 1, # ALLOW notifications
     },
-    args:             %w[--enable-logging --v=1],
+    # Disable shared memory usage as it does not really provide a performance gain but cause resource limit issues in CI.
+    args:             %w[--enable-logging --v=1 --disable-dev-shm-usage],
     # Disable the "Chrome is being controlled by automated test software." info bar.
     exclude_switches: ['enable-automation'],
   )
