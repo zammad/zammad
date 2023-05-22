b01e38d58efab21fe515777ef8240fb39a9fd7c7
Florian Liebe <fl@zammad.com>
2023-05-22 19:21:26 +0200

    Follow-up 54f0620 - Fixes #4595 - 2FA: Authenticator App

--- app/frontend/apps/mobile/pages/login/components/AfterAuth/TwoFactorConfiguration.vue
+++ app/frontend/apps/mobile/pages/login/components/AfterAuth/TwoFactorConfiguration.vue
@@ -1,5 +1,7 @@
 <!-- Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/ -->
 <script setup lang="ts">
+import { useForceDesktop } from '#shared/composables/useForceDesktop.ts'
+
 defineProps<{
   data?: unknown
 }>()
@@ -7,12 +9,14 @@ defineProps<{
 defineEmits<{
   (event: 'redirect', url: string): void
 }>()
+
+const { forceDesktop } = useForceDesktop()
 </script>
 
 <template>
   <div class="my-1 rounded-xl bg-red px-4 py-2 text-center text-white">
     <p>{{ $t('The two-factor authentication is not configured yet.') }}</p>
-    <CommonLink link="/#" class="text-white underline">
+    <CommonLink link="/#" class="text-white underline" @click="forceDesktop">
       {{ $t('Click here to set up a two-factor authentication method.') }}
     </CommonLink>
   </div>
--- spec/system/apps/mobile/after_auth_spec.rb
+++ spec/system/apps/mobile/after_auth_spec.rb
@@ -2,17 +2,39 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Mobile > After Auth', app: :mobile, authenticated_as: :agent, type: :system do
+RSpec.describe 'Mobile > After Auth', :aggregate_failures, app: :mobile, authenticated_as: :agent, type: :system do
   let(:agent) { create(:agent) }
+
+  before do
+    allow_any_instance_of(Auth::AfterAuth::TwoFactorConfiguration).to receive(:check).and_return(true)
+  end
 
   context 'when user is logged in, but after auth is required' do
     it 'requires setting up two factor auth' do
-      allow_any_instance_of(Auth::AfterAuth::TwoFactorConfiguration).to receive(:check).and_return(true)
-
       visit '/', skip_waiting: true
 
       expect(page).to have_content('The two-factor authentication is not configured yet')
       expect_current_route '/login/after-auth'
+    end
+  end
+
+  context 'when user is not authenticated, but 2FA is required', authenticated_as: false do
+    it 'provides a link for setting up 2FA by using the desktop view' do
+      allow_any_instance_of(Auth::AfterAuth::TwoFactorConfiguration).to receive(:check).and_return(true)
+
+      visit '/', skip_waiting: true
+
+      find_input('Username / Email').type(agent.login)
+      find_input('Password').type('test')
+
+      click_button
+
+      expect(page).to have_content('The two-factor authentication is not configured yet')
+      expect(page).to have_link('Click here to set up a two-factor authentication method.', href: '/#')
+
+      click 'a', text: 'Click here to set up a two-factor authentication method.'
+      expect_current_route('dashboard', app: :desktop)
+      expect(page).to have_content('You must protect your account with two-factor authentication.')
     end
   end
 end
