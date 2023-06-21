9e4cecb760d63909f64ac38590f2b97a352a2263
Florian Liebe <fl@zammad.com>
2023-06-21 13:27:46 +0200

    Fixes #4662 - Changing the primary organization of a user updates tickets that are assigned to user's secondary organization.

--- spec/models/user/updates_ticket_organization_spec.rb
+++ spec/models/user/updates_ticket_organization_spec.rb
@@ -48,5 +48,46 @@ RSpec.describe User::UpdatesTicketOrganization, type: :model do
         expect(NotificationFactory::Mailer).not_to have_received(:send)
       end
     end
+
+    context 'when customer has secondary organizations' do
+      let(:customer)                 { create(:customer, organization: old_primary_organization, organizations: [secondary_organization]) }
+      let(:old_primary_organization) { create(:organization) }
+      let(:new_primary_organization) { create(:organization) }
+      let(:secondary_organization)   { create(:organization) }
+
+      let(:tickets) do
+        ticket_primary_org   = create(:ticket, customer: customer, organization: old_primary_organization)
+        ticket_secondary_org = create(:ticket, customer: customer, organization: secondary_organization)
+
+        [ticket_primary_org, ticket_secondary_org]
+      end
+
+      before { tickets }
+
+      it 'updates ticket with primary organization and does not change ticket with secondary organization', :aggregate_failures do
+        customer.update!(organization: new_primary_organization)
+
+        expect(tickets.first.reload.organization).to eq(new_primary_organization)
+        expect(tickets.last.reload.organization).to eq(secondary_organization)
+      end
+
+      context 'when customer has no organization assigned at all' do
+        let(:customer) { create(:customer) }
+
+        let(:tickets) do
+          ticket_primary_org   = create(:ticket, customer: customer)
+          ticket_secondary_org = create(:ticket, customer: customer)
+
+          [ticket_primary_org, ticket_secondary_org]
+        end
+
+        it 'updates all ticket to contain the primary organization', :aggregate_failures do
+          customer.update!(organization: new_primary_organization, organizations: [secondary_organization])
+
+          expect(tickets.first.reload.organization).to eq(new_primary_organization)
+          expect(tickets.last.reload.organization).to eq(new_primary_organization)
+        end
+      end
+    end
   end
 end
