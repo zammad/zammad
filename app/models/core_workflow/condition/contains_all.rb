4824256a5cc410f38d2454eeeccd3630680db453
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-07-14 09:46:16 +0200

    Fixes #4714 - Operator "contains all" does not work with multi tree select.
    Co-authored-by: Dominik Klein <dk@zammad.com>

--- app/models/core_workflow/condition/contains_all.rb
+++ app/models/core_workflow/condition/contains_all.rb
@@ -2,21 +2,6 @@
 
 class CoreWorkflow::Condition::ContainsAll < CoreWorkflow::Condition::Backend
   def match
-    result = false
-    value.each do |current_value|
-      current_match = 0
-      condition_value.each do |current_condition_value|
-        next if current_condition_value.exclude?(current_value)
-
-        current_match += 1
-      end
-
-      next if current_match != condition_value.count
-
-      result = true
-
-      break
-    end
-    result
+    (value & condition_value).count == condition_value.count
   end
 end
