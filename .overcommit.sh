aac6c4b437fd706438bfc19db8d56ac7d714337f
Martin Gruner <mg@zammad.com>
2023-07-28 10:43:22 +0200

    Maintenance: Speed up custom overcommit hook.

--- .overcommit.sh
+++ .overcommit.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 # Don't require redis.
 export ZAMMAD_SAFE_MODE=1
 
 echo "Checking .pot catalog consistency..."
-rails generate zammad:translation_catalog --check
+rails generate zammad:translation_catalog --check &
 
-.gitlab/check_graphql_api_consistency.sh
+.gitlab/check_graphql_api_consistency.sh &
+
+FAILED=0
+for job in $(jobs -p)
+do
+  wait $job || let "FAILED+=1"
+done
+
+exit $FAILED
