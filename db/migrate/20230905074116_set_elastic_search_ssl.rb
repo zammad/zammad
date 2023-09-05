5ea56fefc5a13d96c7ad56c2cbbb92bebbb02117
Martin Gruner <mg@zammad.com>
2023-09-05 14:49:05 +0200

    Maintenance: Fix migration name

--- db/migrate/20230905074116_set_elastic_search_ssl.rb
+++ db/migrate/20230905074116_set_elastic_search_ssl.rb
@@ -1,6 +1,6 @@
 # Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
-class SetElasticSearchSsl < ActiveRecord::Migration[6.1]
+class SetElasticSearchSSL < ActiveRecord::Migration[6.1]
   def change
     # return if it's a new setup
     return if !Setting.exists?(name: 'system_init_done')
