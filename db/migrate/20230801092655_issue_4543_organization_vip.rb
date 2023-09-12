4e2448e6ac874b25d70d57d5e9fa33ce822715f3
Florian Liebe <fl@zammad.com>
2023-09-12 14:10:40 +0200

    Fixes #4811 - Upgrade to Zammad 6.1 not possible / migration is failing.
    Co-authored-by: Martin Gruner <mg@zammad.com>
    Co-authored-by: Florian Liebe <fl@zammad.com>

--- db/migrate/20230801092655_issue_4543_organization_vip.rb
+++ db/migrate/20230801092655_issue_4543_organization_vip.rb
@@ -1,58 +1,103 @@
 # Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
 class Issue4543OrganizationVip < ActiveRecord::Migration[6.1]
+  RESERVED_NAME  = 'vip'.freeze
+  SANITIZED_NAME = '_vip'.freeze
+  VIP_ATTRIBUTE_PAYLOAD = {
+    name:        'vip',
+    display:     'VIP',
+    data_type:   'boolean',
+    data_option: {
+      null:       true,
+      default:    false,
+      item_class: 'formGroup--halfSize',
+      options:    {
+        false => 'no',
+        true  => 'yes',
+      },
+      translate:  true,
+      permission: ['admin.organization'],
+    },
+    editable:    false,
+    active:      true,
+    screens:     {
+      edit:   {
+        '-all-' => {
+          null: true,
+        },
+      },
+      create: {
+        '-all-' => {
+          null: true,
+        },
+      },
+      view:   {
+        '-all-' => {
+          shown: false,
+        },
+      },
+    },
+    position:    1450,
+  }.freeze
+
   def change
     # return if it's a new setup
     return if !Setting.exists?(name: 'system_init_done')
 
+    # If there is already a boolean 'vip' attribute, we reuse it,
+    #   but switch it to the new values.
+    return if update_custom_vip_boolean_attribute
+
+    # If there is a 'vip' attribute of another type, we rename it
+    #   to '_vip' and create the new internal attribute thereafter.
+    rename_custom_vip_attribute
+    create_vip_db_column
+    create_vip_attribute
+  end
+
+  private
+
+  def custom_vip_attribute
+    ObjectManager::Attribute.get(object: 'Organization', name: RESERVED_NAME.dup)
+  end
+
+  def update_custom_vip_boolean_attribute
+    return false if !custom_vip_attribute || custom_vip_attribute.data_type != 'boolean'
+
+    custom_vip_attribute.update!(**VIP_ATTRIBUTE_PAYLOAD)
+    true
+  end
+
+  def rename_custom_vip_attribute
+    return if !custom_vip_attribute
+
+    custom_vip_attribute.update!(name: SANITIZED_NAME)
+
+    return if ActiveRecord::Base.connection.columns('organizations').map(&:name).exclude?(RESERVED_NAME)
+
+    ActiveRecord::Migration.rename_column(:organizations, RESERVED_NAME.to_sym, SANITIZED_NAME.to_sym)
+    Organization.connection.schema_cache.clear!
+    Organization.reset_column_information
+  end
+
+  def create_vip_db_column
     change_table :organizations do |t|
       t.boolean :vip, default: false, null: false
     end
 
     Organization.reset_column_information
+  end
 
+  def create_vip_attribute
     UserInfo.current_user_id = 1
 
     ObjectManager::Attribute.add(
-      force:       true,
-      object:      'Organization',
-      name:        'vip',
-      display:     'VIP',
-      data_type:   'boolean',
-      data_option: {
-        null:       true,
-        default:    false,
-        item_class: 'formGroup--halfSize',
-        options:    {
-          false => 'no',
-          true  => 'yes',
-        },
-        translate:  true,
-        permission: ['admin.organization'],
-      },
-      editable:    false,
-      active:      true,
-      screens:     {
-        edit:   {
-          '-all-' => {
-            null: true,
-          },
-        },
-        create: {
-          '-all-' => {
-            null: true,
-          },
-        },
-        view:   {
-          '-all-' => {
-            shown: false,
-          },
-        },
-      },
-      to_create:   false,
-      to_migrate:  false,
-      to_delete:   false,
-      position:    1450,
+      **VIP_ATTRIBUTE_PAYLOAD,
+      force:      true,
+      object:     'Organization',
+      to_create:  false,
+      to_migrate: false,
+      to_delete:  false,
     )
   end
 end
---
+++ spec/db/migrate/issue_4543_organization_vip_spec.rb
@@ -0,0 +1,38 @@
+# Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
+
+require 'rails_helper'
+
+RSpec.describe Issue4543OrganizationVip, db_strategy: :reset, type: :db_migration do
+  before do
+    # Clean-up vip field of schema
+    ObjectManager::Attribute.remove(object_lookup_id: ObjectLookup.by_name('Organization'), name: 'vip', force: true)
+    ObjectManager::Attribute.migration_execute(false)
+
+    # Create custom vip attribute
+    vip_attribute
+    ObjectManager::Attribute.migration_execute(false)
+  end
+
+  context "when a non-boolean 'vip' attribute already exists" do
+    let(:vip_attribute) { create(:object_manager_attribute_select, name: 'vip', object_name: 'Organization') }
+
+    it 'renames the existing attribute properly', :aggregate_failures do
+      migrate
+
+      expect(vip_attribute.reload.name).to eq('_vip')
+      expect(Organization.attribute_names).to include('_vip', 'vip')
+    end
+  end
+
+  context "when a boolean 'vip' attribute already exists" do
+    let(:vip_attribute) { create(:object_manager_attribute_boolean, name: 'vip', display: 'Custom VIP', object_name: 'Organization') }
+
+    it 'keeps the existing attribute and updates it', :aggregate_failures do
+      migrate
+
+      expect(vip_attribute.reload).to have_attributes(name: 'vip', display: 'VIP', position: 1450)
+      expect(Organization.attribute_names).to include('vip')
+      expect(Organization.attribute_names).not_to include('_vip')
+    end
+  end
+end
