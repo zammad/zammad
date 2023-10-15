b9d0eb15695ea6d3b5f74efa7b55c5d022a563fc
Dusan Vuckovic <dv@zammad.com>
2023-10-15 15:52:45 +0100

    Maintenance: Fix exit code handling during visual regression test run.

--- .cypress/visual-regression/snapshots.sh
+++ .cypress/visual-regression/snapshots.sh
@@ -2,8 +2,7 @@
 
 if [[ $(uname -m) == 'arm64' ]]; then
   # cannot run cypress while on arm, but in amd container
-  docker compose -f .cypress/visual-regression/docker-compose.arm.yml up
+  docker compose -f .cypress/visual-regression/docker-compose.arm.yml up --exit-code-from cypress
 else
-  docker compose -f .cypress/visual-regression/docker-compose.amd64.yml up
+  docker compose -f .cypress/visual-regression/docker-compose.amd64.yml up --exit-code-from cypress
 fi
-
