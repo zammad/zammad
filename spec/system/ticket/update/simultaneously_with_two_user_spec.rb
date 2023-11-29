fe9aab0cfe4f1a5f963e4a4d2008a9b967461cda
Dusan Vuckovic <dv@zammad.com>
2023-11-29 21:31:20 +0100

    Maintenance: Improve Capybara testing with two users simultaneously.

--- spec/system/ticket/update/simultaneously_with_two_user_spec.rb
+++ spec/system/ticket/update/simultaneously_with_two_user_spec.rb
@@ -204,7 +204,7 @@ RSpec.describe 'Ticket > Update > Simultaneously with two different user', type:
 
       using_session(:second_browser) do
         find('.js-textarea').send_keys('some other note')
-        find('.js-objectTitle').set('TTTsome level 2 <b>subject</b> 123äöü')
+        find('.ticketZoom-header .js-objectTitle').set('TTTsome level 2 <b>subject</b> 123äöü')
 
         # Click in the body field, to trigger the title update.
         find('.js-textarea').send_keys('trigger title')
