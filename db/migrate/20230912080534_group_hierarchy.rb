f583ade02714d246b277ee45af5dc86f8b1d4e0a
Dominik Klein <dk@zammad.com>
2023-11-30 21:37:34 +0100

    Follow-up 5b6ad99 - Fixes #4790 - Improve groups to support a more complex hierarchy.

--- db/migrate/20230912080534_group_hierarchy.rb
+++ db/migrate/20230912080534_group_hierarchy.rb
@@ -33,7 +33,7 @@ class GroupHierarchy < ActiveRecord::Migration[6.1]
   def migrate_group_name
     Group.all.each do |group|
       if group.name.exclude?('::')
-        group.update!(name_last: group.name)
+        group.update!(name_last: group.name, updated_by_id: 1)
 
         next
       end
@@ -48,7 +48,7 @@ class GroupHierarchy < ActiveRecord::Migration[6.1]
         try += 1
       end
 
-      group.update!(name: new_name, name_last: new_name)
+      group.update!(name: new_name, name_last: new_name, updated_by_id: 1)
     end
   end
 
