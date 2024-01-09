7d010ac618658c56a3a5b4b072b04d7299c013b8
Dusan Vuckovic <dv@zammad.com>
2024-01-09 16:07:33 +0000

    Maintenance: Fix Vite asset build failure (5th try).

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,9 @@
 // Copyright (C) 2012-2024 Zammad Foundation, https://zammad-foundation.org/
 
-const path = require('path')
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
-
-const entrypoints = path.resolve(__dirname, 'app/frontend/apps')
+const mobileConfig = require('./app/frontend/apps/mobile/styles/tailwind.mobile.js')
 
 module.exports = {
-  plugins: [
-    tailwindcss(path.resolve(entrypoints, 'mobile/styles/tailwind.mobile.js')),
-    tailwindcss(
-      path.resolve(entrypoints, 'desktop/styles/tailwind.desktop.js'),
-    ),
-    autoprefixer,
-  ],
+  plugins: [tailwindcss(mobileConfig), autoprefixer],
 }
