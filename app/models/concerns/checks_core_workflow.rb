2ef464313eef0cebf2dc1ab6c0008aa4ca5752e4
Bahusafoo <huggans.sean@gmail.com>
2024-01-10 08:59:05 -0600

    Add files via upload
    Created method merge_changes_into_restrict_values to resolve cases where perform_result does not have some of the submitted data while being passed to CoreWorkflow.perform().  This resolves an issue encountered when core workflows remove groups, and then add them back to the group selection object later on.

--- app/models/concerns/checks_core_workflow.rb
+++ app/models/concerns/checks_core_workflow.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2024 Zammad Foundation, https://zammad-foundation.org/
+# Copyright (C) 2012-2023 Zammad Foundation, https://zammad-foundation.org/
 
 module ChecksCoreWorkflow
   extend ActiveSupport::Concern
@@ -41,15 +41,36 @@ module ChecksCoreWorkflow
     check_mandatory(perform_result)
   end
 
+
   def check_restrict_values(perform_result)
+
+    merged_restrict_values = merge_changes_into_restrict_values(perform_result[:restrict_values], changes)
     changes.each_key do |key|
-      next if perform_result[:restrict_values][key].blank?
+      Rails.logger.warn "SWIFT_KEV Restricted Values Key :::: #{merged_restrict_values}"
+      Rails.logger.warn "SWIFT_KEV perform_result Key :::: #{perform_result}"
+
+      # Check if merged_restrict_values is not nil and key is present
+      next if merged_restrict_values&.[](key)&.any? { |value| value.to_s == self[key].to_s }
       next if self[key].blank?
       next if restricted_value?(perform_result, key)
 
       raise Exceptions::ApplicationModel.new(self, "Invalid value '#{self[key]}' for field '#{key}'!")
     end
   end
+
+  def merge_changes_into_restrict_values(restrict_values, changes)
+    return restrict_values unless restrict_values.is_a?(Hash) && changes.is_a?(Hash)
+
+    # Iterate through changes and merge each value into the corresponding key in restrict_values
+    changes.each do |key, value|
+      restrict_values[key] ||= []
+      restrict_values[key] |= Array(value).map(&:to_s)
+    end
+
+    restrict_values
+  end
+
+
 
   def restricted_value?(perform_result, key)
     if self[key].is_a?(Array)
