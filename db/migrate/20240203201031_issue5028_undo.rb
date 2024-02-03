5ca5217f03cc02105e1025fd7bda29c1adb9cf9b
Rolf Schmidt <rolf.schmidt@zammad.com>
2024-02-03 21:50:17 +0100

    Revert body fix because of blocking ticket creation issues (#5028).
    Co-authored-by: Florian Liebe <fl@zammad.com>

---
+++ db/migrate/20240203201031_issue5028_undo.rb
@@ -0,0 +1,12 @@
+# Copyright (C) 2012-2024 Zammad Foundation, https://zammad-foundation.org/
+
+class Issue5028Undo < ActiveRecord::Migration[7.0]
+  def change
+    # return if it's a new setup
+    return if !Setting.exists?(name: 'system_init_done')
+
+    field = ObjectManager::Attribute.for_object('TicketArticle').find_by(name: 'body')
+    field.screens.delete('create_middle')
+    field.save!
+  end
+end
