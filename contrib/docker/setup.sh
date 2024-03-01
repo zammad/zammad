ed3a9d1235c539872dffc34f33012a802835f576
Martin Gruner <mg@zammad.com>
2024-03-01 11:42:18 +0100

    Maintenance: Drop nulldb adapter.

--- contrib/docker/setup.sh
+++ contrib/docker/setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'builder' ]; then
-  PACKAGES="build-essential curl git libimlib2-dev libpq-dev shared-mime-info"
+  PACKAGES="build-essential curl git libimlib2-dev libpq-dev shared-mime-info postgresql"
 elif [ "$1" = 'runner' ]; then
   PACKAGES="curl libimlib2 libpq5 nginx gnupg"
 fi
@@ -14,14 +14,19 @@ apt-get install -y --no-install-recommends ${PACKAGES}
 rm -rf /var/lib/apt/lists/*
 
 if [ "$1" = 'builder' ]; then
+  # Create an empty DB just so that the Rails stack can run.
+  /etc/init.d/postgresql start
+  su - postgres bash -c "createuser zammad -R -S"
+  su - postgres bash -c "createdb --encoding=utf8 --owner=zammad zammad"
+
   cd "${ZAMMAD_DIR}"
   bundle config set without 'test development mysql'
   bundle install
-  sed -e 's#.*adapter: postgresql#  adapter: nulldb#g' -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: zammad-postgresql\n#g' < contrib/packager.io/database.yml.pkgr > config/database.yml
-  sed -i "/require 'rails\/all'/a require\ 'nulldb'" config/application.rb
+
   touch db/schema.rb
-  ZAMMAD_SAFE_MODE=1 bundle exec rake assets:precompile # Don't require Redis.
-  rm -r tmp/cache
+  ZAMMAD_SAFE_MODE=1 DATABASE_URL=postgresql://zammad:/zammad bundle exec rake assets:precompile # Don't require Redis.
+
+  rm -r tmp/*
   script/build/cleanup.sh
 fi
 
--- lib/zammad/application/initializer/db_preflight_check.rb
+++ lib/zammad/application/initializer/db_preflight_check.rb
@@ -3,7 +3,6 @@
 require 'zammad/application/initializer/db_preflight_check/base'
 require 'zammad/application/initializer/db_preflight_check/mysql2'
 require 'zammad/application/initializer/db_preflight_check/postgresql'
-require 'zammad/application/initializer/db_preflight_check/nulldb'
 
 module Zammad
   class Application
