250a9abdb94ec5671928c4157d2c6d67eebb6c9f
Dominik Klein <dk@zammad.com>
2024-04-10 07:56:37 +0200

    Fixes #5114 - OTRS import: When importing queues with a parent queue to Zammad...

--- lib/import/otrs/queue_factory.rb
+++ lib/import/otrs/queue_factory.rb
@@ -4,6 +4,15 @@ module Import
   module OTRS
     module QueueFactory
       extend Import::Factory
+
+      # rubocop:disable Style/ModuleFunction
+      extend self
+      # rubocop:enable Style/ModuleFunction
+
+      # We need to sort the records by name, to avoid missing parent queues.
+      def pre_import_hook(records, *_args)
+        records.sort_by! { |record| record['Name'] }
+      end
     end
   end
 end
