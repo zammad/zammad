804cc119d999cb936d5d38cb28b3954e26067f9c
Florian Liebe <fl@zammad.com>
2024-06-14 12:16:29 +0200

    Fixes #5196 - Configuration screen of triggers do not pop up.

--- app/policies/controllers/calendars_controller_policy.rb
+++ app/policies/controllers/calendars_controller_policy.rb
@@ -1,6 +1,6 @@
 # Copyright (C) 2012-2024 Zammad Foundation, https://zammad-foundation.org/
 
 class Controllers::CalendarsControllerPolicy < Controllers::ApplicationControllerPolicy
-  permit! :timezones, to: 'admin'
+  permit! :timezones, to: ['admin.calendar', 'admin.trigger', 'admin.scheduler']
   default_permit!('admin.calendar')
 end
