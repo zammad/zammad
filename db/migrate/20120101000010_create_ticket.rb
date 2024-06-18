c9bf2939f23e58cb51ae9bc44ff50935ca63d9f8
Martin Gruner <mg@zammad.com>
2024-06-18 10:03:33 +0200

    Maintenance: Drop unused templates_groups table.

--- db/migrate/20120101000010_create_ticket.rb
+++ db/migrate/20120101000010_create_ticket.rb
@@ -405,15 +405,6 @@ class CreateTicket < ActiveRecord::Migration[4.2]
     add_foreign_key :templates, :users, column: :created_by_id
     add_foreign_key :templates, :users, column: :updated_by_id
 
-    create_table :templates_groups, id: false do |t|
-      t.references :template
-      t.references :group
-    end
-    add_index :templates_groups, [:template_id]
-    add_index :templates_groups, [:group_id]
-    add_foreign_key :templates_groups, :templates
-    add_foreign_key :templates_groups, :groups
-
     create_table :channels do |t|
       t.references :group,                             null: true
       t.column :area,           :string, limit: 100,   null: false
@@ -582,7 +573,6 @@ class CreateTicket < ActiveRecord::Migration[4.2]
     drop_table :macros
     drop_table :slas
     drop_table :channels
-    drop_table :templates_groups
     drop_table :templates
     drop_table :text_modules_groups
     drop_table :text_modules
