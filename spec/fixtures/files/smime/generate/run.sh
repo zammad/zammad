03843b6f2a702a4462212aeea559a4633bd9e944
Dusan Vuckovic <dv@zammad.com>
2024-08-01 09:43:11 +0200

    Maintenance: Update expired test S/MIME certificates.

--- spec/fixtures/files/smime/generate/run.sh
+++ spec/fixtures/files/smime/generate/run.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 docker build --no-cache --tag zammad/smime-test-certificates:latest .
 
-docker run --rm --env=FORCE_REGENERATE=${FORCE_REGENERATE:=""} --volume "$(pwd)/../:/etc/ssl/certs" zammad/smime-test-certificates:latest
+docker run --rm --env=SKIP_REGENERATE=${SKIP_REGENERATE:=""} --volume "$(pwd)/../:/etc/ssl/certs" zammad/smime-test-certificates:latest
