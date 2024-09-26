012f549d7f31c2221d81b2d99ca080c2c9c36cd8
Martin Gruner <mg@zammad.com>
2024-09-26 07:39:43 +0200

    Maintenance: Fix SSL spec after switching to Debian 12

--- spec/system/ssl_spec.rb
+++ spec/system/ssl_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe 'SSL Verification', :aggregate_failures, authenticated_as: false,
     context 'with verify_ssl: true' do
       it 'UserAgent fails' do
         expect(UserAgent.get(url, {}, { verify_ssl: true })).not_to be_success
-        expect(UserAgent.get(url, {}, { verify_ssl: true }).error).to include('certificate verify failed (self signed certificate)')
+        expect(UserAgent.get(url, {}, { verify_ssl: true }).error).to include('certificate verify failed (self-signed certificate)')
       end
     end
 
     context 'without verify_ssl' do
       it 'UserAgent fails' do
         expect(UserAgent.get(url)).not_to be_success
-        expect(UserAgent.get(url).error).to include('certificate verify failed (self signed certificate)')
+        expect(UserAgent.get(url).error).to include('certificate verify failed (self-signed certificate)')
       end
     end
 
