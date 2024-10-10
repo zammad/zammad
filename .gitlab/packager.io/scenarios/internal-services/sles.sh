ced1602711267a3a82e4806a0374c4200b948a34
Tarek Shehab <tsh@zammad.com>
2024-10-10 11:45:22 +0200

    Remove special handling for Redis on SuSE in packager.io pipeline

--- .gitlab/packager.io/scenarios/internal-services/sles.sh
+++ .gitlab/packager.io/scenarios/internal-services/sles.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-# special handling for Redis on SuSE
-zypper install -y redis7
-cp /etc/redis/default.conf.example /etc/redis/zammad.conf
-chown root:redis /etc/redis/zammad.conf
-systemctl start redis@zammad.service
-
 zypper install -y zammad
 
 curl --retry 30 --retry-delay 1 --retry-connrefused http://localhost:3000 | grep "Zammad Helpdesk"
