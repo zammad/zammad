b5385b0d4c1b7927bfca7c95d6c3e80ab35adad4
Martin Gruner <mg@zammad.com>
2024-11-27 19:17:15 +0100

    try again Maintenance: Update to Rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ end
 module Zammad
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     Rails.autoloaders.each do |autoloader|
       autoloader.ignore            "#{config.root}/app/frontend"
