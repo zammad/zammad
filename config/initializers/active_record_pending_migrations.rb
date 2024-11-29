f9682ab28d1f119f79bf9775c7ac395af0a72e40
Martin Gruner <mg@zammad.com>
2024-11-29 08:09:44 +0100

    Follow-up: 180bc4a - Fix waiting in docker image

--- config/initializers/active_record_pending_migrations.rb
+++ config/initializers/active_record_pending_migrations.rb
@@ -1,5 +1,5 @@
 # Copyright (C) 2012-2024 Zammad Foundation, https://zammad-foundation.org/
 
 if %w[1 true].include? ENV['RAILS_CHECK_PENDING_MIGRATIONS']
-  ActiveRecord::Migration.check_pending!
+  ActiveRecord::Migration.check_all_pending!
 end
