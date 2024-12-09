ad5c4af7f754b244a438e7d3e3a11928ef5aa4de
Rolf Schmidt <rolf.schmidt@zammad.com>
2024-12-09 11:20:59 +0000

    Revert "Maintenance: Update to Rails 7.2"
    This reverts commit dfc455e63384b62a01b314d70338037a6c90015e.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ end
 module Zammad
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 7.1
 
     Rails.autoloaders.each do |autoloader|
       autoloader.ignore            "#{config.root}/app/frontend"
