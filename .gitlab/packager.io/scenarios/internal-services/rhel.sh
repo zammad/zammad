73735359f84f3e14e37ea6396fd3442c878dcddf
Martin Gruner <mg@zammad.com>
2024-12-10 07:39:37 +0100

    Maintenance: Fix packager.io tests for rhel-9

--- .gitlab/packager.io/scenarios/internal-services/rhel.sh
+++ .gitlab/packager.io/scenarios/internal-services/rhel.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+# Workaround: postgresql fails on rhel-9 when it gets installed together with Zammad.
+dnf install -y postgresql-server
+
 dnf install -y zammad
 
 curl --retry 30 --retry-delay 1 --retry-connrefused http://localhost:3000 | grep "Zammad Helpdesk"
