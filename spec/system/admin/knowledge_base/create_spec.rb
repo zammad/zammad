2f860b7978232bf6e54e3e1c5fc28aa482336fa7
Mantas <mantas@idev.lt>
2026-04-28 19:44:41 +0300

    Fixes #5986 - Error message during KB creation does not get translated

---
+++ spec/system/admin/knowledge_base/create_spec.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+require 'rails_helper'
+
+RSpec.describe 'Admin Panel > Knowledge Base > Create', type: :system do
+  before do
+    visit '/#manage/knowledge_base'
+  end
+
+  context 'with valid data' do
+    it 'creates a knowledge base' do
+      find('[data-name="kb_locales"] .js-input').fill_in with: 'Lithuanian'
+      find('.js-option.is-active').click
+
+      click_on 'Create Knowledge Base'
+
+      expect(page).to have_text('Theme').and have_text('Languages')
+    end
+  end
+
+  context 'with missing locale' do
+    it 'shows validation error' do
+      click_on 'Create Knowledge Base'
+
+      expect(page).to have_content('At least one locale is required.')
+    end
+  end
+end
