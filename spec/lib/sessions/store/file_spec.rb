76f4f9157a75f9fe212f04b8b4d7cc274cd29616
Mantas Masalskis <mm@zammad.com>
2026-05-15 00:11:55 +0300

    Fixes #5965 - Sessions metadata is not cleaned up when using ZAMMAD_PROCESS_SESSIONS_JOBS_WORKERS

---
+++ spec/lib/sessions/store/file_spec.rb
@@ -0,0 +1,45 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+require 'rails_helper'
+
+RSpec.describe Sessions::Store::File do
+  let(:instance) { described_class.new }
+
+  describe '#destroy' do
+    let(:client_id) { 'test_client_id' }
+
+    context 'when using single-worker sessions' do
+      before do
+        instance.create(client_id, 'test_data')
+      end
+
+      it 'destroys the session' do
+        expect { instance.destroy(client_id) }
+          .to change { instance.session_exists?(client_id) }
+          .to(false)
+      end
+    end
+
+    context 'when using multi-worker sessions' do
+      let(:node) { 'node1' }
+
+      before do
+        instance.create_node_session(node, client_id, 'test_data')
+      end
+
+      def count_node_sessions
+        i = 0
+
+        instance.each_session_by_node(node) { |_client_id| i += 1 }
+
+        i
+      end
+
+      it 'destroys the session' do
+        expect { instance.destroy(client_id) }
+          .to change { count_node_sessions }
+          .by(-1)
+      end
+    end
+  end
+end
