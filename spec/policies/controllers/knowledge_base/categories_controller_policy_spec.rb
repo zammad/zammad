d20dd0777c7f6aaa43d2bab3722ac821c66fb760
Mantas Masalskis <mm@zammad.com>
2026-05-19 17:08:19 +0300

    Fixes #5197 - Error on adding KB answers to category with granular permissions

--- spec/policies/controllers/knowledge_base/categories_controller_policy_spec.rb
+++ spec/policies/controllers/knowledge_base/categories_controller_policy_spec.rb
@@ -10,21 +10,77 @@ describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
   let(:record_class) { KnowledgeBase::CategoriesController }
 
   let(:record) do
-    rec             = record_class.new
-    rec.action_name = action_name
-    rec.params      = params
+    rec        = record_class.new
+    rec.params = params
 
     rec
   end
 
-  describe '#show?' do
-    let(:action_name) { :show }
-    let(:params)      { { id: internal_answer.category.id } }
+  context 'with knowledge_base.reader permissions' do
+    let(:user)   { create(:agent) }
+    let(:params) { { id: internal_answer.category.id } }
 
-    context 'with knowledge_base.reader permissions' do
-      let(:user) { create(:agent) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_actions(:create, :update, :destroy) }
+  end
 
-      it { is_expected.to permit_action(action_name) }
+  context 'when using granular permissions' do
+    let(:user) { create(:user, role_ids: [role.id]) }
+    let(:role) { create(:role, permission_names: ['knowledge_base.editor']) }
+
+    context 'when managign a subcategory' do
+      let(:params) { { id: subcategory.id, parent_id: category.id } }
+
+      before do
+        KnowledgeBase::PermissionsUpdate.new(knowledge_base).update! role => 'reader'
+        KnowledgeBase::PermissionsUpdate.new(category).update! role => access
+      end
+
+      context 'when parent category is editable' do
+        let(:access) { 'editor' }
+
+        it { is_expected.to permit_actions(:show, :create, :update, :destroy) }
+      end
+
+      context 'when parent category is not editable' do
+        let(:access) { 'reader' }
+
+        it { is_expected.to permit_action(:show) }
+        it { is_expected.to forbid_actions(:create, :update, :destroy) }
+      end
+
+      context 'when parent category is unreachable' do
+        let(:access) { 'none' }
+
+        it { is_expected.to forbid_actions(:show, :create, :update, :destroy) }
+      end
+    end
+
+    context 'when managing a top level category' do
+      let(:params) { { id: category.id } }
+
+      before do
+        KnowledgeBase::PermissionsUpdate.new(knowledge_base).update! role => access
+      end
+
+      context 'when KB is editable' do
+        let(:access) { 'editor' }
+
+        it { is_expected.to permit_actions(:show, :create, :update, :destroy) }
+      end
+
+      context 'when KB is not editable' do
+        let(:access) { 'reader' }
+
+        it { is_expected.to permit_action(:show) }
+        it { is_expected.to forbid_actions(:create, :update, :destroy) }
+      end
+
+      context 'when KB is unreachable' do
+        let(:access) { 'none' }
+
+        it { is_expected.to forbid_actions(:show, :create, :update, :destroy) }
+      end
     end
   end
 end
