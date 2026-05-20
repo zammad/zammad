24982067a1699f06697137fe482a6ffec8c1bf9e
Florian Liebe <fl@zammad.com>
2026-05-20 15:35:14 +0200

    Maintenance: Deduplicate issue tracker controller code.

--- app/controllers/integration/github_controller.rb
+++ app/controllers/integration/github_controller.rb
@@ -1,66 +1,16 @@
 # Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
 
-class Integration::GitHubController < ApplicationController
-  prepend_before_action :authenticate_and_authorize!
+class Integration::GitHubController < Integration::IssueTrackerController
+  private
 
-  SENSITIVE_FIELDS = [:api_token].freeze
-
-  def verify
-    unmasked_params = unmask_sensitive_params(params, Setting.get('github_config'))
-
-    github = ::GitHub.new(endpoint: unmasked_params[:endpoint], api_token: unmasked_params[:api_token])
-
-    github.verify!
-
-    render json: {
-      result: 'ok',
-    }
-  rescue => e
-    logger.error e
-
-    render json: {
-      result:  'failed',
-      message: e.message,
-    }
+  def integration_type
+    'github'
   end
 
-  def query
-    response = if params[:ticket_id]
-                 Service::Ticket::ExternalReferences::IssueTracker::TicketList.execute(
-                   type:   'github',
-                   ticket: Ticket.find(params[:ticket_id]),
-                 )
-               else
-                 Service::Ticket::ExternalReferences::IssueTracker::FetchMetadata.execute(
-                   type:        'github',
-                   issue_links: params[:links],
-                 )
-               end
-
-    render json: {
-      result:   'ok',
-      response: response,
-    }
-  rescue => e
-    logger.error e
-
-    render json: {
-      result:  'failed',
-      message: e.message,
-    }
-  end
-
-  def update
-    ticket = Ticket.find(params[:ticket_id])
-    ticket.with_lock do
-      authorize!(ticket, :show?)
-      ticket.preferences[:github] ||= {}
-      ticket.preferences[:github][:issue_links] = Array(params[:issue_links]).uniq
-      ticket.save!
-    end
-
-    render json: {
-      result: 'ok',
-    }
+  def integration_client
+    ::GitHub.new(
+      endpoint:  unmasked_params[:endpoint],
+      api_token: unmasked_params[:api_token],
+    )
   end
 end
