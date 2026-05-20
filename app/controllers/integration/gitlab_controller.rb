e5571a4f27f23d4ab402fb7530d8b0a80ccfbd7a
Florian Liebe <fl@zammad.com>
2026-05-20 15:35:41 +0200

    Maintenance: Deduplicate issue tracker controller code.
    (cherry picked from commit 7053fff90626a003c91365a0002ed0796c56dacc)
    
    26bfd681 Maintenance: Deduplicate issue tracker controller code.
    e6311e8d Tidy specs, outsource another function
    1181d291 show? -> agent_read_access?
    6c51ffca show? -> agent_read_access? 2.0
    
    Co-authored-by: Florian Liebe <fl@zammad.com>

--- app/controllers/integration/gitlab_controller.rb
+++ app/controllers/integration/gitlab_controller.rb
@@ -1,66 +1,17 @@
 # Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
 
-class Integration::GitLabController < ApplicationController
-  prepend_before_action :authenticate_and_authorize!
+class Integration::GitLabController < Integration::IssueTrackerController
+  private
 
-  SENSITIVE_FIELDS = [:api_token].freeze
-
-  def verify
-    unmasked_params = unmask_sensitive_params(params, Setting.get('gitlab_config'))
-
-    gitlab = ::GitLab.new(endpoint: unmasked_params[:endpoint], api_token: unmasked_params[:api_token], verify_ssl: unmasked_params[:verify_ssl])
-
-    gitlab.verify!
-
-    render json: {
-      result: 'ok',
-    }
-  rescue => e
-    logger.error e
-
-    render json: {
-      result:  'failed',
-      message: e.message,
-    }
+  def integration_type
+    'gitlab'
   end
 
-  def query
-    response = if params[:ticket_id]
-                 Service::Ticket::ExternalReferences::IssueTracker::TicketList.execute(
-                   type:   'gitlab',
-                   ticket: Ticket.find(params[:ticket_id]),
-                 )
-               else
-                 Service::Ticket::ExternalReferences::IssueTracker::FetchMetadata.execute(
-                   type:        'gitlab',
-                   issue_links: params[:links],
-                 )
-               end
-
-    render json: {
-      result:   'ok',
-      response: response,
-    }
-  rescue => e
-    logger.error e
-
-    render json: {
-      result:  'failed',
-      message: e.message,
-    }
-  end
-
-  def update
-    ticket = Ticket.find(params[:ticket_id])
-    ticket.with_lock do
-      authorize!(ticket, :show?)
-      ticket.preferences[:gitlab] ||= {}
-      ticket.preferences[:gitlab][:issue_links] = Array(params[:issue_links]).uniq
-      ticket.save!
-    end
-
-    render json: {
-      result: 'ok',
-    }
+  def integration_client
+    ::GitLab.new(
+      endpoint:   unmasked_params[:endpoint],
+      api_token:  unmasked_params[:api_token],
+      verify_ssl: unmasked_params[:verify_ssl],
+    )
   end
 end
