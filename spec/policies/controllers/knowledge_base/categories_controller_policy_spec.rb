5ae654b020502931d42bd65e0ee9a0ad48a5d57d
Mantas Masalskis <mm@zammad.com>
2026-05-22 19:02:47 +0300

    Fixes #6148 - Creating root-level Knowledge Base category fails with 'Couldn't find KnowledgeBase::Category without an ID'.
    Co-authored-by: Mantas Masalskis <mm@zammad.com>
    Co-authored-by: Dusan Vuckovic <dv@zammad.com>

--- spec/policies/controllers/knowledge_base/categories_controller_policy_spec.rb
+++ spec/policies/controllers/knowledge_base/categories_controller_policy_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
-  subject { described_class.new(user, record) }
+  subject(:policy) { described_class.new(user, record) }
 
   include_context 'basic Knowledge Base'
 
@@ -18,7 +18,7 @@ describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
 
   context 'with knowledge_base.reader permissions' do
     let(:user)   { create(:agent) }
-    let(:params) { { id: internal_answer.category.id } }
+    let(:params) { { id: internal_answer.category.id, knowledge_base_id: knowledge_base.id } }
 
     it { is_expected.to permit_action(:show) }
     it { is_expected.to forbid_actions(:create, :update, :destroy) }
@@ -28,8 +28,8 @@ describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
     let(:user) { create(:user, role_ids: [role.id]) }
     let(:role) { create(:role, permission_names: ['knowledge_base.editor']) }
 
-    context 'when managign a subcategory' do
-      let(:params) { { id: subcategory.id, parent_id: category.id } }
+    context 'when managing a subcategory' do
+      let(:params) { { id: subcategory.id, parent_id: category.id, knowledge_base_id: knowledge_base.id } }
 
       before do
         KnowledgeBase::PermissionsUpdate.new(knowledge_base).update! role => 'reader'
@@ -56,8 +56,28 @@ describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
       end
     end
 
+    context 'when creating a new root-level category (#6148)' do
+      let(:params) { { knowledge_base_id: knowledge_base.id } }
+
+      before do
+        KnowledgeBase::PermissionsUpdate.new(knowledge_base).update! role => access
+      end
+
+      context 'when KB is editable' do
+        let(:access) { 'editor' }
+
+        it { is_expected.to permit_action(:create) }
+      end
+
+      context 'when KB is not editable' do
+        let(:access) { 'reader' }
+
+        it { is_expected.to forbid_action(:create) }
+      end
+    end
+
     context 'when managing a top level category' do
-      let(:params) { { id: category.id } }
+      let(:params) { { id: category.id, knowledge_base_id: knowledge_base.id } }
 
       before do
         KnowledgeBase::PermissionsUpdate.new(knowledge_base).update! role => access
@@ -80,6 +100,14 @@ describe Controllers::KnowledgeBase::CategoriesControllerPolicy do
         let(:access) { 'none' }
 
         it { is_expected.to forbid_actions(:show, :create, :update, :destroy) }
+      end
+    end
+
+    context 'when creating a root-level category without knowledge_base_id' do
+      let(:params) { { id: category.id } }
+
+      it 'raises an appropriate error' do
+        expect { policy.create? }.to raise_error(ActiveRecord::RecordNotFound, %r{Couldn't find KnowledgeBase without an ID})
       end
     end
   end
