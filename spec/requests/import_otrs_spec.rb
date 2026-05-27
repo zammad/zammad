565daa7ee070daba9e3290e81cf5ef99dab21efa
Florian Liebe <fl@zammad.com>
2026-05-27 20:43:11 +0200

    Maintenance: Improve OTRS import controller.

--- spec/requests/import_otrs_spec.rb
+++ spec/requests/import_otrs_spec.rb
@@ -6,6 +6,27 @@ RSpec.describe 'ImportOtrs', authenticated_as: false, set_up: false, type: :requ
   let(:action)   { nil }
   let(:endpoint) { "/api/v1/import/otrs/#{action}" }
 
+  describe 'POST /api/v1/import/otrs/import_check' do
+    let(:action) { 'import_check' }
+
+    context 'when setup is already done' do
+      before do
+        create_list(:user, 3)
+
+        allow(Import::OTRS::Requester).to receive(:list)
+        allow(Import::OTRS::Requester).to receive(:load)
+      end
+
+      it 'returns setup_done', :aggregate_failures do
+        post endpoint, as: :json
+        expect(json_response['setup_done']).to be true
+
+        expect(Import::OTRS::Requester).not_to have_received(:list)
+        expect(Import::OTRS::Requester).not_to have_received(:load)
+      end
+    end
+  end
+
   describe 'GET /api/v1/import/otrs/import_status' do
     let(:action) { 'import_status' }
 
@@ -21,6 +42,13 @@ RSpec.describe 'ImportOtrs', authenticated_as: false, set_up: false, type: :requ
 
       get endpoint, as: :json
       expect(json_response['setup_done']).to be true
+    end
+
+    it 'returns not running message when import has not started' do
+      allow(Import::OTRS).to receive(:status_bg).and_return({ message: 'not running' })
+
+      get endpoint, as: :json
+      expect(json_response['message']).to eq('not running')
     end
   end
 end
