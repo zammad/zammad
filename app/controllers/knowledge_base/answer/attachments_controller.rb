c9d09c26a35c453c84b1094d718b072a1f2e7234
Mantas Masalskis <mm@zammad.com>
2026-05-28 14:00:19 +0300

    Maintenance: Improve KB attachments handling

--- app/controllers/knowledge_base/answer/attachments_controller.rb
+++ app/controllers/knowledge_base/answer/attachments_controller.rb
@@ -2,22 +2,21 @@
 
 class KnowledgeBase::Answer::AttachmentsController < ApplicationController
   prepend_before_action :authenticate_and_authorize!
-  before_action :fetch_answer
 
   def create
-    @answer.add_attachment params[:file]
+    answer.add_attachment params[:file]
 
-    render json: @answer.assets({})
+    render json: answer.assets({})
   end
 
   def destroy
-    @answer.remove_attachment params[:id]
+    answer.remove_attachment params[:id]
 
-    render json: @answer.assets({})
+    render json: answer.assets({})
   end
 
   def clone_to_form
-    new_attachments = @answer.clone_attachments('UploadCache', params[:form_id], only_attached_attachments: true)
+    new_attachments = answer.clone_attachments('UploadCache', params[:form_id], only_attached_attachments: true)
 
     render json: {
       attachments: new_attachments
@@ -26,7 +25,7 @@ class KnowledgeBase::Answer::AttachmentsController < ApplicationController
 
   private
 
-  def fetch_answer
-    @answer = KnowledgeBase::Answer.find params[:answer_id]
+  def answer
+    @answer ||= KnowledgeBase::Answer.find params[:answer_id]
   end
 end
--- app/policies/controllers/knowledge_base/answer/attachments_controller_policy.rb
+++ app/policies/controllers/knowledge_base/answer/attachments_controller_policy.rb
@@ -1,6 +1,21 @@
 # Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
 
 class Controllers::KnowledgeBase::Answer::AttachmentsControllerPolicy < Controllers::ApplicationControllerPolicy
-  permit! :clone_to_form, to: 'knowledge_base.*'
-  default_permit!('knowledge_base.editor')
+  def create?
+    answer_policy.update?
+  end
+
+  def destroy?
+    answer_policy.update?
+  end
+
+  def clone_to_form?
+    answer_policy.show?
+  end
+
+  private
+
+  def answer_policy
+    @answer_policy ||= KnowledgeBase::AnswerPolicy.new(user, record.send(:answer))
+  end
 end
