1f5d8846a443b798228d5fb5c33798a60dad5d57
Florian Liebe <fl@zammad.com>
2026-06-08 12:06:23 +0200

    Maintenance: Desktop View - Feedback> Milestone question is asked multiple times.

--- app/frontend/apps/desktop/stores/appUsage.ts
+++ app/frontend/apps/desktop/stores/appUsage.ts
@@ -1,13 +1,15 @@
 // Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
 
+import { useLocalStorage } from '@vueuse/core'
 import { acceptHMRUpdate, defineStore } from 'pinia'
-import { computed } from 'vue'
+import { computed, toRef } from 'vue'
 
-import { initializeBetaUi } from '#desktop/components/BetaUi/composables/useBetaUi.ts'
+import { useApplicationStore } from '#shared/stores/application.ts'
+import { useSessionStore } from '#shared/stores/session.ts'
+
 import { useBetaUiFeedbackConsentState } from '#desktop/components/BetaUi/composables/useBetaUiFeedbackConsentState.ts'
-import { useAppUsage } from '#desktop/composables/BetaUi/useAppUsage.ts'
 import { useTimeTracker } from '#desktop/composables/BetaUi/useTimeTracker.ts'
-import type { MilestoneKey } from '#desktop/types/appUsage.ts'
+import type { MilestoneKey, MilestonesHistoryRecords } from '#desktop/types/appUsage.ts'
 
 const MILESTONES = [
   { key: '1h', milliseconds: 60 * 60 * 1000 }, // 1 hour
@@ -16,22 +18,59 @@ const MILESTONES = [
 ] as const
 
 export const useAppUsageStore = defineStore('appUsage', () => {
-  const {
-    milestoneHistory,
-    triggerMilestone,
-    totalAppUsageTime,
-    neverAskAgainForTimedFeedback,
-    setNeverAskAgainForTimedFeedback,
-  } = useAppUsage()
+  const milestoneHistory = useLocalStorage<MilestonesHistoryRecords>(
+    'app-usage-milestones-trigger-history',
+    () => ({
+      '1h': false,
+      '5h': false,
+      '20h': false,
+    }),
+  )
+
+  const triggerMilestone = (key: MilestoneKey) => {
+    milestoneHistory.value[key] = true
+  }
+
+  const resetMilestoneHistory = () => {
+    milestoneHistory.value = {
+      '1h': false,
+      '5h': false,
+      '20h': false,
+    }
+  }
+
+  const totalAppUsageTime = useLocalStorage('app-usage-total-time', 0)
+
+  const resetTotalAppUsageTime = () => {
+    totalAppUsageTime.value = 0
+  }
+
+  const neverAskAgainForTimedFeedback = useLocalStorage(
+    'beta-ui-feedback-never-ask-again-timed',
+    false,
+  )
+
+  const setNeverAskAgainForTimedFeedback = (value = true) => {
+    neverAskAgainForTimedFeedback.value = value
+  }
+
+  const switchValue = useLocalStorage('beta-ui-switch', false)
+  const dismissValue = useLocalStorage('beta-ui-switch-dismiss', false)
+
+  const user = toRef(useSessionStore(), 'user')
+  const config = toRef(useApplicationStore(), 'config')
+
+  const betaUiSwitchAvailable = computed(
+    () => config.value?.ui_desktop_beta_switch && user.value?.hasBetaUiSwitchAvailable,
+  )
+
+  const { hasFeedbackConsent } = useBetaUiFeedbackConsentState()
 
   const updateTotalUsage = (millisecondsCount: number) => {
     if (typeof totalAppUsageTime.value !== 'number') totalAppUsageTime.value = 0
 
     totalAppUsageTime.value += millisecondsCount
   }
-
-  const { hasFeedbackConsent } = useBetaUiFeedbackConsentState()
-  const { switchValue, betaUiSwitchAvailable } = initializeBetaUi()
 
   useTimeTracker(updateTotalUsage, {
     enabled: () =>
@@ -57,12 +96,18 @@ export const useAppUsageStore = defineStore('appUsage', () => {
   })
 
   return {
+    switchValue,
+    dismissValue,
+    betaUiSwitchAvailable,
+    neverAskAgainForTimedFeedback: computed(() => neverAskAgainForTimedFeedback.value),
+    setNeverAskAgainForTimedFeedback,
     totalAppUsageTime: computed(() => totalAppUsageTime.value),
     triggeredMilestones: computed(() => milestoneHistory.value),
     triggerMilestone,
+    resetMilestoneHistory,
+    resetTotalAppUsageTime,
     currentMilestoneKey,
     shouldTriggerMilestoneDialog,
-    setNeverAskAgainForTimedFeedback,
   }
 })
 
