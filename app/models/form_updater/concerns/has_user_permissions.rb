f87b42752e070087cfd2d5f6e3e6b12dbf182b23
Dusan Vuckovic <dv@zammad.com>
2026-06-10 09:58:36 +0100

    Follow-up b2cfa6f350532be49380b21b4b4fc7c11bde0d01 - Fixes #6159 - Note fields accept and display HTML tags.

--- app/models/form_updater/concerns/has_user_permissions.rb
+++ app/models/form_updater/concerns/has_user_permissions.rb
@@ -31,7 +31,7 @@ module FormUpdater::Concerns::HasUserPermissions
         {
           value:       elem.id,
           label:       elem.name,
-          description: elem.note&.html2text(true, link_style: :plain),
+          description: elem.note&.html2text(link_style: :plain),
         }
       end
   end
