8b260ab956d3567056e56446d430de35677d54c7
Joe Schroecker <js@zammad.com>
2026-06-11 21:11:43 +0200

    Maintenance - Desktop View: Improve explicitly configured theme doesn't get applied when switching user.
    Co-authored-by: Benjamin Scharf <bs@zammad.com>

--- app/frontend/apps/desktop/stores/theme.ts
+++ app/frontend/apps/desktop/stores/theme.ts
@@ -32,89 +32,93 @@ const saveDOMTheme = (theme: AppThemeName) => {
   root.style.colorScheme = theme
 }
 
-export const useThemeStore = defineStore('theme', () => {
-  const session = useSessionStore()
+export const useThemeStore = defineStore(
+  'theme',
+  () => {
+    const session = useSessionStore()
 
-  const savingTheme = ref(false)
+    const savingTheme = ref(false)
 
-  const setThemeMutation = new MutationHandler(useUserCurrentAppearanceMutation(), {
-    errorNotificationMessage: __('The appearance could not be updated.'),
-  })
-
-  const saveTheme = (newTheme: AppThemeName) => {
-    const sanitizedTheme = sanitizeTheme(newTheme)
-    saveDOMTheme(sanitizedTheme)
-  }
-
-  const setTheme = async (theme: string) => {
-    const oldTheme = session.user?.preferences?.theme
-
-    session.setUserPreference('theme', theme)
-
-    return setThemeMutation.send({ theme: theme as EnumAppearanceTheme }).catch(() => {
-      session.setUserPreference('theme', oldTheme)
+    const setThemeMutation = new MutationHandler(useUserCurrentAppearanceMutation(), {
+      errorNotificationMessage: __('The appearance could not be updated.'),
     })
-  }
 
-  const currentTheme = computed<EnumAppearanceTheme>(
-    () => session.user?.preferences?.theme || EnumAppearanceTheme.Auto,
-  )
-
-  const preferredColorScheme = usePreferredColorScheme()
-
-  const isDarkMode = computed(() => {
-    if (currentTheme.value === EnumAppearanceTheme.Auto) {
-      return preferredColorScheme.value === 'no-preference'
-        ? false // if no system preference, default to light mode
-        : preferredColorScheme.value === EnumAppearanceTheme.Dark
+    const saveTheme = (newTheme: AppThemeName) => {
+      const sanitizedTheme = sanitizeTheme(newTheme)
+      saveDOMTheme(sanitizedTheme)
     }
-    return currentTheme.value === EnumAppearanceTheme.Dark
-  })
 
-  const updateTheme = async (value: EnumAppearanceTheme) => {
-    try {
+    const setTheme = async (theme: string) => {
+      const oldTheme = session.user?.preferences?.theme
+
+      session.setUserPreference('theme', theme)
+
+      return setThemeMutation.send({ theme: theme as EnumAppearanceTheme }).catch(() => {
+        session.setUserPreference('theme', oldTheme)
+      })
+    }
+
+    const currentTheme = computed<EnumAppearanceTheme>(
+      () => session.user?.preferences?.theme || EnumAppearanceTheme.Auto,
+    )
+
+    const preferredColorScheme = usePreferredColorScheme()
+
+    const isDarkMode = computed(() => {
+      if (currentTheme.value === EnumAppearanceTheme.Auto) {
+        return preferredColorScheme.value === 'no-preference'
+          ? false // if no system preference, default to light mode
+          : preferredColorScheme.value === EnumAppearanceTheme.Dark
+      }
+      return currentTheme.value === EnumAppearanceTheme.Dark
+    })
+
+    const updateTheme = async (value: EnumAppearanceTheme) => {
       if (value === session.user?.preferences?.theme || savingTheme.value) return
 
       savingTheme.value = true
 
-      await setTheme(value)
-    } finally {
-      savingTheme.value = false
-    }
-  }
-
-  // sync theme in case HTML value was not up-to-date when we loaded user preferences
-  const syncTheme = () => {
-    if (currentTheme.value !== 'auto') return saveDOMTheme(currentTheme.value)
-    const theme = getPreferredTheme()
-    saveDOMTheme(theme)
-  }
-
-  // Update based on global system level preference
-  watch(preferredColorScheme, (newTheme) => {
-    const theme = (currentTheme.value as AppThemeName) || newTheme
-    saveTheme(theme)
-  })
-
-  // in case user changes the theme in another tab
-  watch(
-    () => currentTheme.value,
-    (newTheme) => {
-      if (newTheme) {
-        saveTheme(newTheme as AppThemeName)
+      try {
+        await setTheme(value)
+      } finally {
+        savingTheme.value = false
       }
-    },
-  )
+    }
 
-  return {
-    savingTheme,
-    preferredColorScheme,
-    currentTheme,
-    isDarkMode,
-    updateTheme,
-    syncTheme,
-  }
-})
+    // sync theme in case HTML value was not up-to-date when we loaded user preferences
+    const syncTheme = () => {
+      if (currentTheme.value !== EnumAppearanceTheme.Auto) return saveDOMTheme(currentTheme.value)
+      const theme = getPreferredTheme()
+      saveDOMTheme(theme)
+    }
+
+    // Update based on global system level preference
+    watch(preferredColorScheme, (newTheme) => {
+      const theme = (currentTheme.value as AppThemeName) || newTheme
+      saveTheme(theme)
+    })
+
+    // in case user changes the theme in another tab, or store is recreated after logout
+    watch(
+      () => currentTheme.value,
+      (newTheme) => {
+        if (newTheme) saveTheme(newTheme as AppThemeName)
+      },
+    )
+
+    return {
+      savingTheme,
+      preferredColorScheme,
+      currentTheme,
+      isDarkMode,
+      updateTheme,
+      syncTheme,
+    }
+  },
+  {
+    requiresAuth: false,
+  },
+)
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useThemeStore, import.meta.hot))
