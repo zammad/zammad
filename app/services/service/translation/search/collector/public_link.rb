cc23d5d8b1f9a3ebaa6735752a402f94588b97ea
Benjamin Scharf <bs@zammad.com>
2026-06-16 22:26:15 +0200

    Fixes #6184 - Public links do not get translated

---
+++ app/services/service/translation/search/collector/public_link.rb
@@ -0,0 +1,25 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+class Service::Translation::Search::Collector::PublicLink < Service::Translation::Search::Collector
+  private
+
+  def list_sources
+    @list_sources ||= display_titles | link_descriptions
+  end
+
+  def search_sources
+    @search_sources ||= list_sources.select { |source| source.downcase.include?(query.downcase) }
+  end
+
+  def public_links
+    @public_links ||= ::PublicLink.all
+  end
+
+  def display_titles
+    public_links.pluck(:title)
+  end
+
+  def link_descriptions
+    public_links.pluck(:description)
+  end
+end
