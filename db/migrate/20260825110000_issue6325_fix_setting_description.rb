9c43b12444c003580f374e3b8b942d637b18bf29
brigitte-bb <brigitte-bb@users.noreply.github.com>
2026-08-25 13:51:34 +0200

    Add: migration to fix description of send_no_auto_response_reg_exp
    Fixes #6325

---
+++ db/migrate/20260825110000_issue6325_fix_setting_description.rb
@@ -0,0 +1,10 @@
+# Copyright (C) 2012-2026 Zammad Foundation, https://zammad-foundation.org/
+
+class Issue6325FixSettingDescription < ActiveRecord::Migration[8.0]
+  def change
+    # return if it's a new setup
+    return if !Setting.exists?(name: 'system_init_done')
+
+    Setting.find_by(name: 'send_no_auto_response_reg_exp')&.update(description: 'If this regex matches, no notification will be sent to the sender.')
+  end
+end
