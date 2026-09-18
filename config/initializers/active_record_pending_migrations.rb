e14dae3f7bd5c1c653ec99482298958c27600de9
Rolf Schmidt <rolf.schmidt@zammad.com>
2026-09-18 15:14:54 +0200

    Maintenance: Cover the staged package check in the pending migrations initializer for Kubernetes deployments.
    Approved-by: Martin Gruner <mg@zammad.com>
    
    
    (cherry picked from commit c545ff10b2ea40e1f18fb770c628c9381a512376)
    
    7cbb9c79 Maintenance: Cover the staged package check in the pending migrations...
    
    Co-authored-by: Rolf Schmidt <rolf.schmidt@zammad.com>

--- config/initializers/active_record_pending_migrations.rb
+++ config/initializers/active_record_pending_migrations.rb
@@ -2,4 +2,10 @@
 
 if %w[1 true].include? ENV['RAILS_CHECK_PENDING_MIGRATIONS']
   ActiveRecord::Migration.check_all_pending!
+
+  # Staged addon packages of container images must be fully applied as well.
+  #   Deferred, because app constants must not be autoloaded while initializers run.
+  Rails.application.config.after_initialize do
+    Package.check_staged_packages_applied!
+  end
 end
